bf6d971d17123753363ee896fe1709b2c16aa5a9
Igor Rjabinin <igor.rjabinin@gmail.com>
2014-04-28 11:53:10 +0200

    rozdelenie prostredi na local + staging pripojenie k DB inicializacia migracii

--- app/config/database.php
+++ app/config/database.php
@@ -55,11 +55,11 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'webumenia',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
+			'collation' => 'utf8_slovak_ci',
 			'prefix'    => '',
 		),
 
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+	'local' => array('igorovmcbookpro', 'IgorovMacbookPro.local', '*.local'),
+	'staging' => array('sngweb'),
 
 ));
 
