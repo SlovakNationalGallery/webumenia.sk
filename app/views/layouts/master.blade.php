afbf6e0c9f06289696f79be7143277467a68f11e
Igor Rjabinin <igor.rjabinin@gmail.com>
2015-06-01 09:55:06 +0200

    socialny footer

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -121,7 +121,20 @@
 
 	<div class="footer">
       <div class="container">
-        <p class="text-muted text-center">Vyrobil a spravuje <a href="http://lab.sng.sk" target="_blank" class="sans">lab.SNG</a></p>
+      	<div class="row">
+      		<div class="col-xs-6">
+      			<a href="https://www.facebook.com/webumenia.sk" target="_blank" data-toggle="tooltip" title="facebook"><i class="fa fa-facebook fa-lg"></i></a>
+      			<a href="http://webumenia.tumblr.com/" target="_blank" data-toggle="tooltip" title="tumblr"><i class="fa fa-tumblr fa-lg"></i></a>
+      			<a href="https://vimeo.com/webumeniask" target="_blank" data-toggle="tooltip" title="vimeo"><i class="fa fa-vimeo-square fa-lg"></i></a>
+      			<a href="https://sk.pinterest.com/webumeniask/" target="_blank" data-toggle="tooltip" title="pinterest"><i class="fa fa-pinterest fa-lg"></i></a>
+      			<a href="https://twitter.com/webumeniask" target="_blank" data-toggle="tooltip" title="twitter"><i class="fa fa-twitter fa-lg"></i></a>
+      			<a href="https://github.com/SlovakNationalGallery" target="_blank" data-toggle="tooltip" title="github"><i class="fa fa-github fa-lg"></i></a>
+      		</div>
+      		<div class="col-xs-6">
+      			<p class="text-muted text-right">Vyrobil a spravuje <a href="http://lab.sng.sk" target="_blank" class="sans">lab.SNG</a></p>
+      		</div>
+      	</div>
+        
       </div>
     </div>
 
