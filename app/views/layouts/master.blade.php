859d026bedbdb4007117dabc1f5ba194b8aae6cb
Igor Rjabinin <igor.rjabinin@gmail.com>
2015-06-01 18:52:34 +0200

    ga

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -37,14 +37,15 @@
 		{{ HTML::style('css/magnific-popup.css') }}
         {{ HTML::script('js/modernizr.custom.js') }}
 
-		<script type="text/javascript">
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-			ga('create', 'UA-19030232-4', 'sng.sk');
-			ga('send', 'pageview');	 
+		  ga('create', 'UA-19030232-8', 'auto');
+		  ga('send', 'pageview');
+
 		</script>
 
 
