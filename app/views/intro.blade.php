935239df064c031607c3fa8d482e1e33fc6d77b3
Igor Rjabinin <igor.rjabinin@gmail.com>
2015-06-17 12:17:54 +0200

    leto na intro stranku

--- app/views/intro.blade.php
+++ app/views/intro.blade.php
@@ -19,9 +19,10 @@
       <div class="carousel-inner" role="listbox">
 
         <div class="item active ">
-            <a href="/clanok/spustili-sme-novy-web-umenia" class="header-image text-center" style="background-image: url(/images/clanky/psicek-a-macicka-sa-tesia.jpg); text-shadow:0px 1px 0px #777; color: #fff">
+            <a href="/kolekcia/25" class="header-image text-center" style="background-image: url(/images/kolekcie/leto.jpg); text-shadow:0px 1px 0px #777; color: #fff">
                 <div class="header-body">
-                    <h1>Spustili sme nový Web umenia!</h1>
+                    <h1>Leto</h1>
+                    <h3>37 pohľadov na najteplejšie mesiace roku</h3>
                 </div>
             </a>
         </div>
@@ -36,13 +37,11 @@
         </div>
 
         <div class="item">
-            <a href="/katalog?work_type=kresba&amp;technique=koláž&amp;has_iip=1" class="header-image text-center" style="background-image: url(/images/kolekcie/kolaz.jpg); text-shadow:0px 1px 0px #777; color: #fff">
+            <a href="/katalog?search=&author=&work_type=kresba&tag=&gallery=Slovenská+národná+galéria%2C+SNG&topic=&technique=akvarel&has_iip=1&year-range=600%2C2015" class="header-image text-center" style="background-image: url(/images/kolekcie/nosorozec.jpg); text-shadow:0px 1px 0px #777; color: #fff">
                 <div class="header-body">
                     <h2>výtvarný druh: kresba<br>
                     +<br>
-                    technika: koláž<br>
-                    + <br>
-                    len diela so zoom
+                    technika: akvarel<br>
                     </h2>
                 </div>
             </a>
