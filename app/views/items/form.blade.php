9ce1ee33edf8b2a863ef2254a6aa426deaaca542
Igor Rjabinin <igor.rjabinin@gmail.com>
2015-08-26 01:21:15 +0200

    oprava v editacii diela - nastavit autora popisu ako predvoleneho aj ked je iny, ako aktualne prihlaseny uzivatel

--- app/views/items/form.blade.php
+++ app/views/items/form.blade.php
@@ -57,7 +57,7 @@
 <div class="col-md-4">
 	<div class="form-group">
 	{{ Form::label('description_user_id', 'popis - autor') }}
-	{{ Form::select('description_user_id', User::lists('name','id'), Auth::user()->id, array('class' => 'form-control')) }}
+	{{ Form::select('description_user_id', User::lists('name','id'), Input::old('description_user_id', (isSet($item)) ? $item->description_user_id : Auth::user()->id), array('class' => 'form-control')) }}
 	</div>
 </div>
 <div class="col-md-4">
