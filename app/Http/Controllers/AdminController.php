24cba6e5e92f5fd2aa0c8fa0b148992994cc2215
Laravel Shift <shift@laravelshift.com>
2016-05-11 10:36:45 -0400

    Shift controllers
    Laravel 5 replaced `app/controllers/BaseController` with
    `app/Http/Controllers/Controller`. Any custom code within the
    `BaseController` class has been moved to the new `Controller` class and
    all controllers have been updated to extend `Controller`.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,6 +1,6 @@
 <?php
 
-class AdminController extends BaseController
+class AdminController extends Controller
 {
 
     public function index()
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,4 +8,16 @@ abstract class Controller extends BaseController
 {
 
     use DispatchesCommands, ValidatesRequests;
+
+    /**
+     * Setup the layout used by the controller.
+     *
+     * @return void
+     */
+    protected function setupLayout()
+    {
+        if (! is_null($this->layout)) {
+            $this->layout = View::make($this->layout);
+        }
+    }
 }
