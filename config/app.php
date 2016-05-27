49298a769a2d81757f626601a6f21f79e4caf926
Laravel Shift <shift@laravelshift.com>
2016-05-27 03:46:10 -0400

    Remove old providers
    Laravel 5.1 removes the `BusServiceProvider` and
    `ConfigServiceProvider` providers.

--- config/app.php
+++ config/app.php
@@ -121,8 +121,6 @@ return array(
         'Illuminate\Auth\Passwords\PasswordResetServiceProvider',
         'Collective\Html\HtmlServiceProvider',
         'App\Providers\AppServiceProvider',
-        'App\Providers\BusServiceProvider',
-        'App\Providers\ConfigServiceProvider',
         'App\Providers\EventServiceProvider',
         'App\Providers\RouteServiceProvider',
     ),
