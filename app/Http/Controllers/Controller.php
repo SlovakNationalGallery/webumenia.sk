39e9a7d05a28a50529df8b8f399e8f5a04aa8720
Laravel Shift <shift@laravelshift.com>
2016-06-01 08:47:52 -0400

    Various upgrades for Laravel 5.2

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,11 +4,12 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 
-abstract class Controller extends BaseController
+class Controller extends BaseController
 {
 
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
 
     /**
      * Setup the layout used by the controller.
