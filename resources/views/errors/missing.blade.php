2e6ba66c203a1547b84b1121f4cadce0a4faf1c5
Philo van Kemenade <philovankemenade@gmail.com>
2016-07-06 15:07:37 +0200

    catch error in case $item is empty due to unsuccesful harvesting

--- resources/views/errors/missing.blade.php
+++ resources/views/errors/missing.blade.php
@@ -14,11 +14,16 @@ ERROR 404 - Not Found
                     <div class="text-center">
                         <h1>ERROR 404</h1>
                         <h2>STRÁNKU NENAŠLO</h2>
-                        <p>No toto! Na tejto adrese nič nie je, ale môžete si pozrieť napríklad toto dielo:</p>
-                        <a href="{!! $item->getUrl() !!}"><img src="{!! $item->getImagePath() !!}" class="img-responsive img-dielo"></a>
-                        <p>
-                            <a href="{!! $item->getUrl() !!}">{!! implode(', ', $item->authors)!!} - {!! $item->title !!}</a>
-                        </p>
+                        @if ($item)
+                            <p>No toto! Na tejto adrese nič nie je, ale môžete si pozrieť napríklad toto dielo:</p>
+
+                            <a href="{!! $item->getUrl() !!}"><img src="{!! $item->getImagePath() !!}" class="img-responsive img-dielo"></a>
+                            <p>
+                                <a href="{!! $item->getUrl() !!}">{!! implode(', ', $item->authors)!!} - {!! $item->title !!}</a>
+                            </p>
+                        @else
+                            <p>No toto! Na tejto adrese nič nie je.</p>
+                        @endif
 
                         <h3><a href="{!!URL::to('/')!!}">návrat <i class="icon-versus"></i> domov</a></h3>
 
