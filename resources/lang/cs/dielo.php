41657239356a67b795501c817734669ad0748e92
Igor Rjabinin <igor.rjabinin@gmail.com>
2017-05-02 13:20:43 +0200

    add CS translations for item

--- resources/lang/cs/dielo.php
+++ resources/lang/cs/dielo.php
@@ -8,36 +8,35 @@ return array(
     |--------------------------------------------------------------------------
     */
     
-    'item_previous-work' => 'predchádzajúce dielo',
-    'item_next-work'     => 'nasledujúce dielo',
-    'item_order'         => 'objednať reprodukciu',
-    'item_download'      => 'stiahnuť',
+    'item_previous-work' => 'předchozí dílo',
+    'item_next-work'     => 'následující dílo',
+    'item_order'         => 'objednat reprodukci',
+    'item_download'      => 'stáhnout',
     
-    'item_attr_dating'         => 'datovanie',
-    'item_attr_measurements'   => 'rozmer',
+    'item_attr_dating'         => 'datace',
+    'item_attr_measurements'   => 'rozměry',
     'item_attr_work_type'      => 'výtvarný druh',
-    'item_attr_topic'          => 'žáner',
+    'item_attr_topic'          => 'žánr, ale dle našich dat bychom tuto kategorii raději nazvali "námět"',
     'item_attr_tag'            => 'tagy',
-    'item_attr_collections'    => 'kolekcie',
+    'item_attr_collections'    => 'kolekce',
     'item_attr_medium'         => 'materiál',
     'item_attr_technique'      => 'technika',
-    'item_attr_state_edition'  => 'stupeň spracovania',
+    'item_attr_state_edition'  => 'stupeň zpracování',
     'item_attr_integrity'      => 'stupeň integrity',
-    'item_attr_integrity_work' => 'integrita s dielami',
-    'item_attr_inscription'    => 'značenie',
-    'item_attr_gallery'        => 'galéria',
-    'item_attr_identifier'     => 'inventárne číslo',
-    'item_attr_licence'        => 'licencia',
-    'item_attr_place'          => 'geografická oblasť',
+    'item_attr_integrity_work' => 'integrita s díly',
+    'item_attr_inscription'    => 'značení',
+    'item_attr_gallery'        => 'galerie',
+    'item_attr_identifier'     => 'inventární číslo',
+    'item_attr_licence'        => 'licence',
+    'item_attr_place'          => 'geografická oblast',
     
-    'more-items_related-artworks' => 'súvisiace diela',
-
-    'modal_license_body-content'        => '<p><strong>Vami zvolené dielo by sa malo začať v krátkom čase automaticky sťahovať.</strong></p>
-                                            <p>Digitálne reprodukcie diel SNG na tejto stránke sú sprístupnené ako <a href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank" class="underline">verejné vlastníctvo (public domain)</a>. Môžete si ich voľne stiahnuť vo vysokom rozlíšení a využívať na súkromné aj komerné účely &ndash; kopírovať, zdieľať i upravovať.</p>
-                                            <p>Pri ďalšom šírení prosíme uviesť meno autora, názov, majiteľa diela a zdroj <code>:item_url</code></p>
-                                            <p>Ak plánujete využiť reprodukcie na komerčné účely, prosím informujte o vašich plánoch vopred, naši odborníci vám vedia poradiť.</p>  
-                                            <p><a class="underline" href=":free_url">Všetky voľne stiahnuteľné diela nájdete tu.</a></p>',
+    'more-items_related-artworks' =>  'související díla',
+    'modal_license_body-content'        => '<p><strong>Vámi zvolené dílo by se mělo v krátkém čase začít automaticky stahovat.</strong></p>
+                                            <p>Digitální reprodukce děl SNG na této stránce jsou přístupné jako <a href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank" class="underline">verejné vlastníctvo (public domain)</a>. Můžete si je volně stáhnout ve vysokém rozlišení a využívat k soukromým i komerčním účelům &ndash; kopírovat, sdílet i upravovat.</p>
+                                            <p>Při dalším šíření prosíme uvést jméno autora, název, majitele díla a zdroj <code>:item_url</code></p>
+                                            <p>Pokud plánujete využít reprodukce ke komerčním účelům, prosíme informujte o Vašich plánech dopředu, naši odborníci Vám mohou poradit. </p>
+                                            <p><a class="underline" href=":free_url">Všechna díla volná ke stažení najdete zde.</a></p>', 
     'modal_downloadfail_header-content' => 'Nastala chyba',
-    'modal_downloadfail_body-content'   => '<p>Vami zvolené dielo nebolo možné v tomto momente stiahnuť. Skúste to prosím neskôr.</p>
-                                            <p>Pokiaľ problém pretrváva, kontaktujte nás na <a href="mailto:lab@sng.sk">lab@sng.sk</a></p>',
+    'modal_downloadfail_body-content'   => '<p>Vámi zvolené dílo nebylo nyní možné stáhnout. Zkuste to prosím později.</p>  =  
+                                            <p>Pokud problém přetrvává, kontaktujte nás na <a href="mailto:lab@sng.sk">lab@sng.sk</a></p>',
 );
