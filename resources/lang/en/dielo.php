22974137fc48de2f79d8581d0549a8890b70a940
ustengrav <ustengrav@users.noreply.github.com>
2017-05-10 19:18:03 +0200

    translate static content

--- resources/lang/en/dielo.php
+++ resources/lang/en/dielo.php
@@ -10,34 +10,34 @@ return array(
     
     'item_previous-work' => 'previous artwork',
     'item_next-work'     => 'next artwork',
-    'item_order'         => 'order reproductions',
+    'item_order'         => 'order reproduction',
     'item_download'      => 'download',
     
-    'item_attr_dating'         => 'dating',
-    'item_attr_measurements'   => 'rozmer',
-    'item_attr_work_type'      => 'výtvarný druh',
-    'item_attr_topic'          => 'žáner',
-    'item_attr_tag'            => 'tagy',
-    'item_attr_collections'    => 'kolekcie',
-    'item_attr_medium'         => 'materiál',
-    'item_attr_technique'      => 'technika',
-    'item_attr_state_edition'  => 'stupeň spracovania',
-    'item_attr_integrity'      => 'stupeň integrity',
-    'item_attr_integrity_work' => 'integrita s dielami',
-    'item_attr_inscription'    => 'značenie',
-    'item_attr_gallery'        => 'galéria',
-    'item_attr_identifier'     => 'inventárne číslo',
-    'item_attr_licence'        => 'licencia',
-    'item_attr_place'          => 'geografická oblasť',
+    'item_attr_dating'         => 'date',
+    'item_attr_measurements'   => 'dimensions',
+    'item_attr_work_type'      => 'art type',
+    'item_attr_topic'          => 'genre',
+    'item_attr_tag'            => 'tags',
+    'item_attr_collections'    => 'in collections',
+    'item_attr_medium'         => 'material',
+    'item_attr_technique'      => 'technique',
+    'item_attr_state_edition'  => 'stage of processing',
+    'item_attr_integrity'      => 'stage of integrity',
+    'item_attr_integrity_work' => 'in a series with',
+    'item_attr_inscription'    => 'inscription',
+    'item_attr_gallery'        => 'owner',
+    'item_attr_identifier'     => 'inventory number',
+    'item_attr_licence'        => 'licence',
+    'item_attr_place'          => 'geographic location',
     
     'more-items_related-artworks' => 'related artworks',
 
-    'modal_license_body-content'        => '<p><strong>Vami zvolené dielo by sa malo začať v krátkom čase automaticky sťahovať.</strong></p>
-                                            <p>Digitálne reprodukcie diel SNG na tejto stránke sú sprístupnené ako <a href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank" class="underline">verejné vlastníctvo (public domain)</a>. Môžete si ich voľne stiahnuť vo vysokom rozlíšení a využívať na súkromné aj komerné účely &ndash; kopírovať, zdieľať i upravovať.</p>
-                                            <p>Pri ďalšom šírení prosíme uviesť meno autora, názov, majiteľa diela a zdroj <code>:item_url</code></p>
-                                            <p>Ak plánujete využiť reprodukcie na komerčné účely, prosím informujte o vašich plánoch vopred, naši odborníci vám vedia poradiť.</p>  
-                                            <p><a class="underline" href=":free_url">Všetky voľne stiahnuteľné diela nájdete tu.</a></p>',
-    'modal_downloadfail_header-content' => 'Nastala chyba',
-    'modal_downloadfail_body-content'   => '<p>Vami zvolené dielo nebolo možné v tomto momente stiahnuť. Skúste to prosím neskôr.</p>
-                                            <p>Pokiaľ problém pretrváva, kontaktujte nás na <a href="mailto:lab@sng.sk">lab@sng.sk</a></p>',
+    'modal_license_body-content'        => '<p><strong>Your selected artwork should start downloading automatically.</strong></p>
+                                            <p>Downloadable digital reproductions of artworks on Web umenia are available as <a href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank" class="underline">public domain</a>. You can download them in high resolution and use them for both private and commercial use &ndash; copy, share and modify.</p>
+                                            <p>When sharing, please include the artist name, artwork title, owner and the source <code>:item_url</code></p>
+                                            <p>If you intend to use the reproduction for commercial purpose, please inform us in advance, our experts can offer advice.</p>  
+                                            <p><a class="underline" href=":free_url">You can find all downloadable artworks here.</a></p>',
+    'modal_downloadfail_header-content' => 'An error has occured',
+    'modal_downloadfail_body-content'   => '<p>Your selected artwork could not be downloaded at this time. Please try again later.</p>
+                                            <p>If the problem persists, contact us at <a href="mailto:lab@sng.sk">lab@sng.sk</a></p>',
 );
