71452e7f233ef8a9841688196faee6429543217b
ustengrav <ustengrav@users.noreply.github.com>
2017-05-10 19:20:27 +0200

    translate static content

--- resources/lang/en/fatal.php
+++ resources/lang/en/fatal.php
@@ -9,7 +9,7 @@ return array(
     */
 
     'title'         => 'ERROR 500 - Fatal Error',
-    'h2'            => 'Nastala Chyba',
-    'paragraph'     => 'Prepáčte, ale táto stránka je z technických príčin dočasne nedostupná.',
+    'h2'            => 'An error has occured',
+    'paragraph'     => 'This page is currently unavailable due to technical difficulties. We are sorry for the inconvenience',
     'image_caption' => 'Július Koller &ndash; Oznámenie: NedorozUMENIE',
 );
