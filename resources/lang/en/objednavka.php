41ff4e70a5ca1f3abcd5aad99ac83399e25db024
Philo van Kemenade <philovankemenade@gmail.com>
2017-05-22 17:04:35 +0200

    improve translations in EN objednavka

--- resources/lang/en/objednavka.php
+++ resources/lang/en/objednavka.php
@@ -11,7 +11,10 @@ return array(
     'title'         => 'Reproduction order',
     'order_alert'   => '<strong>Warning:</strong> Orders completed after December 6th will not be realized before Christmas.<br>You can pick up your printed reproductions in Ex Libris SNG bookstore until December 23rd or after December 27th.',
     'order_content' => '<h2 class="bottom-space">Reproduction order</h2>
-                        <p>We offer high quality printed or digital reproductions of selected artworks from the collections of Slovak National Gallery. We provide digital reproductions of artworks only for special purposes (educational, scientific, promotional, private), which can be specified in an order form below. After selecting the artworks you want, please select either printed or digital format. If you want both printed and digital reproductions, please create a separate order for each. You will recieve an e-mail with more detailed information after placing the order. Printed reproductions can only be picked-up personally at our delivery points: <a href="http://www.sng.sk/en/bratislava/visiting-us/how-to-find-us/ex-libris-bookshop" target="_blank" class="underline">Ex Libris Bookshop in the Esterházy Palace, Námestie Ľudovíta Štúra 4, Bratislava</a>  or at the cash desk in <a href="http://www.sng.sk/en/zvolen/visiting-us/how-to-find-us" target="_blank" class="underline">Castle Zvolen, SNP Square 594/1, Zvolen</a>.</p>
+                        <p class="lead">We offer high quality printed or digital reproductions of selected artworks from the collections of the Slovak National Gallery.</p>
+                        <p>After selecting the artworks you want, please select either printed or digital format. If you want both printed and digital reproductions, please create a separate order for each. You will recieve an e-mail with more detailed information after placing the order. </p>
+                        <p>Printed reproductions can only be picked up at one of our pick up locations: <a href="http://www.sng.sk/en/bratislava/visiting-us/how-to-find-us/ex-libris-bookshop" target="_blank" class="underline">Ex Libris Bookshop in the Esterházy Palace, Námestie Ľudovíta Štúra 4, Bratislava</a>  or at the cash desk in <a href="http://www.sng.sk/en/zvolen/visiting-us/how-to-find-us" target="_blank" class="underline">Castle Zvolen, SNP Square 594/1, Zvolen</a>.</p>
+                        <p>We provide digital reproductions of <abbr title="less than 70 years has followed the death of the artist" data-toggle="tooltip">copyright protected artworks</abbr> only for special purposes (educational, scientific, promotional, private), which can be specified in the order form below.</p>
                         <div class="alert alert-info  top-space" role="alert">
                             For technical reasons, the orders of printed reproductions with mount and frame are temporarily disabled. <strong>You can still order printed reproductions without mount or frame</strong>. We apologize for the inconvenience.
                         </div>',
@@ -30,11 +33,10 @@ return array(
     'form_format_a3'               => 'A3+ size (35 eur/piece)',
     'form_format_for-download'     => 'For download:',
     'form_format_digital'          => 'Digital reproduction',
-    'form_purpose-alert'           => 'The copyright law prevents us from providing digital reproductions of <abbr title="70 years did not pass since the death of the artist" data-toggle="tooltip">copyright protected artworks</abbr> for general private purposes (decoration, gift etc.). Slovak National Gallery will create a written contract, according to the information provided in the order. This contract will limit the use of the reproduction just for the purpose stated in the order.<br>
-        <strong>If you are interested in a printed artwork, please consider the printed reproduction option, where the quality is guaranteed by the Slovak National Gallery</strong>',
+    'form_purpose-alert'           => 'The copyright law prevents us from providing digital reproductions of <abbr title="less than 70 years has followed the death of the artist" data-toggle="tooltip">copyright protected artworks</abbr> for general private purposes (decoration, gift etc.). Slovak National Gallery will create a written contract, according to the information provided in the order. This contract will limit the use of the reproduction just for the purpose stated in the order.',
     'form_purpose-label'           => 'Purpose',
     'form_purpose-info'            => 'Purpose description',
-    'form_delivery-point'          => 'Delivery point',
+    'form_delivery-point'          => 'Pick up location',
     'form_delivery-point_exlibris' => 'Ex Libris Bookshop, SNG, Bratislava',
     'form_delivery-point_zvolen'   => 'Zvolen castle, Zvolen',
     'form_note'                    => 'Note (Comment)',
