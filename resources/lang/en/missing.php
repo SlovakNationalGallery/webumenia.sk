589018b448bf7b4f869dd14ef4abbf60099d5ccc
Philo van Kemenade <philovankemenade@gmail.com>
2017-05-22 17:04:46 +0200

    improve translations in EN missing

--- resources/lang/en/missing.php
+++ resources/lang/en/missing.php
@@ -11,6 +11,6 @@ return array(
     'title'            => 'ERROR 404 - Not Found',
     'h1'               => 'ERROR 404',
     'h2'               => 'The page was not found',
-    'paragraph_item'   => 'Oh no! This address does not exist, but you can enjoy this random artwork:',
+    'paragraph_item'   => 'Oh no! This address does not exist, but you might enjoy this random artwork:',
     'paragraph_noitem' => 'Oh no! This address does not exist.',
 );
