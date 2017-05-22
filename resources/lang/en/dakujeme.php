905a9ae3a1a4de34379f86047967160e2e89d28a
Philo van Kemenade <philovankemenade@gmail.com>
2017-05-22 17:06:23 +0200

    improve translations in EN dakujeme

--- resources/lang/en/dakujeme.php
+++ resources/lang/en/dakujeme.php
@@ -9,6 +9,6 @@ return array(
     */
 
     'title'     => 'Thank you!',
-    'paragraph' => 'Your reproduction order was accepted. An automatic e-mail with detailed information was sent to the address you provided. We wish you a nice day!',
-    'button'    => 'Return to the main page',
+    'paragraph' => 'Your reproduction order has been received. An automatic e-mail with detailed information was sent to the address you provided. Have a nice day!',
+    'button'    => 'Return to home page',
 );
