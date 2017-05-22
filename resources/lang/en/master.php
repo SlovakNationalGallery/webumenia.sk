39b82178ab8a81209f1f83e56d3ef7d57d14a839
Philo van Kemenade <philovankemenade@gmail.com>
2017-05-22 11:17:32 +0200

    change authors to artists in EN master.php

--- resources/lang/en/master.php
+++ resources/lang/en/master.php
@@ -11,13 +11,13 @@ return array(
     'meta_description'   => 'Web umenia is an on-line catalog of artworks from collections of Slovak galleries. You can find here basic information about artworks and artists as well as articles, thematic collections and videos.',
     'artworks'           => 'artworks',
     'collections'        => 'collections',
-    'authors'            => 'authors',
+    'authors'            => 'artists',
     'articles'           => 'articles',
     'galleries'          => 'galleries',
     'info'               => 'info',
     'language'           => 'language',
     'made_by'            => 'made by',
-    'search_placeholder' => 'Search for artworks, authors...',
+    'search_placeholder' => 'Search for artworks, artists...',
     'to_top'             => 'back to top',
 
 );
