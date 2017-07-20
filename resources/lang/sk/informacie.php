202315b53d102fc22405fc4dd88492568d7f09e0
ustengrav <ustengrav@users.noreply.github.com>
2017-07-20 17:10:08 +0200

    correct some errors in slovak version of informacie.php

--- resources/lang/sk/informacie.php
+++ resources/lang/sk/informacie.php
@@ -14,7 +14,7 @@ return array(
 
     'info_heading'       => 'čo je web umenia?',
     'info_p_lead'        => 'Web umenia je <strong>on-line katalóg</strong> výtvarných diel zo zbierok <strong>slovenských galérií</strong> evidovaných v&nbsp;<a class="unstyled" href="http://www.sng.sk/sk/zbierky/centralny-katalog-diel" target="_blank">Centrálnej evidencii diel výtvarného umenia</a>.',
-    'info_p'             => 'Nájdete tu základné informácie o dielach a ich autoroch, ale aj pôvodné články, videá a kolekcie.Údaje a digitálne reprodukcie sú preberané a pravidelne aktualizované zo systému CEDVU (Centrálna evidencia diel výtvarného umenia), kam ich vkladajú zamestnanci jednotlivých galérií.',
+    'info_p'             => 'Nájdete tu základné informácie o dielach a ich autoroch, ale aj pôvodné články, videá a kolekcie. Údaje a digitálne reprodukcie sú preberané a pravidelne aktualizované zo systému CEDVU (Centrálna evidencia diel výtvarného umenia), kam ich vkladajú zamestnanci jednotlivých galérií.',
     'info_gallery_SNG'   => 'Slovenská národná galéria',
     'info_gallery_OGD'   => 'Oravská galéria v Dolnom Kubíne',
     'info_gallery_GNZ'   => 'Galéria umenia Ernesta Zmetáka v Nových Zámkoch',
@@ -33,16 +33,16 @@ return array(
     'mozete_col_vyuzivat_li_2'     => '<strong>vlastná tvorba</strong> (digitálna úprava reprodukcie, využitie jej časti pre animáciu alebo koláž)',
     'mozete_col_vyuzivat_li_3'     => '<strong>vzdelávanie</strong> (vloženie obrázku na vlastnú webstránku, použitie na Wikipedii či ako súčasť prezentácie)    ',
     'mozete_col_objednat_heading'  => 'Objednať si kvalitné reprodukcie',
-    'mozete_col_objednat_p'        => 'K vybraným dielam zo zbierok SNG ponúkame možnosť objednať si reprodukcie v archívnej kvalite na fineartových papieroch. Po výbere diela, vyplnení údajov a odoslaní objednávky vás bude kontaktovať pracovník SNG s podrobnejšími informáciami. Momentálne je možné vyzdvihnúť si diela len osobne v kníhkupectve <a href="http://www.sng.sk/sk/uvod/navsteva-sng/sluzby/knihkupectvo-ex-libris" target="blank" class="strong">Ex&nbsp;Libris v priestoroch SNG</a> na <a href="https://goo.gl/maps/k0sBz" target="_blank" class="strong">Námestí. Štúra 4 v Bratislave</a> alebo v pokladni <a href="http://www.sng.sk/sk/uvod/vysunute-pracoviska/zvolen" target="blank" class="strong">Zvolenského zámku</a> - <a href="https://goo.gl/maps/MPRy6Qdwm8s" target="_blank" target="blank" class="strong">Námestie SNP 594/1</a>.',
+    'mozete_col_objednat_p'        => 'K vybraným dielam zo zbierok SNG ponúkame možnosť objednať si reprodukcie v archívnej kvalite na fineartových papieroch. Po výbere diela, vyplnení údajov a odoslaní objednávky vás bude kontaktovať pracovník SNG s podrobnejšími informáciami. Momentálne je možné vyzdvihnúť si diela len osobne v kníhkupectve <a href="http://www.sng.sk/sk/uvod/navsteva-sng/sluzby/knihkupectvo-ex-libris" target="blank" class="strong">Ex&nbsp;Libris v priestoroch SNG</a> na <a href="https://goo.gl/maps/k0sBz" target="_blank" class="strong">Námestí Ľ. Štúra 4 v Bratislave</a> alebo v pokladni <a href="http://www.sng.sk/sk/uvod/vysunute-pracoviska/zvolen" target="blank" class="strong">Zvolenského zámku</a> - <a href="https://goo.gl/maps/MPRy6Qdwm8s" target="_blank" target="blank" class="strong">Námestie SNP 594/1</a>.',
     'mozete_free_artworks'         => 'voľné diela na stiahnutie',
     
     'more-items_heading'                 => 'vyberte si reprodukcie',
     'more-items_button'                  => 'zobraziť všetkých',
     'more-items_connect_heading'         => 'spojte sa s nami',
     'more-items_connect_col1_lead'       => 'zverejňujeme výtvarné diela <br>zo zbierok <strong>slovenských galérií</strong>.',
-    'more-items_connect_col1_p'          => 'podmienkou pre zverejnenie diel a ďalšich autorov na Web umenia je ich zastúpenie v zbierkach registrovaných galérií. Diela zo súkromných zbierok a iných zdrojov aktuálne nezverejňujeme. '  ,
+    'more-items_connect_col1_p'          => 'podmienkou pre zverejnenie diel a ďalších autorov na Web umenia je ich zastúpenie v zbierkach registrovaných galérií. Diela zo súkromných zbierok a iných zdrojov aktuálne nezverejňujeme. '  ,
     'more-items_connect_col2_lead'       => 'sme otvorení <strong>ďalším <br>formám&nbsp;spolupráce</strong>.',
-    'more-items_connect_col2_p'          => 'sme však otvorení iným formám spolupráce, čoskoro zverejníme časť výtvarných diel ako otvorené dáta. Ak viete napríklad o niektorom z autorov viac informácií, alebo ste na jeho profile u nás nenašli fotografiu, budeme radi ak nám pomôžete pri ich doplnení. Napíšte nám na <a href="mailto:lab@sng.sk">lab@sng.sk</a>, spojíme sa s vami.',
+    'more-items_connect_col2_p'          => 'Ak viete o niektorom z diel alebo autorov viac informácií, alebo ste na jeho profile u nás nenašli fotografiu, budeme radi ak nám pomôžete pri ich doplnení. Napíšte nám na <a href="mailto:lab@sng.sk">lab@sng.sk</a>, spojíme sa s vami.',
     'more-items_connect_col3_lead'       => 'web umenia realizuje <br>kolektív <strong><a href="http://lab.sng.sk/" target="_blank" class="strong">lab.SNG</a></strong>',
     'more-items_connect_col3_ul-content' => '<li><strong>Michal Čudrnák</strong> vedúci lab.SNG, autor článkov</li>
                                              <li><strong>Lukáš Štepanovský</strong> editor obsahu, autor článkov</li>
