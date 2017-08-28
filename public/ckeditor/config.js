bd2eec135114cf5562263a80557bf733ea1afff7
Igor Rjabinin <igor.rjabinin@gmail.com>
2017-08-28 14:17:43 +0200

    WEBUMENIA-241 add embed to ckeditor

--- public/ckeditor/config.js
+++ public/ckeditor/config.js
@@ -1,6 +1,6 @@
 CKEDITOR.editorConfig = function( config ) {
 	config.toolbarGroups = [
-		{ name: 'clipboard', groups: [ 'clipboard' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
 		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
 		{ name: 'links', groups: [ 'links' ] },
 		{ name: 'insert', groups: [ 'insert' ] },
@@ -11,8 +11,21 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
 		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
 		{ name: 'styles', groups: [ 'styles' ] },
-		{ name: 'colors', groups: [ 'colors' ] }
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'about', groups: [ 'about' ] }
 	];
 
-	config.removeButtons = 'Underline,Subscript,Superscript,Cut,Copy';
+	// Remove some buttons provided by the standard plugins, which are
+	// not needed in the Standard(s) toolbar.
+	config.removeButtons = 'Underline,Subscript,Superscript,Cut,Copy,Undo,Redo,CodeSnippet,Strike,About';
+
+	// Set the most common block elements.
+	config.format_tags = 'p;h1;h2;h3;pre';
+
+	// Simplify the dialog windows.
+	// config.removeDialogTabs = 'image:advanced;link:advanced';
+
+	// https://docs.ckeditor.com/#!/guide/dev_media_embed-section-configuring-the-content-provider
+	config.embed_provider = '//ckeditor.iframe.ly/api/oembed?url={url}&callback={callback}';
+
 };
