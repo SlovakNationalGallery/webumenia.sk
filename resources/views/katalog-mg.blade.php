11c378fe6c61788f7236f700b6c1f80f669a42b1
Igor Rjabinin <igor.rjabinin@gmail.com>
2017-09-27 23:52:43 +0200

    remove rounded corders from "load more" button in catalog  [WEBUMENIA-506]

--- resources/views/katalog-mg.blade.php
+++ resources/views/katalog-mg.blade.php
@@ -143,8 +143,7 @@
                     <div class="col-sm-12 text-center">
                         {!! $paginator->appends(@Input::except('page'))->render() !!}
                         @if ($paginator->hasMorePages() )
-                            <a id="next" href="{!! URL::to('katalog')!!}"><svg xmlns="http://www.w3.org/2000/svg" width="100px" height="100px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve"> <path d="M0.492,8.459v83.427c4.124,0.212,7.409,3.497,7.622,7.622h83.357
-        c0.22-4.265,3.719-7.664,8.036-7.664V8.571c-4.46,0-8.079-3.617-8.079-8.079H8.157C8.157,4.774,4.755,8.239,0.492,8.459z"/>
+                            <a id="next" href="{!! URL::to('katalog')!!}"><svg xmlns="http://www.w3.org/2000/svg" width="100px" height="100px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve"> <path d="M0.5,0.2v99.5h99V0.2H0.5z"/>
 <text text-anchor="middle" alignment-baseline="middle" x="50" y="50">
     {{ trans('katalog.catalog_show_more') }}
   </text>
