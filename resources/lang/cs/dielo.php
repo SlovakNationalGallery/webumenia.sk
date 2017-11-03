e25df2cdb573fdc508901ba1ade62c45b230cdbf
Igor Rjabinin <igor.rjabinin@gmail.com>
2017-11-03 17:07:35 +0100

    WEBUMENIA-531 adjust text for downloading free artworks

--- resources/lang/cs/dielo.php
+++ resources/lang/cs/dielo.php
@@ -7,12 +7,12 @@ return array(
     | Language Lines for dielo.blade.php template
     |--------------------------------------------------------------------------
     */
-    
+
     'item_previous-work' => 'předchozí dílo',
     'item_next-work'     => 'následující dílo',
     'item_order'         => 'objednat reprodukci',
     'item_download'      => 'stáhnout',
-    
+
     'item_attr_dating'         => 'datace',
     'item_attr_measurements'   => 'rozměry',
     'item_attr_work_type'      => 'výtvarný druh',
@@ -29,14 +29,13 @@ return array(
     'item_attr_identifier'     => 'inventární číslo',
     'item_attr_licence'        => 'licence',
     'item_attr_place'          => 'místo vzniku',
-    
+
     'more-items_related-artworks' =>  'související díla',
     'modal_license_body-content'        => '<p><strong>Vámi zvolené dílo by se mělo v krátkém čase začít automaticky stahovat.</strong></p>
-                                            <p>Digitální reprodukce volných děl na této stránce jsou přístupné jako <a href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank" class="underline">verejné vlastníctvo (public domain)</a>. Můžete si je volně stáhnout ve vysokém rozlišení a využívat k soukromým i komerčním účelům &ndash; kopírovat, sdílet i upravovat.</p>
+                                            <p>Digitální reprodukce volných děl na této stránce jsou přístupné jako <a href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank" class="underline">verejné vlastníctvo (public domain)</a>. Můžete si je volně stahovat ve vysokém rozlišení pouze pro soukromé užití.</p>
                                             <p>Při dalším šíření prosíme uvést jméno autora, název, majitele díla a zdroj <code>:item_url</code></p>
-                                            <p>Pokud plánujete využít reprodukce ke komerčním účelům, prosíme informujte o Vašich plánech dopředu, naši odborníci Vám mohou poradit. </p>
-                                            <p><a class="underline" href=":free_url">Všechna díla volná ke stažení najdete zde.</a></p>', 
+                                            <p><a class="underline" href=":free_url">Všechna díla volná ke stažení najdete zde.</a></p>',
     'modal_downloadfail_header-content' => 'Nastala chyba',
-    'modal_downloadfail_body-content'   => '<p>Vámi zvolené dílo nebylo nyní možné stáhnout. Zkuste to prosím později.</p>  =  
+    'modal_downloadfail_body-content'   => '<p>Vámi zvolené dílo nebylo nyní možné stáhnout. Zkuste to prosím později.</p>  =
                                             <p>Pokud problém přetrvává, kontaktujte nás na <a href="mailto:lab@sng.sk">lab@sng.sk</a></p>',
 );
