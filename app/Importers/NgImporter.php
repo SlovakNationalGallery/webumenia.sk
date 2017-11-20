18eb5b7b9ca463aa4bd6977df7a6c126f3bbcbda
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2017-11-20 19:26:07 +0100

    Add NG importer

---
+++ app/Importers/NgImporter.php
@@ -0,0 +1,170 @@
+<?php
+
+
+namespace App\Importers;
+
+
+use App\Collection;
+use App\Repositories\IFileRepository;
+
+class NgImporter extends AbstractImporter {
+
+    protected $options = [
+        'delimiter' => ';',
+        'enclosure' => '"',
+        'escape' => '\\',
+        'newline' => "\r\n",
+        'input_encoding' => 'CP1250',
+    ];
+
+    protected $mapping = [
+        'Název díla' => 'title',
+        'z cyklu' => 'related_work',
+        'Datace' => 'dating',
+        'Technika' => 'technique',
+        'Materiál' => 'medium',
+        'Inventární čís.' => 'identifier',
+        'Popis' => 'description',
+    ];
+
+    protected $defaults = [
+        'work_type' => '',
+        'topic' => '',
+        'relationship_type' => '',
+        'place' => '',
+        'gallery' => '',
+        'description' => '',
+        'title' => '',
+    ];
+
+    protected static $cz_gallery_collection_spec = [
+        'SGK' => 'Sbírka grafiky a kresby',
+        'SMSU' => 'Sbírka moderního a současného umění',
+        'SUDS' => 'Sbírka umění 19. století',
+        'SSU' => 'Sbírka starého umění',
+        'SAA' => 'Sbírka umění Asie a Afriky',
+    ];
+
+    protected static $name = 'ng';
+
+    public function __construct(IFileRepository $repository) {
+        parent::__construct($repository);
+
+        $this->sanitizers[] = function($value) {
+            return empty_to_null($value);
+        };
+    }
+
+
+    protected function getItemId(array $record) {
+        $key = 'Inventární čís.';
+        $id = $record[$key];
+        $id = strtr($id, ' ', '_');
+
+        return sprintf('CZE:NG.%s', $id);
+    }
+
+    protected function getItemImageFilename(array $record) {
+        $key = 'Inventární čís.';
+        $filename = $record[$key];
+        $filename = strtr($filename, ' ', '_');
+
+        return $filename;
+    }
+
+    protected function getItemIipImageUrl($csv_filename, $image_filename) {
+        return sprintf(
+            '/NG/jp2/%s.jp2',
+            $image_filename
+        );
+    }
+
+    protected function createItem(array $record) {
+        $item = parent::createItem($record);
+
+        $collection = Collection::where('name', $record['Kolekce (budova)'])->first();
+        if ($collection) {
+            $item->collections()->sync([$collection->id]);
+        }
+
+        return $item;
+    }
+
+
+    protected function hydrateAuthor(array $record) {
+        $keys = [
+            'Autor (jméno příjmení, příp. Anonym)',
+            'Autor 2',
+            'Autor 3',
+        ];
+
+        $keys = array_filter($keys, function ($key) use ($record) {
+            return $record[$key] !== null;
+        });
+
+        $authors = array_filter(
+            array_map(function($key) use ($record) {
+                return self::bracketAuthor($record[$key]);
+            }, $keys)
+        );
+
+        return implode('; ', $authors);
+    }
+
+    protected function hydrateInscription(array $record) {
+        $key1 = 'značeno kde (umístění v díle)';
+        $key2 = 'Značeno (jak např. letopočet, signatura, monogram)';
+
+        return sprintf('%s: %s', $record[$key1], $record[$key2]);
+    }
+
+    protected function hydrateMeasurement(array $record) {
+        $measurement = [];
+
+        $width = 'šířka';
+        $height = 'výška';
+        $depth = 'hloubka';
+        $units = 'jednotky';
+
+        $units_suffix = $record[$units] !== null ? sprintf(' %s', $record[$units]) : '';
+        if ($record[$width] !== null) {
+            $measurement[] = sprintf('šířka %s%s', $record[$width], $units_suffix);
+        }
+        if ($record[$height] !== null) {
+            $measurement[] = sprintf('výška %s%s', $record[$height], $units_suffix);
+        }
+        if ($record[$depth] !== null) {
+            $measurement[] = sprintf('hloubka %s%s', $record[$depth], $units_suffix);
+        }
+
+        return implode('; ', $measurement);
+    }
+
+    protected function hydrateDateEarliest(array $record) {
+        if (preg_match_all('/(\d{4})/', $record['Datování (určené)'], $matches)) {
+            if (isset($matches[1][0])) {
+                return $matches[1][0];
+            }
+        }
+
+        return 0;
+    }
+
+    protected function hydrateDateLatest(array $record) {
+        if (preg_match_all('/(\d{4})/', $record['Datování (určené)'], $matches)) {
+            if (isset($matches[1][count($matches) - 1])) {
+                return $matches[1][count($matches) - 1];
+            }
+        }
+
+        return 0;
+    }
+
+    /**
+     * @param string $author
+     * @return string
+     */
+    protected static function bracketAuthor($author) {
+        return preg_replace('/,\s*(.*)/', ' ($1)', $author);
+    }
+}
---
+++ tests/NgImporterTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests;
+
+use App\Collection;
+use App\Import;
+use App\Importers\NgImporter;
+use App\Repositories\CsvRepository;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class NgImporterTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testImport() {
+        $collection = new Collection();
+        $collection->name = 'Veletržní palác';
+        $collection->type = 'type';
+        $collection->order = 1;
+        $collection->save();
+
+        $data = [
+            'Inventární čís.' => 'Vm 1168',
+            'Značeno (jak např. letopočet, signatura, monogram)' => 'Podpis: Namaloval Čchi Chuang. Pečeť: Starý Paj.',
+            'značeno kde (umístění v díle)' => 'vlevo nahoře',
+            'Autor (jméno příjmení, příp. Anonym)' => 'Prezývka, vlastným menom Meno',
+            'Autor 2' => 'Ďalší autor, autorka',
+            'Autor 3' => 'Tretí autor',
+            'Čistý rozměr (bez rámu, pasparty apod)' => '',
+            'šířka' => '1',
+            'výška' => '2',
+            'hloubka' => '3',
+            'jednotky' => 'cm',
+            'šířka_0' => '4',
+            'výška_0' => '5',
+            'hloubka_0' => '6',
+            'jednotky_0' => 'm',
+            'Rozměr 2' => 'druhý rozmer',
+            'popis rozměru (např. s rámem, se soklem, celý papír apod.)' => 'celkovo',
+            'Datování (určené)' => '1998 - 2003',
+            'Kolekce (budova)' => 'Veletržní palác',
+        ];
+        $records = new \ArrayIterator([$data]);
+
+        $repositoryMock = $this->getMock(CsvRepository::class);
+        $repositoryMock->method('getFiltered')->willReturn($records);
+
+        $this->importer = new NgImporter($repositoryMock);
+
+        $importMock = $this->getMock(Import::class, ['getAttribute']);
+        $importMock->method('getAttribute')->willReturn(1);
+        $file = ['basename' => '', 'path' => ''];
+
+        $items = $this->importer->import($importMock, $file);
+
+        $this->assertCount(1, $items);
+
+        $expected = [
+            'id' => 'CZE:NG.Vm_1168',
+            'identifier' => 'Vm 1168',
+            'inscription' => 'vlevo nahoře: Podpis: Namaloval Čchi Chuang. Pečeť: Starý Paj.',
+            'author' => 'Prezývka (vlastným menom Meno); Ďalší autor (autorka); Tretí autor',
+            'measurement' => 'šířka 1 cm; výška 2 cm; hloubka 3 cm',
+            'date_earliest' => 1998,
+            'date_latest' => 2003,
+        ];
+
+        foreach ($expected as $key => $value) {
+            $this->assertEquals($value, $items[0]->$key);
+        }
+
+        $this->assertCount(1, $items[0]->collections);
+        $this->assertEquals($collection->name, $items[0]->collections[0]->name);
+    }
+}
