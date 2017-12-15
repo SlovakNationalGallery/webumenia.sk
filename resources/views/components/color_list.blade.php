816f6749207e36dffec1c3f0b56d5e9eeb7b607c
Philo van Kemenade <philovankemenade@gmail.com>
2017-12-15 16:56:52 +0100

    set min-width of colorlist-items via flexbox

--- resources/views/components/color_list.blade.php
+++ resources/views/components/color_list.blade.php
@@ -2,7 +2,7 @@
   @foreach ($colors as $color)
     <div 
       class="colorlist-item" 
-      style="background-color: {{$color["hex"]}}; width: calc({{$color["amount"]}} - 4px)"
+      style="background-color: {{$color["hex"]}}; width: {{$color["amount"]}}"
       title="{{$color["hex"]}}"></div>
   @endforeach
 </div>
