ca8e458e6fa582b0179543149d02ca0b2975601d
Igor Rjabinin <igor.rjabinin@gmail.com>
2018-05-16 23:17:43 +0200

    fix merge for kolekcie.blade.php

--- resources/views/kolekcie.blade.php
+++ resources/views/kolekcie.blade.php
@@ -25,48 +25,6 @@
     @endif
 @endforeach
 
-{{-- <section class="filters">
-    <div class="container content-section">
-        @if (empty($cc))
-        {!! Form::open(array('id'=>'filter', 'method' => 'get')) !!}
-        {!! Form::hidden('search', @$search) !!}
-        <div class="row">
-            <!-- <h3>Filter: </h3> -->
-            <div  class="col-md-4 col-xs-6">
-                    {!! Form::select('role', array('' => '') + $roles,  @$input['role'], array('class'=> 'chosen-select form-control', 'data-placeholder' => 'Rola')) !!}
-            </div>
-            <div  class="col-md-4 col-xs-6">
-                    {!! Form::select('nationality', array('' => '') + $nationalities, @$input['nationality'], array('class'=> 'chosen-select form-control', 'data-placeholder' => 'Príslušnosť')) !!}
-            </div>
-            <div  class="col-md-4 col-xs-6">
-                    {!! Form::select('place', array('' => '') + $places,  @$input['place'], array('class'=> 'chosen-select form-control', 'data-placeholder' => 'Miesto')) !!}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-1 text-right year-range">
-                    <b class="sans" id="from_year">{!! !empty($input['year-range']) ? reset((explode(',', $input['year-range']))) : App\Collection::sliderMin() !!}</b>
-            </div>
-            <div class="col-sm-10 year-range">
-                    <input id="year-range" name="year-range" type="text" class="span2" data-slider-min="{!! App\Collection::sliderMin() !!}" data-slider-max="{!! App\Collection::sliderMax() !!}" data-slider-step="5" data-slider-value="[{!! !empty($input['year-range']) ? $input['year-range'] : App\Collection::sliderMin().','.App\Collection::sliderMax() !!}]"/>
-            </div>
-            <div class="col-sm-1 text-left year-range">
-                    <b class="sans" id="until_year">{!! !empty($input['year-range']) ? end((explode(',', $input['year-range']))) : App\Collection::sliderMax() !!}</b>
-            </div>
-        </div>
-        <div class="row" style="padding-top: 20px;">
-            <div  class="col-sm-12 text-center alphabet sans">
-                @foreach (range('A', 'Z') as $char)
-                    <a href="{!! url_to('kolekcie', ['first-letter' => $char]) !!}" class="{!! (Input::get('first-letter')==$char) ? 'active' : '' !!}" rel="{!! $char !!}">{!! $char !!}</a> &nbsp;
-                @endforeach
-                {!! Form::hidden('first-letter', @$input['first-letter'], ['id'=>'first-letter']) !!}
-                {!! Form::hidden('sort_by', @$input['sort_by'], ['id'=>'sort_by']) !!}
-            </div>
-        </div>
-         {!! Form::close() !!}
-         @endif
-    </div>
-</section> --}}
-
 {!! Form::open(array('id'=>'filter', 'method' => 'get')) !!}
     {!! Form::hidden('sort_by', @$input['sort_by'], ['id'=>'sort_by']) !!}
 {!! Form::close() !!}
@@ -77,7 +35,7 @@
                 @if (!empty($search))
                     <h4 class="inline">{{ utrans('kolekcie.collections_found_collections') }} &bdquo;{!! $search !!}&ldquo; (<span>{!! $collections->total() !!}</span>) </h4>
                 @else
-            		<h4 class="inline">{!! $collections->total() !!} {{ trans('kolekcie.collections_collections') }}</h4>
+            		<h4 class="inline">{!! $collections->total() !!} {{ trans_choice('kolekcie.collections_collections', $collections->total(), ['collections_count' => $collections->total()] )}}</h4>
                 @endif
                 @if ($collections->count() == 0)
                     <p class="text-center">{{ utrans('kolekcie.collections_no_results') }}</p>
@@ -117,10 +75,7 @@
                     </a>
                 </div>
                 <div class="collection-meta grey">
-                   {{--  {!! $collection->author !!} &nbsp;&middot;&nbsp; --}}
-                    {!! $collection->created_at->format('d. m. Y') !!} &nbsp;&middot;&nbsp;
-                    {!! $collection->user->name !!} &nbsp;&middot;&nbsp;
-                    {!! $collection->items->count() !!} {{ trans('kolekcie.collections_artworks') }}
+                   {{ trans_choice('general.artworks_counted', $collection->items()->count(), ['artworks_count' => $collection->items->count()]) }}
                 </div>
                 <div>
                     {!! $collection->getShortTextAttribute($collection->text, 350) !!}
