38fcd58525eeecde0570b4e3e7f2c55d077d6400
Igor Rjabinin <igor.rjabinin@gmail.com>
2018-05-25 16:20:49 +0200

    WEBUMENIA-705 add "GDPR checkbox" to reproduction order screen

--- resources/lang/sk/objednavka.php
+++ resources/lang/sk/objednavka.php
@@ -42,6 +42,7 @@ return array(
     'form_delivery-point_exlibris' => 'Kníhkupectvo Ex Libris v SNG',
     'form_delivery-point_zvolen'   => 'Zvolenský zámok',
     'form_note'                    => 'Poznámka',
+    'form_terms_and_conditions'    => 'súhlasím so <a href="http://www.sng.sk/sk/o-galerii/dokumenty/gdpr" class="underline" target="_blank">spracovaním osobných údajov</a> na účel vybavenia objednávky',
     'form_order'                   => 'Objednať',
 
     // in routes.php
