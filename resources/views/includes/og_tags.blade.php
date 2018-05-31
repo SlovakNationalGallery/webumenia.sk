778be3f943c733e873ba6c068545a358cbbb5e88
Igor Rjabinin <igor.rjabinin@gmail.com>
2018-05-31 11:04:35 +0200

    remove og:image

--- resources/views/includes/og_tags.blade.php
+++ resources/views/includes/og_tags.blade.php
@@ -6,6 +6,5 @@
 <meta property="og:description" content="{{ trans('master.meta_description') }}" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="{!! Request::url() !!}" />
-<meta property="og:image" content="{!! URL::to('/images/og-image-'.random_int(1, 2).'.jpg') !!}" />
 <meta property="og:site_name" content="Národní galerie v Praze - sbírky" />
 @show
