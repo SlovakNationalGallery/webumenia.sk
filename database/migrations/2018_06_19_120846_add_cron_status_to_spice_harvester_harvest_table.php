1b42f99058d0d9dab6dcde9342ce395ac0c584e0
Endut <adam.a.juraszek@googlemail.com>
2018-07-03 12:18:43 +0200

    add cron_status column to harvests table, add scheduled tasks for daily / weekly harvests TODO: clean up query logic from Kernel and move harvests to job

---
+++ database/migrations/2018_06_19_120846_add_cron_status_to_spice_harvester_harvest_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCronAttribute extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('spice_harvester_harvests', function (Blueprint $table) {
+            $table->enum('cron_status', array('manual','daily','weekly'))->default('manual');
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('spice_harvester_harvests', function (Blueprint $table) {
+            $table->dropColumn('status');
+            //
+        });
+    }
+}
