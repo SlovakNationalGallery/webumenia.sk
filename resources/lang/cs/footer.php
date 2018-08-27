50c1ae80c648e56bfef53c0a4d4b693a3c0d42ba
Igor Rjabinin <igor.rjabinin@gmail.com>
2018-08-27 15:38:00 +0200

    [footer] add newsletter link into footer + fix responsive issues on small screen + add translations (sk/en/cs)

---
+++ resources/lang/cs/footer.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Language Lines for footer.blade.php template
+    |--------------------------------------------------------------------------
+    */
+
+    'made_by'            => 'vyrobil a spravuje',
+    'subscribe'            => 'přihlašte se k odběru',
+    'newsletter'            => 'newsletter-u',
+
+);
---
+++ resources/lang/sk/footer.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Language Lines for footer.blade.php template
+    |--------------------------------------------------------------------------
+    */
+
+    'made_by'            => 'vyrobil a spravuje',
+    'subscribe'            => 'prihláste sa na odber',
+    'newsletter'            => 'newsletter-u',
+
+);
