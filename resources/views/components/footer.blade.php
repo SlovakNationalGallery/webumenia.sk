755c55a376d1dadd605d7799682d3ec44b122aa8
Igor Rjabinin <igor.rjabinin@gmail.com>
2018-09-04 10:00:49 +0200

    reuse already existing classes

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="footer">
   <div class="container">
     <div class="row">
-      <div class="col-sm-6 text-center text-sm-left space">
+      <div class="col-sm-6 text-center text-sm-left top-space bottom-space">
         <a href="https://www.facebook.com/webumenia.sk" target="_blank" data-toggle="tooltip" title="facebook"><i class="fa fa-facebook fa-lg"></i></a>
         <a href="http://webumenia.tumblr.com/" target="_blank" data-toggle="tooltip" title="tumblr"><i class="fa fa-tumblr fa-lg"></i></a>
         <a href="https://vimeo.com/webumeniask" target="_blank" data-toggle="tooltip" title="vimeo"><i class="fa fa-vimeo-square fa-lg"></i></a>
@@ -10,7 +10,7 @@
         <a href="https://instagram.com/web_umenia/" target="_blank" data-toggle="tooltip" title="instagram"><i class="fa fa-instagram fa-lg"></i></a>
         <a href="https://github.com/SlovakNationalGallery" target="_blank" data-toggle="tooltip" title="github"><i class="fa fa-github fa-lg"></i></a>
       </div>
-      <div class="col-sm-6 text-center text-sm-right space">
+      <div class="col-sm-6 text-center text-sm-right top-space bottom-space">
         <span class="text-muted">
           {{ utrans('footer.made_by') }} <a href="http://lab.sng.sk" target="_blank" class="sans underline">lab.SNG</a>.
           {{ utrans('footer.subscribe') }} <a href="https://webumenia.us3.list-manage.com/subscribe?u=0836676badd21fd175052c757&id=1d7d3f54f9" target="_blank" class="sans underline">{{ trans('footer.newsletter') }}</a>.
