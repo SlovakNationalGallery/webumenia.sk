de0d0c170529cb8eff3325b984a9d1f49705368c
Igor Rjabinin <igor.rjabinin@gmail.com>
2018-09-07 14:25:01 +0200

    refactor code

--- resources/views/informacie.blade.php
+++ resources/views/informacie.blade.php
@@ -174,31 +174,5 @@
 
 @section('javascript')
     @include('components.artwork_carousel_js', ['slick_query' => '.artworks-preview'])
-
-    <script type="text/javascript">
-        $( ".galleries a" ).hover(function() {
-          var gallery_id = $( this ).attr('id');
-          var old_classes = $( "svg#map path."+gallery_id ).attr('class');
-          $( "svg#map path."+gallery_id ).attr('class', 'active '+old_classes);
-          $( "svg#map path."+gallery_id ).data('old_classes', old_classes);
-        }, function() {
-          var gallery_id = $( this ).attr('id');
-          var old_classes = $( "svg#map path."+gallery_id ).data('old_classes');
-          $( "svg#map path."+gallery_id ).attr('class', old_classes);
-        });
-
-
-        $( "svg#map path.cls-4" ).hover(function() {
-          var classes = $( this ).attr('class').split(/\s+/);
-          $.each( classes, function( index, class_name ){
-              // if class_name is uppercased (e.g. SNG, OGD ...) it's gallery ID
-              if (class_name === class_name.toUpperCase()) {
-                  console.log(class_name);
-                  $( ".galleries a#"+ class_name ).addClass('active');
-              }
-          });
-        }, function() {
-          $( ".galleries a" ).removeClass('active');
-        });
-    </script>
+    {!! Html::script('js/components/map-with-galleries.js') !!}
 @stop
