f48d47914eab337ccd679c083e144d23a48f1756
memb <ustengrav@users.noreply.github.com>
2018-10-10 18:52:10 +0200

    incorporate grammar corrections on reproductions page

--- resources/lang/sk/reprodukcie.php
+++ resources/lang/sk/reprodukcie.php
@@ -12,9 +12,7 @@ return array(
     'print_heading'     => 'Tlačené Reprodukcie',
     'print_body'        => '<p>Tlačenú reprodukciu si objednáte pomocou tlačidla objednať reprodukciu na stránke konkrétneho diela.</p>
                            <p>Cena reprodukcie sa odvíja od zvoleného rozmeru a typu spracovania, ktoré si zvolíte pri vyplňovaní objednávky.</p>
-                           <p>Pokiaľ je dielo zdigitalizované v dostatočnej kvalite, vybavenie žiadosti môže trvať 7 až 30 dní.</p>
-                           <p>V prípade, ak dielo zdigitalizované nie je, čakacia doba sa zvyšuje o čas potrebný na jeho digitalizáciu.</p>
-                           <p>V prípade dlhšej čakacej doby Vás systém automaticky informuje pri vypĺňaní on-line objednávky.</p>
+                           <p>Štandardná lehota na vybavenie objednávky je <strong>30 dní</strong>. V prípade dlhšej čakacej doby Vás systém automaticky informuje pri vypĺňaní on-line objednávky.</p>
                            <p>Momentálne je možné vyzdvihnúť si reprodukcie len osobne v <a href="https://www.sng.sk/sk/bratislava/navsteva/kde-nas-najdete/knihkupectvo-ex-libris">kníhkupectve Ex Libris</a> v priestoroch SNG na Námestí Ľ. Štúra 4 v Bratislave alebo v pokladni <a href="https://www.sng.sk/sk/zvolen/navsteva/kde-nas-najdete">Zvolenského zámku - Námestie SNP 594/1</a>.</p>',
     'print_recommended' => 'Odporúčame',
     'print_list'        => '<ul>
@@ -35,18 +33,18 @@ return array(
     'print_offer_poster_title'             => 'Poster (Art Plagát)',
     'print_offer_poster_description'       => '<p>Reprodukcia je vytlačená na špeciálny, vysokokvalitný, mapový papier rozmeru A1.</p><p>Momentálne ponúkame v tejto forme iba reprodukcie <a href="https://www.webumenia.sk/kolekcia/144">plagátov zo zbierok SNG</a>.</p>',
     'digital_heading'   => 'Digitálne Reprodukcie',
-    'digital_body'      => '<p>Poskytnutie digitálnych reprodukcii diel posudzujeme individuálne podľa konkrétneho diela a účelu použitia.</p>
-                            <p>V súvislosti s ochranou autorských práv, <strong>SNG neposkytuje digitálne reprodukcie chránených diel na všeobecné súkromné účely (dekorácia, dar)</strong>.</p>
-                            <p>Na <strong>výskumný účel</strong> (školské a vedecké práce, biografie) a <strong>edukačný účel</strong> (vzdelávacie pomôcky, učebnice) poskytujeme reprodukcie zväčša bezplatne. Na <strong>komerčný účel</strong> (články v tlači, publikácie, propagačné materiály) a výstavy sa cena odvíja od viacerých faktorov a hodnotí sa individuálne.</p>
-                            <p>Pri poskytovaní diel, ktoré sú chránené autorským právom (nie sú voľné) postupuje SNG s ohľadom na licenciu, právo na použitie na základe prejavu vôle autora a s ohľadom na historický kontext vývoja autorského práva.</p>
-                            <p>SNG nedisponuje právom na neobmedzené použitie svojich zbierkových predmetov. <strong>Autorské práva môže vlastniť resp. spravovať aj iný subjekt</strong>, napr. autorská spoločnosť LITA, autori, dedičia. V takých prípadoch je potrebné konzultovať&nbsp; povolenie na použitie s príslušným subjektom.</p>
-                            <p>Čas potrebný na vybavenie objednávky závisí od autorskej situácie konkrétnych diel a môže v niektorých prípadochvyžadovať aj niekoľko týždňov. Odporúčame preto digitálne reprodukcie <strong>objednať v dostatočnom predstihu.</strong></p>',
+    'digital_body'      => '<p>Poskytnutie digitálnych reprodukcií diel posudzujeme individuálne podľa konkrétneho diela a účelu použitia.</p>
+                            <p>V súvislosti s ochranou autorských práv <strong>Slovenská národná galéria neposkytuje digitálne reprodukcie chránených diel na všeobecné súkromné účely (dekorácia, dar)</strong>.</p>
+                            <p>Na <strong>výskumný účel</strong> (školské a vedecké práce, biografie) a <strong>edukačný účel</strong> (vzdelávacie pomôcky, učebnice) poskytujeme reprodukcie zväčša bezplatne. Na <strong>komerčný účel</strong> (články v tlači, publikácie, propagačné materiály) a na výstavy sa cena odvíja od viacerých faktorov a hodnotí sa individuálne.</p>
+                            <p>Pri poskytovaní diel, ktoré sú chránené autorským právom (nie sú voľné), postupuje SNG s ohľadom na licenciu, právo na použitie na základe prejavu vôle autora a s ohľadom na historický kontext vývoja autorského práva.</p>
+                            <p>SNG nedisponuje právom na neobmedzené použitie svojich zbierkových predmetov. <strong>Autorské práva môže vlastniť, resp. spravovať aj iný subjekt</strong>, napr. autorská spoločnosť LITA, autori, dedičia. V takých prípadoch je potrebné konzultovať&nbsp; povolenie na použitie s príslušným subjektom.</p>
+                            <p>Čas potrebný na vybavenie objednávky závisí od autorskej situácie konkrétnych diel a môže v niektorých prípadoch vyžadovať aj niekoľko týždňov. Odporúčame preto digitálne reprodukcie <strong>objednať v dostatočnom predstihu.</strong></p>',
     'digital_examples'  => 'Príklady použitia digitálnych reprodukcii',
     'digital_list'      => '<ul>
                               <li>Každé poskytnutie digitálnej reprodukcie je <strong>podmienené podpísaním zmluvy</strong>, ktorá špecifikuje účel jej použitia.&nbsp;Odkaz na stiahnutie obrázkov zo servera SNG posielame až po obdržaní podpísanej zmluvy (odfotenej, oskenovanej alebo zaslanej poštou).</li>
                               <li>Digitálne reprodukcie diel spravovaných inými galériami poskytujeme iba po predložení písomného súhlasu príslušnej inštitúcie. Názov galérie spravujúcej dotyčné dielo je uvedené v položke <strong>„Galéria“</strong> v informáciách o diele.</li>
-                              <li>Ak si chcete objednať aj tlačené aj digitálne reprodukcie, vytvorte <strong>zvlášť objednávku pre každý typ.</strong>       </li>
+                              <li>Ak si chcete objednať tlačené aj digitálne reprodukcie, vytvorte <strong>zvlášť objednávku pre každý typ.</strong>       </li>
                             </ul>',
     'digital_choice'    => 'Vyberte si reprodukcie',
-    'more-items_button' => 'zobraziť všetkých'
+    'more-items_button' => 'zobraziť všetky'
 );
