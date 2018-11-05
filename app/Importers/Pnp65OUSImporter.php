b0c9ed73b852adc5c5e220b5d59952e650d40da1
Endut <adam.a.juraszek@googlemail.com>
2018-11-05 20:48:56 +0100

    wrong identifiers in import

--- app/Importers/Pnp65OUSImporter.php
+++ app/Importers/Pnp65OUSImporter.php
@@ -14,22 +14,22 @@ class Pnp65OUSImporter extends AbstractImporter
     ];
 
     protected $mapping = [
-        'Inventární číslo' => 'identifier',
-        'Autor' => 'author',
-        'Datace' => 'dating',
-        'Rok - od' => 'date_earliest',
-        'Rok - do' => 'date_latest',
-        'Výtvarný druh' => 'work_type',
-        'Název' => 'title',
-        'Materiál' => 'medium',
-        'Technika' => 'technique',
-        'Námět' => 'topic',
-        'Značení' => 'inscription',
-        'Rozměry' => 'measurement',
-        'Ze souboru' => 'related_work',
-        'Popis' => 'description',
-        'Nakladatel' => 'publisher',
-        'Autor textu' => 'description_source',
+        'Inventární číslo:' => 'identifier',
+        'Autor:' => 'author',
+        'Datace:' => 'dating',
+        'Rok - od:' => 'date_earliest',
+        'Rok - do:' => 'date_latest',
+        'Výtvarný druh:' => 'work_type',
+        'Název:' => 'title',
+        'Materiál:' => 'medium',
+        'Technika:' => 'technique',
+        'Námět:' => 'topic',
+        'Značení:' => 'inscription',
+        'Rozměry:' => 'measurement',
+        'Ze souboru:' => 'related_work',
+        'Popis:' => 'description',
+        'Nakladatel:' => 'publisher',
+        'Autor textu:' => 'description_source',
     ];
 
     protected static $name = 'pnp65ous';
@@ -37,21 +37,21 @@ class Pnp65OUSImporter extends AbstractImporter
     public function __construct(IFileRepository $repository) {
         parent::__construct($repository);
         $this->filters[] = function (array $record) {
-            return $record['Signatura'] !== '';
+            return $record['Inventární číslo:'] !== '';
         };
     }
 
     protected function getItemId(array $record) {
-        return sprintf('CZE:PNP.%s',  $this->sanitizeIdentifier($record['Signatura']));
+        return sprintf('CZE:PNP.%s',  $this->sanitizeIdentifier($record['Inventární číslo:']));
     }
 
     protected function getItemImageFilenameFormat(array $record) {
-        return sprintf('%s--*', $this->sanitizeIdentifier($record['Signatura']));
+        return sprintf('%s--*', $this->sanitizeIdentifier($record['Inventární číslo:']));
     }
 
     protected function hydrateAuthor(array $record) {
         // @todo other authors
-        return $record['Autor'];
+        return $record['Autor:'];
     }
 
     protected function sanitizeIdentifier($identifier) {
