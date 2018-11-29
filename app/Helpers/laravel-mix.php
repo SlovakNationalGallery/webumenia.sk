7db5088245523bb7ce2d5a7d35b75e304d154aef
Igor Rjabinin <igor.rjabinin@gmail.com>
2018-11-29 17:33:36 +0100

    basic setup

---
+++ app/Helpers/laravel-mix.php
@@ -0,0 +1,58 @@
+<?php
+use App\Services\Deployment;
+use \Illuminate\Support\Str;
+use \Illuminate\Support\HtmlString;
+
+if (! function_exists('mix')) {
+    /**
+     * Get the path to a versioned Mix file.
+     *
+     * @param  string  $path
+     * @param  string  $manifestDirectory
+     * @return \Illuminate\Support\HtmlString
+     *
+     * @throws \Exception
+     */
+    function mix($path, $manifestDirectory = '')
+    {
+        static $manifests = [];
+
+        if (! Str::startsWith($path, '/')) {
+            $path = "/{$path}";
+        }
+
+        if ($manifestDirectory && ! Str::startsWith($manifestDirectory, '/')) {
+            $manifestDirectory = "/{$manifestDirectory}";x
+        }
+
+        if (file_exists(public_path($manifestDirectory.'/hot'))) {
+            return new HtmlString("//localhost:8080{$path}");
+        }
+
+        $manifestPath = public_path($manifestDirectory.'/mix-manifest.json');
+
+        if (! isset($manifests[$manifestPath])) {
+            if (! file_exists($manifestPath)) {
+                throw new Exception('The Mix manifest does not exist.');
+            }
+
+            $manifests[$manifestPath] = json_decode(file_get_contents($manifestPath), true);
+        }
+
+        $manifest = $manifests[$manifestPath];
+
+        if (\App::environment('local')) {
+            foreach($manifest as $key => &$value) {
+                $value .= '?key=' . md5(microtime(true));
+            }
+        }
+
+        if (! isset($manifest[$path])) {
+            if (! app('config')->get('app.debug')) {
+                return $path;
+            }
+        }
+
+        return new HtmlString($manifestDirectory.$manifest[$path]);
+    }
+}
