fb54f49e67b4f0f1314cc87b18bf2a312adf71e2
Igor Rjabinin <igor.rjabinin@gmail.com>
2018-12-06 11:02:05 +0100

    add "check spam folder" into thank-you message in order

--- resources/lang/en/dakujeme.php
+++ resources/lang/en/dakujeme.php
@@ -9,6 +9,6 @@ return array(
     */
 
     'title'     => 'Thank you!',
-    'paragraph' => 'Your reproduction order has been received. An automatic e-mail with detailed information was sent to the address you provided. Have a nice day!',
+    'paragraph' => 'Your reproduction order has been received. An automatic e-mail with detailed information was sent to the address you provided. In case you haven\'t received any e-mail, please check your spam folder. Have a nice day!',
     'button'    => 'Return to home page',
 );
