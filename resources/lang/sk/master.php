8b3914235828f28213804c796db86dc529a5f660
Igor Rjabinin <igor.rjabinin@gmail.com>
2018-12-12 13:58:39 +0100

    autori -> umelci

--- resources/lang/sk/master.php
+++ resources/lang/sk/master.php
@@ -11,7 +11,7 @@ return array(
     'meta_description'   => 'Web umenia je on-line katalóg výtvarných diel zo zbierok slovenských galérií. Nájdete tu základné informácie o dielach a ich autoroch, ale aj pôvodné články, videá a kolekcie.',
     'artworks'           => 'diela',
     'collections'        => 'kolekcie',
-    'authors'            => 'autori',
+    'authors'            => 'umelci',
     'articles'           => 'články',
     'galleries'          => 'galérie',
     'reproductions'      => 'reprodukcie',
