80ac04654242b725b85228302742f385e0717653
Igor Rjabinin <igor.rjabinin@gmail.com>
2018-12-12 10:03:03 +0100

    clickable logo points to root url

--- resources/views/components/khb_logo_artbase.blade.php
+++ resources/views/components/khb_logo_artbase.blade.php
@@ -1,5 +1,7 @@
-<img 
-    src="/images/khb/logo-artbase.svg" 
-    alt="Artbase Logo"
-    class="w-100 {{$class_names or ''}}"
->
+<a href="/" class="header-logo no-underline">
+    <img
+        src="/images/khb/logo-artbase.svg"
+        alt="Artbase Logo"
+        class="w-100 {{$class_names or ''}}"
+    >
+</a>
