ece9e52a375246afd0b07af904abdda6f28c91c5
Philo van Kemenade <philovankemenade@gmail.com>
2018-12-12 16:19:48 +0100

    create KHBGridCellSpeaker component & add to intro view

---
+++ resources/views/components/khb_grid_cell_speaker.blade.php
@@ -0,0 +1,4 @@
+<div class="col-6 p-0 grid-cell-speaker" style="background-image: url({!! $artistImageUrl !!});">
+    <div class="grid-cell-speaker-artwork" style="background-image: url({!! $artworkImageUrl !!});"></div>
+    <h2 class="grid-cell-speaker-name m-3">{!! $artistName !!}</h2>
+</div>
