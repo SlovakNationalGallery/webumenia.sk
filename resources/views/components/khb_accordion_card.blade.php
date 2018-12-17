e5a933e7ddee903bb3417db5cc96690adcfeb2d2
Philo van Kemenade <philovankemenade@gmail.com>
2018-12-17 12:48:31 +0100

    fix indentation

--- resources/views/components/khb_accordion_card.blade.php
+++ resources/views/components/khb_accordion_card.blade.php
@@ -1,5 +1,5 @@
 <div class="card">
-<div class="card-header" id="heading{{ studly_case($name) }}">
+  <div class="card-header" id="heading{{ studly_case($name) }}">
     <h5 class="mb-0">
       <button class="btn btn-link font-weight-bold p-0" type="button" data-toggle="collapse" data-target="#collapse{{ studly_case($name) }}" aria-expanded="true" aria-controls="collapse{{ studly_case($name) }}">
         {{ trans('autor.'.$name) }}
@@ -7,7 +7,7 @@
     </h5>
   </div>
 
-<div id="collapse{{ studly_case($name) }}" class="collapse {{ ($show) ? 'show' : ''}}" aria-labelledby="heading{{ studly_case($name) }}" data-parent="#{{ $parrentId }}">
+  <div id="collapse{{ studly_case($name) }}" class="collapse {{ ($show) ? 'show' : ''}}" aria-labelledby="heading{{ studly_case($name) }}" data-parent="#{{ $parrentId }}">
     <div class="card-body pt-0">
       {!!  $content !!}
     </div>
