33c0da362f0c2e57a3c1c49b73113e84bc38dd7b
Igor Rjabinin <igor.rjabinin@gmail.com>
2018-12-18 18:14:50 +0100

    fix alphabet border-top + apply grid-cell-link

--- resources/views/autori.blade.php
+++ resources/views/autori.blade.php
@@ -88,10 +88,11 @@
     </form>
 </section>
 
-<div class="row">
+<div class="row border-top">
     @foreach (range('A', 'Z') as $char)
-        <div class="col-sm-2 alphabet text-sans">
-            <a href="{!! url_to(((Request::is('teoretici')) ? 'teoretici' : 'umelci'), ['first-letter' => $char]) !!}" class="{!! (Input::get('first-letter')==$char) ? 'active' : '' !!}" rel="{!! $char !!}">{!! $char !!}</a>
+        <div class="col-sm-2 alphabet text-sans grid-cell-link {!! (Input::get('first-letter')==$char) ? 'active' : '' !!}">
+            <a href="{!! url_to(((Request::is('teoretici')) ? 'teoretici' : 'umelci'), ['first-letter' => $char]) !!}" rel="{!! $char !!}"></a>
+            <span>{!! $char !!}</span>
         </div>
     @endforeach
 </div>
