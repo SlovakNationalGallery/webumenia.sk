bf1b1cbccb39089ef12658455433f81bbe62d46c
Philo van Kemenade <philovankemenade@gmail.com>
2018-12-18 16:46:02 +0100

    improve responsiveness KHBNavBar

--- resources/views/components/khb_nav_bar.blade.php
+++ resources/views/components/khb_nav_bar.blade.php
@@ -1,14 +1,9 @@
 <nav class="khb-nav-bar">
     <div class="row">
-        <div class="col d-none d-sm-flex">
+        <div class="col">
             <!-- Search not yet supported -->
         </div>
-        <div class="col col-sm-2dot4 grid-cell-link {!! Request::is('o-projekte') ? 'active' : '' !!}">
-            <a href="{{{ URL::to('o-projekte') }}}" class="">
-            </a>
-            <span>{{ utrans('master.about') }}</span>
-        </div>
-        <div class="col col-sm-2dot4 py-0 border-0 d-flex">
+        <div class="col-6 col-sm-4 col-xl-2 py-0 border-0 d-flex">
             @include('components.khb_language_switch', [
                 'currentLocale' => App::getLocale(),
                 'localesOrdered' => LaravelLocalization::getLocalesOrder(),
@@ -17,30 +12,35 @@
         </div>
     </div>
     <div class="row">
-        <div class="col grid-cell-link {!! (Request::is('umelci') || Request::is('umelec/*')) ? 'active' : '' !!}">
+        <div class="col-6 col-sm-4 col-xl-2 grid-cell-link {!! (Request::is('umelci') || Request::is('umelec/*')) ? 'active' : '' !!}">
             <a href="{{{ URL::to('umelci') }}}" class="">
             </a>
             <span>{{ utrans('master.authors') }}</span>
         </div>
-        <div class="col grid-cell-link {!! Request::is('skupiny') ? 'active' : '' !!}">
+        <div class="col-6 col-sm-4 col-xl-2 grid-cell-link {!! Request::is('skupiny') ? 'active' : '' !!}">
             <a href="{{{ URL::to('skupiny') }}}" class="">
             </a>
             <span>{{ utrans('master.groups') }}</span>
         </div>
-        <div class="col grid-cell-link {!! (Request::is('teoretici') || Request::is('teoretik/*')) ? 'active' : '' !!}">
+        <div class="col-6 col-sm-4 col-xl-2 grid-cell-link {!! (Request::is('teoretici') || Request::is('teoretik/*')) ? 'active' : '' !!}">
             <a href="{{{ URL::to('teoretici') }}}" class="">
             </a>
             <span>{{ utrans('master.theoreticians') }}</span>
         </div>
-        <div class="col grid-cell-link {!! Request::is('vystavne-priestory') ? 'active' : '' !!}">
+        <div class="col-6 col-sm-4 col-xl-2 grid-cell-link {!! Request::is('vystavne-priestory') ? 'active' : '' !!}">
             <a href="{{{ URL::to('vystavne-priestory') }}}" class="">
             </a>
             <span>{{ utrans('master.spaces') }}</span>
         </div>
-        <div class="col grid-cell-link {!! Request::is('klucove-slova') ? 'active' : '' !!}">
+        <div class="col-6 col-sm-4 col-xl-2 grid-cell-link {!! Request::is('klucove-slova') ? 'active' : '' !!}">
             <a href="{{{ URL::to('klucove-slova') }}}" class="">
             </a>
             <span>{{ utrans('master.keywords') }}</span>
+        </div>
+        <div class="col-6 col-sm-4 col-xl-2 grid-cell-link {!! Request::is('o-projekte') ? 'active' : '' !!}">
+            <a href="{{{ URL::to('o-projekte') }}}" class="">
+            </a>
+            <span>{{ utrans('master.about') }}</span>
         </div>
     </div>
 </nav>
