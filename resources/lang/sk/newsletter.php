a3f96e0f37d31e8b5fc3f2cb752375787294252e
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2018-12-20 14:48:43 +0100

    [newsletter] Add cs/en translations

--- resources/lang/sk/newsletter.php
+++ resources/lang/sk/newsletter.php
@@ -2,5 +2,5 @@
 
 return [
     'signup_success_message' => 'Boli ste úspešne prihlásený do newslettera',
-    'signup_failed_message' => 'Vyskytla sa chyba pri prihlasovaní do newslettera'
+    'signup_failed_message' => 'Vyskytla sa chyba pri prihlasovaní do newslettera',
 ];
