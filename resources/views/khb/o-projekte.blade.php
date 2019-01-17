6c656d4fbd11c6d83c9575610232d1e2a82f6a43
Philo van Kemenade <philovankemenade@gmail.com>
2019-01-17 09:10:27 +0100

    add ReadMore functionality to OProjektePage

--- resources/views/khb/o-projekte.blade.php
+++ resources/views/khb/o-projekte.blade.php
@@ -50,5 +50,22 @@
         </div>
     </div>
 </section>
+@stop
 
+@section('javascript')
+{!! Html::script('js/plugins/readmore.min.js') !!}
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('.expandable').readmore({
+            moreLink: '<a href="#" class="no-underline text-right">{{ trans("general.show_more") }} <i class="fas fa-chevron-right"></i></a>',
+            lessLink: '<a href="#" class="no-underline text-right">{{ trans("general.show_less") }} <i class="fas fa-chevron-up"></i></a>',
+            maxHeight: 400,
+            afterToggle: function(trigger, element, expanded) {
+              if(! expanded) { // The "Close" link was clicked
+                $('html, body').animate( { scrollTop: element.offset().top }, {duration: 100 } );
+              }
+            }
+        });
+    });
+</script>
 @stop
