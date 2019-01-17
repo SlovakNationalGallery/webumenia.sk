34e85a173a225b97d930dc3287da07d4ad1f4e31
Philo van Kemenade <philovankemenade@gmail.com>
2019-01-17 09:01:06 +0100

    fix responsiveness constrained width of KHBAccordionCard content on AuthorPage and OProjektePage

--- resources/views/khb/o-projekte.blade.php
+++ resources/views/khb/o-projekte.blade.php
@@ -6,48 +6,49 @@
 @stop
 
 @section('content')
-
-<div class="row">
-    <div class="col p-0">
-        <div class="accordion" id="authorAccordion">
-            @include('components.khb_accordion_card', [
-                'title' => utrans('khb-o-projekte.about-title'),
-                'content' => utrans('khb-o-projekte.about-content'),
-                'parrentId' => 'authorAccordion',
-                'show' => true,
-            ])
-            @include('components.khb_accordion_card', [
-                'title' => utrans('khb-o-projekte.content_structure-title'),
-                'content' => utrans('khb-o-projekte.content_structure-content'),
-                'parrentId' => 'authorAccordion',
-                'show' => false,
-            ])
-            @include('components.khb_accordion_card', [
-                'title' => utrans('khb-o-projekte.committee-title'),
-                'content' => utrans('khb-o-projekte.committee-content'),
-                'parrentId' => 'authorAccordion',
-                'show' => false,
-            ])
-            @include('components.khb_accordion_card', [
-                'title' => utrans('khb-o-projekte.team-title'),
-                'content' => utrans('khb-o-projekte.team-content'),
-                'parrentId' => 'authorAccordion',
-                'show' => false,
-            ])
-            @include('components.khb_accordion_card', [
-                'title' => utrans('khb-o-projekte.contact-title'),
-                'content' => utrans('khb-o-projekte.contact-content'),
-                'parrentId' => 'authorAccordion',
-                'show' => false,
-            ])
-            @include('components.khb_accordion_card', [
-                'title' => utrans('khb-o-projekte.copyright-title'),
-                'content' => utrans('khb-o-projekte.copyright-content'),
-                'parrentId' => 'authorAccordion',
-                'show' => false,
-            ])
+<section class="about">
+    <div class="row">
+        <div class="col p-0">
+            <div class="accordion" id="authorAccordion">
+                @include('components.khb_accordion_card', [
+                    'title' => utrans('khb-o-projekte.about-title'),
+                    'content' => utrans('khb-o-projekte.about-content'),
+                    'parrentId' => 'authorAccordion',
+                    'show' => true,
+                ])
+                @include('components.khb_accordion_card', [
+                    'title' => utrans('khb-o-projekte.content_structure-title'),
+                    'content' => utrans('khb-o-projekte.content_structure-content'),
+                    'parrentId' => 'authorAccordion',
+                    'show' => false,
+                ])
+                @include('components.khb_accordion_card', [
+                    'title' => utrans('khb-o-projekte.committee-title'),
+                    'content' => utrans('khb-o-projekte.committee-content'),
+                    'parrentId' => 'authorAccordion',
+                    'show' => false,
+                ])
+                @include('components.khb_accordion_card', [
+                    'title' => utrans('khb-o-projekte.team-title'),
+                    'content' => utrans('khb-o-projekte.team-content'),
+                    'parrentId' => 'authorAccordion',
+                    'show' => false,
+                ])
+                @include('components.khb_accordion_card', [
+                    'title' => utrans('khb-o-projekte.contact-title'),
+                    'content' => utrans('khb-o-projekte.contact-content'),
+                    'parrentId' => 'authorAccordion',
+                    'show' => false,
+                ])
+                @include('components.khb_accordion_card', [
+                    'title' => utrans('khb-o-projekte.copyright-title'),
+                    'content' => utrans('khb-o-projekte.copyright-content'),
+                    'parrentId' => 'authorAccordion',
+                    'show' => false,
+                ])
+            </div>
         </div>
     </div>
-</div>
+</section>
 
 @stop
