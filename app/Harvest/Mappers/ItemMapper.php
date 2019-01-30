e692ba348c812c58fe2375c4ccb86f5d2a73d1a1
Endut <adam.a.juraszek@googlemail.com>
2019-01-30 12:38:48 +0100

    fix failing travis check - silly php syntax error

--- app/Harvest/Mappers/ItemMapper.php
+++ app/Harvest/Mappers/ItemMapper.php
@@ -189,7 +189,7 @@ class ItemMapper extends AbstractModelMapper
     }
 
     public function mapContributor(array $row) {
-        return isset($row['contributor'][0]) ? $row['contributor'][0] : null
+        return isset($row['contributor'][0]) ? $row['contributor'][0] : null;
     }
 
     protected function getRelatedParts(array $row) {
