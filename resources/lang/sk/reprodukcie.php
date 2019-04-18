1e417574c41436849605c42ebe7887985d663c5c
memb <ustengrav@users.noreply.github.com>
2019-04-18 12:09:57 +0200

    rephrase three list items
    replace "...may lead to a fee" with "...may lead to the breaking of the law"

--- resources/lang/sk/reprodukcie.php
+++ resources/lang/sk/reprodukcie.php
@@ -12,14 +12,14 @@ return array(
     'info_title'        => 'Dôležité informácie',
     'print_heading'     => 'Tlačené Reprodukcie',
     'print_body'        => '<p>Tlačenú reprodukciu si objednáte pomocou tlačidla <strong>objednať reprodukciu</strong> na stránke konkrétneho diela.</p>
-                           <p>Cena reprodukcie sa odvíja od zvoleného rozmeru a typu spracovania, ktoré si zvolíte pri vyplňovaní objednávky.</p>
+                           <p>Cena reprodukcie sa odvíja od zvoleného rozmeru a typu spracovania, ktoré si zvolíte pri vypĺňaní objednávky.</p>
                            <p>Štandardná lehota na vybavenie objednávky je <strong>30 dní</strong>. V prípade dlhšej čakacej doby Vás systém automaticky informuje pri vypĺňaní on-line objednávky.</p>
                            <p>Momentálne je možné vyzdvihnúť si reprodukcie len osobne v <a href="https://www.sng.sk/sk/bratislava/navsteva/kde-nas-najdete/knihkupectvo-ex-libris">kníhkupectve Ex Libris</a> v priestoroch SNG na Námestí Ľ. Štúra 4 v Bratislave alebo v pokladni <a href="https://www.sng.sk/sk/zvolen/navsteva/kde-nas-najdete">Zvolenského zámku - Námestie SNP 594/1</a>.</p>',
     'print_recommended' => 'Odporúčame',
     'print_list'        => '<ul>
                               <li>Výtvarné diela <strong>netlačíme v pôvodnej veľkosti</strong> aby nedošlo k zámene s originálom. Pri tlači diela zachovávame pôvodný pomer strán, farebnosť aj signatúru. Maximálny možný rozmer&nbsp;reprodukcie je dvojnásobok rozmerov originálu.</li>
                               <li>Vhodný <strong>typ papiera a atramentu vyberajú pracovníci SNG</strong> podľa výtvarnej techniky a materiálu diela v objednávke.</li>
-                              <li>Tlačené reprodukcie je možné použiť iba na <strong>súkromný účel</strong>. V prípade záujmu použiť tlačené reprodukcie na komerčný účel (napr. ako dekoráciu reštaurácie), vybavenie objednávky prebieha podobne ako pri digitálnej reprodukcii (pozri nižšie). Použitie tlačenej reprodukcie na komerčný účel bez predošlej konzultácie so Slovenskou národnou galériou môže viesť k zmluvnej pokute.</li>
+                              <li>Tlačené reprodukcie je možné použiť iba na <strong>súkromný účel</strong>. V prípade záujmu použiť tlačené reprodukcie na komerčný účel (napr. ako dekoráciu reštaurácie), vybavenie objednávky prebieha podobne ako pri digitálnej reprodukcii (pozri nižšie). Použitie tlačenej reprodukcie na komerčný účel bez predošlej konzultácie so Slovenskou národnou galériou môže viesť k <strong>porušeniu zákona.</strong></li>
                               <li>Po výbere diel, vyplnení údajov a odoslaní objednávky Vám pošleme <strong>e-mail s podrobnejšími informáciami</strong> o objednávke.</li>
                               <li>Ak si chcete objednať aj tlačené aj digitálne reprodukcie, vytvorte <strong>zvlášť objednávku pre každý typ</strong>.</li>
                             </ul>',
@@ -42,7 +42,7 @@ return array(
                             <p>Čas potrebný na vybavenie objednávky závisí od autorskej situácie konkrétnych diel a môže v niektorých prípadoch vyžadovať aj niekoľko týždňov. Odporúčame preto digitálne reprodukcie <strong>objednať v dostatočnom predstihu.</strong></p>',
     'digital_examples'  => 'Príklady použitia digitálnych reprodukcii',
     'digital_list'      => '<ul>
-                              <li>Každé poskytnutie digitálnej reprodukcie je <strong>podmienené podpísaním zmluvy</strong>, ktorá špecifikuje účel jej použitia.&nbsp;Odkaz na stiahnutie obrázkov zo servera SNG posielame až po obdržaní podpísanej zmluvy (odfotenej, oskenovanej alebo zaslanej poštou).</li>
+                              <li>Každé poskytnutie digitálnej reprodukcie je <strong>podmienené podpísaním zmluvy</strong>, ktorá špecifikuje účel jej použitia.&nbsp;Odkaz na stiahnutie obrázkov zo servera SNG posielame až po obdržaní podpísanej zmluvy (odfotenej, oskenovanej alebo zaslanej poštou). Použitie digitálnej reprodukcie na iný účel, ako bolo špecifikované v zmluve o poskytnutí, môže viesť k <strong>porušeniu zákona.</strong></li>
                               <li>Digitálne reprodukcie diel spravovaných inými galériami poskytujeme iba po predložení písomného súhlasu príslušnej inštitúcie. Názov galérie spravujúcej dotyčné dielo je uvedené v položke <strong>„Galéria“</strong> v informáciách o diele.</li>
                               <li>Ak si chcete objednať tlačené aj digitálne reprodukcie, vytvorte <strong>zvlášť objednávku pre každý typ.</strong>       </li>
                             </ul>',
