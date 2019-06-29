ad85f307b4b7994487389fd8014330a4617f323c
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2019-06-29 17:06:17 +0200

    [collection] Fix sorting by translated name on frontend

--- tests/View/CollectionViewTest.php
+++ tests/View/CollectionViewTest.php
@@ -14,23 +14,46 @@ class CollectionViewTest extends TestCase
     public function testIndexSortedByName() {
         $user = factory(User::class)->create();
 
-        $collections = [
-            factory(Collection::class)->create([
-                'name' => 'z',
-                'publish' => true,
-                'user_id' => $user->id,
-            ]),
-            factory(Collection::class)->create([
-                'name' => 'a',
-                'publish' => true,
-                'user_id' => $user->id,
-            ]),
-        ];
+        $collectionB = factory(Collection::class)->create([
+            'name' => 'b',
+            'publish' => true,
+            'user_id' => $user->id,
+        ]);
+        $collectionA = factory(Collection::class)->create([
+            'name' => 'a',
+            'publish' => true,
+            'user_id' => $user->id,
+        ]);
 
         $response = $this->route('get', 'frontend.collection.index', ['sort_by' => 'name']);
         $data = $response->original->getData();
 
-        $this->assertEquals($data['collections'][0]->id, $collections[1]->id);
-        $this->assertEquals($data['collections'][1]->id, $collections[0]->id);
+        $this->assertCount(2, $data['collections']);
+        $this->assertEquals($data['collections'][0]->id, $collectionA->id);
+        $this->assertEquals($data['collections'][1]->id, $collectionB->id);
+    }
+
+    public function testIndexSortedByNameTranslationFallback() {
+        $user = factory(User::class)->create();
+
+        $collectionB = factory(Collection::class)->create([
+            'name' => 'b',
+            'publish' => true,
+            'user_id' => $user->id,
+        ]);
+        $collectionC = factory(Collection::class)->create([
+            'name' => 'a',
+            'name:en' => 'c',
+            'publish' => true,
+            'user_id' => $user->id,
+        ]);
+
+        \App::setLocale('en'); // request en route
+        $response = $this->route('get', 'frontend.collection.index', ['sort_by' => 'name']);
+        $data = $response->original->getData();
+
+        $this->assertCount(2, $data['collections']);
+        $this->assertEquals($data['collections'][0]->id, $collectionB->id);
+        $this->assertEquals($data['collections'][1]->id, $collectionC->id);
     }
 }
