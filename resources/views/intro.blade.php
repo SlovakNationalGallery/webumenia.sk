ed4ddcbd1fdeb4332368cc163f1f81412d15477b
Igor Rjabinin <igor.rjabinin@gmail.com>
2019-07-30 15:01:58 +0200

    fix link in carousel in homepage

--- resources/views/intro.blade.php
+++ resources/views/intro.blade.php
@@ -42,7 +42,7 @@
                     @endif
                 </div>
             </a>
-        </div>        
+        </div>
     @endforeach
 </div>
 
@@ -77,7 +77,7 @@
 	                <div class="col-sm-6 col-xs-12 bottom-space">
                         @include('components.article_thumbnail', [
                             'article' => $article
-                        ])                    
+                        ])
 	                </div>
                     @if ($i%2 == 1)
                         <div class="clearfix"></div>
@@ -115,7 +115,9 @@ $(document).ready(function(){
         var $link = $( cellElement ).find('a');
         var url = $link.attr('href');
         var id = $link.data('id');
-        $.get('/slideClicked', {'id': id});
+        $.get('/slideClicked', {'id': id}).done(function( data ) {
+            window.location.href = url;
+        });
 
     });
 
@@ -125,7 +127,7 @@ $(window).on('resize',function() {
     if ( $( ".flickity-slider" ).length ) {
         setTimeout(function(){
           $('.webumeniaCarousel').children('.flickity-page-dots').css('left',  parseInt($('.flickity-slider').css('transform').split(',')[4]) );
-        }, 200);            
+        }, 200);
     }
 });
 
