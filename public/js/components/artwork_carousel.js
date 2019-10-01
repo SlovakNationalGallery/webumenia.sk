99bd09a85c901efbfe65b4877750948afee7dcc7
Igor Rjabinin <igor.rjabinin@gmail.com>
2019-10-01 14:30:55 +0200

    update version of slick.js (1.5.6->1.8.1) + fix initializing of carousel + avoid layout jumps when slick is not initialized

--- public/js/components/artwork_carousel.js
+++ public/js/components/artwork_carousel.js
@@ -1,14 +1,12 @@
 $(document).ready(function(){
   $(".artworks-preview").slick({
     lazyLoad: 'progressive',
-    slide: 'a',
     variableWidth: true,
     infinite: false
   });
   $(".multiple-views").slick({
     lazyLoad: 'progressive',
-    slide: 'a',
-    variableWidth: true,
+    adaptiveHeight: true,
     dots: true
   });
 })
