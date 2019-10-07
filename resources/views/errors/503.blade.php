f37e9bf744a4fca1280679d0241f172c75f0c33c
Igor Rjabinin <igor.rjabinin@gmail.com>
2019-10-08 00:02:09 +0200

    prepare view for maintenance mode

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.blank')
+
+@section('title')
+{{ trans('maintenance.title') }}
+@stop
+
+@section('content')
+
+<section class="intro intro500">
+    <div class="intro-body">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2 content-section">
+                    <div class="text-center">
+                        {{-- <h1>ERROR 500</h1> --}}
+                        <h2 class="top-margin uppercase">{{ trans('maintenance.h2') }}</h2>
+                        <p>{{ trans('maintenance.paragraph') }}</p>
+                        <a href="/dielo/SVK:SNG.UP-DK_1104"><img src="/images/errors/error.maintenance.jpeg" alt="Sezónni robotníci. Oprava trate" class="img-responsive img-dielo"></a>
+                        <p>
+                            <a href="/dielo/SVK:SNG.UP-DK_1104">{!! trans('maintenance.image_caption') !!}</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+@stop
