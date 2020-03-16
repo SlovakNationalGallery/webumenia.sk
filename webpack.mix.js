5ec10704ed3131a5174c8e08531d39e947f19132
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-03-16 21:49:51 +0100

    use laravel mix to compile styles

---
+++ webpack.mix.js
@@ -0,0 +1,22 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.less('resources/less/style.less', 'public/css').options({
+    autoprefixer: {
+        options: {
+            browsers: [
+                'last 6 versions',
+            ]
+        }
+    }
+});
