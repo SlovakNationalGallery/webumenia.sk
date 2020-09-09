3a598bfbce278fefd45d01398ae2966d17e3a17f
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2020-09-09 23:42:19 +0200

    [item] Display zero-value related work order

--- resources/views/components/item_related_work_order_total.blade.php
+++ resources/views/components/item_related_work_order_total.blade.php
@@ -1,3 +1,5 @@
-@if ($item->related_work_order)
-    ({{ $item->related_work_order }}@if ($item->related_work_total)/{{ $item->related_work_total }}@endif)
+@if ($item->related_work_order !== null && $item->related_work_total)
+    ({{ $item->related_work_order }}/{{ $item->related_work_total }})
+@elseif ($item->related_work_order)
+    ({{ $item->related_work_order }})
 @endif
