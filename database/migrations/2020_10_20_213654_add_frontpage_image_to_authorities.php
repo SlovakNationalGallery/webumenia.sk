cb05de7b8cf22e50720501a9dd53cba1fec7a67d
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-10-20 22:37:41 +0200

    migrate existing images

--- database/migrations/2020_10_20_213654_add_frontpage_image_to_authorities.php
+++ database/migrations/2020_10_20_213654_add_frontpage_image_to_authorities.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use \App\Authority;
 
 class AddFrontpageImageToAuthorities extends Migration
 {
@@ -15,6 +16,16 @@ class AddFrontpageImageToAuthorities extends Migration
         Schema::table('authorities', function (Blueprint $table) {
             $table->string('frontpage_image')->nullable();
         });
+
+        foreach (Authority::all() as $authority) {
+            $old_path = '/images/khb/';
+            $name = "thumb-".str_slug($authority->formatedName, '-').".jpg";
+            if (file_exists(public_path($old_path.$name))) {
+                rename(public_path($old_path.$name), public_path(Authority::FRONTPAGE_IMG_DIR.$name));
+                $authority->frontpage_image = $name;
+                $authority->save();
+            }
+        }
     }
 
     /**
@@ -27,5 +38,13 @@ class AddFrontpageImageToAuthorities extends Migration
         Schema::table('authorities', function (Blueprint $table) {
             $table->dropColumn('frontpage_image');
         });
+
+        foreach (Authority::all() as $authority) {
+            $old_path = '/images/khb/';
+            $name = "thumb-".str_slug($authority->formatedName, '-').".jpg";
+            if (file_exists(public_path(Authority::FRONTPAGE_IMG_DIR.$name))) {
+                rename(public_path(Authority::FRONTPAGE_IMG_DIR.$name), public_path($old_path.$name));
+            }
+        }
     }
 }
