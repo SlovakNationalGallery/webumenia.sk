0f3a7d0d178504f9abc06e2a0bdd56aefcd091b2
Ernest Walzel <ernest.walzel@sng.sk>
2020-11-06 14:48:27 +0100

    Import isotope-layout via laravel-mix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-require('./bootstrap');
+require('./bootstrap')
+
+const jQueryBridget = require('jquery-bridget')
+const isotope = require('isotope-layout')
+
+jQueryBridget('isotope', isotope, $)
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,3 @@
 try {
     window.$ = window.jQuery = require('jquery');
 } catch (e) {}
-
