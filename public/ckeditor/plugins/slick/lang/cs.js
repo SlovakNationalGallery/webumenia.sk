089d270c763c2dbf47ffae6449ec3e3a4b4c27e1
Martin Havala <martin.havala@gmail.com>
2020-11-25 08:35:12 +0100

    cleanup

--- public/ckeditor/plugins/slick/lang/cs.js
+++ public/ckeditor/plugins/slick/lang/cs.js
@@ -3,9 +3,7 @@
 (function (CKEDITOR) {
     CKEDITOR.plugins.setLang('slick', 'cs', {
         title: 'Slick carousel',
-        imagesRequired: 'Prosím definujte alespoň 1 obrázek',
         heightRequired: 'Prosím zadefinujte výšku kontajnera',
         height: 'Výška (html atribut, hodnota + jednotka)',
-        images: "Seznam obrázků. Jeden na řádek, formát: id / url (link)"
     });
 })(CKEDITOR);
--- public/ckeditor/plugins/slick/lang/en.js
+++ public/ckeditor/plugins/slick/lang/en.js
@@ -4,8 +4,6 @@
     CKEDITOR.plugins.setLang('slick', 'en', {
         title: 'Slick carousel',
         imagesRequired: 'Please define at least one image.',
-        heightRequired: 'Please define height of container',
         height: 'Height, use format: value+unit',
-        images: "List of images. One per row, use format: id/url(target)"
     });
 })(CKEDITOR);
