4fda655065b708b20c26eb65b30d6cbd7db8baf2
Martin Havala <martin.havala@gmail.com>
2020-12-02 10:55:59 +0100

    migrations

---
+++ database/migrations/2020_10_01_112350_update_collections_main_images.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateCollectionsMainImages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+		  DB::statement('UPDATE `slides` SET `image` =  CONCAT(id,"/",image,".jpg")');
+		  DB::statement('UPDATE `collections` SET `main_image` =  CONCAT(id,".jpg") WHERE main_image IS NULL');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+		  DB::statement('UPDATE `slides` SET `image` =  REPLACE(image,CONCAT(id ,"/"),"")');
+    }
+}
