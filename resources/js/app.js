3ee6c921b8194b5b42c5fff17abc8bd58eee7951
Ernest Walzel <ernest.walzel@sng.sk>
2020-12-03 11:54:54 +0100

    php artisan preset vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,3 +21,17 @@ require('jquery.easing')
 // Components
 require('./components/searchbar')
 require('./components/clipboard-button')
+
+window.Vue = require('vue')
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+});
