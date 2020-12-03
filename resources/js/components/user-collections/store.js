74477358595317a3015444f797f2020c45a574b0
Ernest Walzel <ernest.walzel@sng.sk>
2020-12-03 13:16:05 +0100

    Use collection store and 'examples' component

---
+++ resources/js/components/user-collections/store.js
@@ -0,0 +1,35 @@
+const defaultState = {
+  items: [],
+}
+
+const store = {
+  state: JSON.parse(localStorage.getItem('userCollections')) || defaultState,
+
+  getItems() {
+    return this.state.items
+  },
+
+  addItem(itemId) {
+    this.state.items.push({id: itemId})
+    this._saveState()
+  },
+
+  removeItem(itemId) {
+    this.state.items = this.state.items.filter(({id}) => id !== itemId)
+    this._saveState()
+  },
+
+  hasItem(itemId) {
+    return this.state.items.findIndex(({id}) => console.log(id, itemId) || id == itemId) > -1
+  },
+
+  toggleItem(itemId) {
+    this.hasItem(itemId) ? this.removeItem(itemId) : this.addItem(itemId)
+  },
+
+  _saveState() {
+    localStorage.setItem('userCollections', JSON.stringify(this.state))
+  },
+}
+
+module.exports = store
