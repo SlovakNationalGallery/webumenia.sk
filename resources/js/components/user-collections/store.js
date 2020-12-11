768a68e77dd0eccf521e072bd399e9580c9dc279
Ernest Walzel <ernest.walzel@sng.sk>
2020-12-11 10:54:18 +0100

    Add basic GTM tracking for user collections

--- resources/js/components/user-collections/store.js
+++ resources/js/components/user-collections/store.js
@@ -12,11 +12,13 @@ const store = {
   addItem(itemId) {
     this.state.items.push({id: itemId})
     this._saveState()
+    this._track('addItem', itemId)
   },
 
   removeItem(itemId) {
     this.state.items = this.state.items.filter(({id}) => id !== itemId)
     this._saveState()
+    this._track('removeItem', itemId)
   },
 
   hasItem(itemId) {
@@ -30,11 +32,25 @@ const store = {
   clearAllItems() {
     this.state.items = []
     this._saveState()
+    this._track('clearAllItems', null)
   },
 
   _saveState() {
     localStorage.setItem('userCollections', JSON.stringify(this.state))
   },
+
+  // GTM Tracking
+  _track(actionName, itemId) {
+    if (!window.dataLayer) return
+
+    window.dataLayer.push({
+      'event': 'UserCollectionEvent',
+      'eventCategory': 'UserCollection',
+      'eventAction': actionName,
+      'eventLabel': itemId,
+      'userCollection': this.state.items
+    })
+  }
 }
 
 module.exports = store
