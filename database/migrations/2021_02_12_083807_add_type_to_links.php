7389f635fd1a2fcce960fcf924576b618ebf10bd
Ernest Walzel <ernest.walzel@sng.sk>
2021-02-12 11:52:54 +0100

    Specify link type

---
+++ database/migrations/2021_02_12_083807_add_type_to_links.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class AddTypeToLinks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('links', function (Blueprint $table) {
+            $table->string('type')->after('label')->index();
+        });
+
+        DB::statement('UPDATE links SET type="external"');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('links', function (Blueprint $table) {
+            $table->dropColumn('type');
+        });
+    }
+}
