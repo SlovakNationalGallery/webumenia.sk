9d68c3fcd2f346bde1f3e7f6d5af587ee01a427b
Ernest Walzel <ernest.walzel@sng.sk>
2021-03-25 12:10:58 +0100

    Define 'administer' and 'edit' gates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,6 +13,25 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        Gate::define('administer', function ($user) {
+            return $user->role === 'admin';
+        });
+
+        Gate::define('edit', function ($user) {
+            if ($user->role === 'admin') return true;
+            return $user->role === 'editor';
+        });
+    }
 }
