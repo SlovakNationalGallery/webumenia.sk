7d0970cf411b7cfbce19f055ce9f95ea3e454907
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-04-09 00:14:32 +0200

    add KGC into list of galleries in the info page

--- resources/lang/cs/informacie.php
+++ resources/lang/cs/informacie.php
@@ -29,6 +29,7 @@ return array(
     'info_gallery_TGP'   => 'Tatranská galéria',
     'info_gallery_PGU'   => 'Považská galéria umenia',
     'info_gallery_SGP'   => 'Šariššská galéria v Prešove',
+    'info_gallery_KGC'   => 'Kysucká galerie v Oščadnici',
     'info_gallery_PNP'   => 'Památník národního písemnictví',
     'info_gallery_4RG'   => 'Galerie moderního umění v Roudnici nad Labem',
 
