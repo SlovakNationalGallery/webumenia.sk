f0fcac99ef2ad1e15dba482bf19f5fd70d1b438a
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-04-09 10:43:33 +0200

    cleanup + fix test

--- app/Harvest/Mappers/AuthorityMapper.php
+++ app/Harvest/Mappers/AuthorityMapper.php
@@ -38,22 +38,6 @@ class AuthorityMapper extends BaseAuthorityMapper
         }, $row['sex']);
     }
 
-    // Do not store vp:Biography_Text from OAI in Biography on Web (WEBUMENIA-1580)
-    /*
-    public function mapBiography(array $row) {
-        if (!isset($row['biography'][0])) {
-            return '';
-        }
-
-        $biography = Str::after($row['biography'][0], '(ZNÁMY)');
-        if (str_contains($biography, 'http')) {
-            return '';
-        }
-
-        return $biography;
-    }
-    */
-
     public function mapBirthPlace(array $row, $locale) {
         if (isset($row['birth_place'][0])) {
             return $this->chooseTranslation($row['birth_place'][0], $locale);
