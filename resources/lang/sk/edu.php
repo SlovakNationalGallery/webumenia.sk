7620fc417955f23a5201aed39eb5cb562a4da1dc
Ernest Walzel <ernest.walzel@sng.sk>
2021-05-20 08:48:41 +0200

    Extract strings for filters

---
+++ resources/lang/sk/edu.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'filter' => [
+      'media_type' => 'formát',
+      'target_group' => 'cieľová skupina',
+      'suitable_for_home' => 'aj pre rodičov',
+      'keywords' => 'kľúčové slová',
+    ],
+];
