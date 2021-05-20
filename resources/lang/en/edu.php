58a8a22ea93b72656832d28ae9257e47b622b896
Ernest Walzel <ernest.walzel@sng.sk>
2021-05-20 09:10:34 +0200

    Add EN and CS translations

---
+++ resources/lang/en/edu.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'filter' => [
+      'media_type' => 'format',
+      'target_group' => 'target group',
+      'suitable_for_home' => 'suitable for parents',
+      'keywords' => 'keywords',
+    ],
+
+    'media_type' => [
+      'methodology' => 'methodology',
+      'worksheet' => 'worksheet',
+      'video' => 'video',
+      'collection' => 'collection',
+      'workshop' => 'workshop',
+      'virtual_exhibition' => 'virtual exhibition',
+    ],
+    
+    'age_group' => [
+      '3-6' => '3—6 years',
+      '7-10' => '7—10 years',
+      '11-15' => '11—15 years',
+      '16-19' => '16—19 years',
+    ],
+];
