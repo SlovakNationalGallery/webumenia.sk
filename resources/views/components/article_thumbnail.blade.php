1dd176cd790258cc81961c81f7f9f3f5195e0c05
Ernest Walzel <eronisko@users.noreply.github.com>
2021-05-26 09:08:37 +0200

    Update resources/views/components/article_thumbnail.blade.php
    Co-authored-by: Igor Rjabinin <igor.rjabinin@gmail.com>

--- resources/views/components/article_thumbnail.blade.php
+++ resources/views/components/article_thumbnail.blade.php
@@ -21,13 +21,13 @@ $showEduTags = $showEduTags ?? false;
   @if($showEduTags)
   <div class="mt-3">
     @foreach ($article->edu_target_age_groups ?? [] as $ageGroup)
-      <a href="{{ request()->fullUrlWithQuery(['age_group' => $ageGroup]) }}" class="btn btn-default btn-xs btn-outline">{{ trans("edu.age_group.$ageGroup") }}</a>
+      <a href="{{ route('frontend.educational-article.index', ['age_group' => $ageGroup]) }}" class="btn btn-default btn-xs btn-outline">{{ trans("edu.age_group.$ageGroup") }}</a>
     @endforeach
     @foreach ($article->edu_media_types ?? [] as $mediaType)
-      <a href="{{ request()->fullUrlWithQuery(['media_type' => $mediaType]) }}" class="btn btn-default btn-xs btn-outline">{{ trans("edu.media_type.$mediaType") }}</a>
+      <a href="{{ route('frontend.educational-article.index', ['media_type' => $mediaType]) }}" class="btn btn-default btn-xs btn-outline">{{ trans("edu.media_type.$mediaType") }}</a>
     @endforeach
     @foreach ($article->edu_keywords ?? [] as $keyword)
-      <a href="{{ request()->fullUrlWithQuery(['keyword' => $keyword]) }}" class="btn btn-default btn-xs btn-outline">{{ $keyword }}</a>
+      <a href="{{ route('frontend.educational-article.index', ['keyword' => $keyword]) }}" class="btn btn-default btn-xs btn-outline">{{ $keyword }}</a>
     @endforeach
   </div>
   @endif
