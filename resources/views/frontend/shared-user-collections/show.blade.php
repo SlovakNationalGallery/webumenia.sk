373a85baf9334e4475a1587637b03a7530daf166
Ernest Walzel <ernest.walzel@sng.sk>
2021-08-20 15:15:36 +0200

    Basic BorderlessInput component

--- resources/views/frontend/shared-user-collections/show.blade.php
+++ resources/views/frontend/shared-user-collections/show.blade.php
@@ -21,37 +21,76 @@
 @php
     $editable = $editable ?? false;    
 @endphp
-
 <form action="{{ $formAction ?? route('frontend.shared-user-collections.store') }}" method="POST">
     @method($formMethod ?? 'POST')
     @csrf
 
-    <label for="name">Názov</label>
-    <input type="text" id="name" name="name" value="{{ old('name', $collection->name ?? null) }}" />
+    <div class="container content-section">
+        <div class="row">
+            <div class="column">
+                <textarea name="name" class="borderless">{{ old('name', $collection->name ?? null) }}</textarea>
+                {{-- <input type="text" class="borderless" id="name" name="name" value="{{ old('name', $collection->name ?? null) }}" /> --}}
+                {{-- <input type="hidden" name="name" value="{{ old('name', $collection->name ?? null) }}" /> --}}
 
-    <br />
+                <borderless-input 
+                    name="name" 
+                    value="{{ old('name', $collection->name ?? null) }}"
+                    class="borderless-72"
+                    placeholder="Zadaj nazov"
+                ></borderless-input>
 
-    <label for="author">Autor</label>
-    <input type="text" id="author" name="author" value="{{ old('author', $collection->author ?? null) }}" />
+                <br />
 
-    <br />
+                <label for="author">Autor</label>
+                <input type="text" id="author" name="author" value="{{ old('author', $collection->author ?? null) }}" />
 
-    <label for="description">Popis</label>
-    <textarea id="description" name="description">{{ old('description', $collection->description ?? null) }}</textarea>
+                <br />
 
-    <br />
+                <label for="description">Popis</label>
+                <textarea id="description" name="description">{{ old('description', $collection->description ?? null) }}</textarea>
 
-    <ul>
-        @foreach ($items as $item)
-            <li>
-                {{ $item->title }}
-                <input type="hidden" name="items[][id]" value="{{ $item->id }}" />
-            </li>
-        @endforeach
-    </ul>
+                <br />
 
-    @if($editable)
-    <button type="submit" name="shared-user-collection-submit">Uložiť</button>
-    @endif
+                @if(isset($collection) && $editable)
+                @php
+                    $shareableUrl = route('frontend.shared-user-collections.show', compact('collection'));
+                @endphp
+
+                <div class="panel panel-default">
+                    <h4>Zdieľaj odkaz na svoj výber:</h4>
+                    <p>
+                        <strong>{{ $shareableUrl }}</strong>
+                        <a
+                            class="btn btn-outline no-border"
+                            data-toggle="tooltip"
+                            data-trigger="hover"
+                            title="{{ trans('general.copy') }}"
+                            data-success-title="{{ trans('general.copied_to_clipboard') }}"
+                            data-clipboard-text="{{ $shareableUrl }}"
+                        >
+                            <i class="fa fa-clipboard"></i> {{ trans('general.copy') }}
+                        </a>
+                    </p>
+                    <p>Ak chceš dalej editovať, ulož si túto adresu:
+                        {{ route('frontend.shared-user-collections.edit', ['collection' => $collection, 'token' => $collection->update_token]) }}
+                    </p>
+                </div>
+                @endif
+
+                <ul>
+                    @foreach ($items as $item)
+                        <li>
+                            {{ $item->title }}
+                            <input type="hidden" name="items[][id]" value="{{ $item->id }}" />
+                        </li>
+                    @endforeach
+                </ul>
+
+                @if($editable)
+                <button type="submit" name="shared-user-collection-submit">Uložiť</button>
+                @endif
+            </div>
+        </div>
+    </div>
 </form>
 @endsection
