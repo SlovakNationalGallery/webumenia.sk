fb78661b352a594bf55cef22f8d3d9d44e91431f
Ernest Walzel <ernest.walzel@sng.sk>
2021-08-20 15:15:36 +0200

    Basic BorderlessInput component

---
+++ resources/js/components/BorderlessInput.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="borderless__container">
+        <div class="borderless__spacer">{{ currentValue || placeholder }}</div>
+        <textarea 
+          :name="name" 
+          :placeholder="placeholder"
+          :aria-label="placeholder"
+          v-model="currentValue"
+          @keydown.enter.prevent /></textarea>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        value: String,
+        placeholder: String
+    },
+    data() {
+        return {
+            currentValue: this.value
+        }
+    }
+}
+</script>
