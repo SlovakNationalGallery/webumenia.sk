3bc239f4922895723b0e9b989f5572018c95683b
Ernest Walzel <ernest.walzel@sng.sk>
2021-08-26 15:54:33 +0200

    ShareForm component

---
+++ resources/js/components/user-collections/ShareForm.vue
@@ -0,0 +1,35 @@
+<template>
+    <form :action="action" method="POST">
+        <slot :values="values" :editing="editing" :setEditing="setEditing"></slot>
+    </form>
+</template>
+
+<script>
+export default {
+    props: {
+        action: String,
+        initialValueName: String,
+        initialValueAuthor: String,
+        initialValueDescription: String,
+    },
+    data() {
+        return {
+            editing: false,
+            values: {
+                name: this.initialValueName,
+                author: this.initialValueAuthor,
+                description: this.initialValueDescription,
+            },
+        }
+    },
+    methods: {
+        setEditing(editing) {
+            this.editing = editing
+            console.log(this.editing)
+        },
+        save() {
+            console.log('save')
+        },
+    }
+}
+</script>
