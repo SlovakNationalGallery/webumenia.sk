04371261bfdc5677be2cc2575b5b200d25878b17
Ernest Walzel <ernest.walzel@sng.sk>
2021-09-09 16:38:40 +0200

    CopyToClipboard component

---
+++ resources/js/components/CopyToClipboard.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="input-group">
+        <input
+            ref="input"
+            type="text" 
+            readonly
+            class="form-control bg-light"
+            :value="value"
+            v-on:focus="e => e.target.select()"
+        >
+        <span class="input-group-btn">
+            <button
+                ref="button"
+                type="button"
+                class="btn btn-default"
+                data-toggle="tooltip"
+                data-placement="top"
+                data-trigger="manual"
+                :title="successText"
+                @click="copy"
+            >
+                <i class="fa fa-clipboard"></i>
+                {{ buttonLabel }}</button>
+            </button>
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        value: String,
+        buttonLabel: String,
+        successText: String,
+    },
+    methods: {
+        copy(e) {
+            if (navigator.clipboard) {
+                navigator.clipboard.writeText(this.value)
+            } else {
+                this.$refs.input.select()
+                document.execCommand("copy")
+            }
+
+            $(this.$refs.button).tooltip('show')
+
+            setTimeout(() => {
+                $(this.$refs.button).tooltip('hide')
+            }, 3000)
+        },
+    }
+}
+</script>
