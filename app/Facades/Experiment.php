b42f0e1514edf2c3eebb21a5f4a67839c243173e
Ernest Walzel <ernest.walzel@sng.sk>
2021-09-30 16:39:34 +0200

    Try with 'Experiment' Facade

---
+++ app/Facades/Experiment.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Experiment extends Facade
+{
+    public static function is($name)
+    {
+        return static::$app['request']->session()->get('experiment') === $name;
+    }
+
+    public static function set($name)
+    {
+        return static::$app['request']->session()->set('experiment', $name);
+    }
+}
