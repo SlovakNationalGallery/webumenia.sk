51acf7c78c1c34720189d2a941d7f8633b72876c
Ernest Walzel <ernest.walzel@sng.sk>
2021-10-14 13:16:15 +0200

    Re-set page after filter change when using CustomSelect filters

--- resources/js/components/filter/filter.js
+++ resources/js/components/filter/filter.js
@@ -1,6 +1,10 @@
 function addParameterToQuery(name, value) {
   const url = new URL(window.location.href)
   value ? url.searchParams.set(name, value) : url.searchParams.delete(name)
+
+  // Reset page on filter change
+  url.searchParams.delete('page')
+
   window.location.href = url
 }
 
