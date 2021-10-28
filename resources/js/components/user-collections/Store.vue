ade2df336d01933f100c27494b0bd24ecd0f571d
Ernest Walzel <ernest.walzel@sng.sk>
2021-10-28 15:05:27 +0200

    Store shared user collections in localStorage

---
+++ resources/js/components/user-collections/Store.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <slot :sharedCollections="sharedCollections" ></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            store: require('./store'),
+        }
+    },
+    computed: {
+        sharedCollections() {
+            return this.store.getSharedCollections()
+        },
+    }
+}
+</script>
