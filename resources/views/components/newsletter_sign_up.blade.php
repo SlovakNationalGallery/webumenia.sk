fc70e4770f35950a6b4c5767334cd0f81dafbd12
Ernest Walzel <ernest.walzel@sng.sk>
2021-10-29 12:18:50 +0200

    Basic styling

---
+++ resources/views/components/newsletter_sign_up.blade.php
@@ -0,0 +1,14 @@
+<div class="bg-blue text-black p-4 pt-5">
+    <h3 class="mt-0 text-xl font-semibold">Chceš umenie do mailovej schránky?</h3>
+    <p class="text-lg">Nové kolekcie, články, projekty a tipy na výtvarné diela <strong>2x mesačne</strong>.</p>
+
+    <div class="input-group mt-4">
+        <input type="email" class="form-control bg-light no-border" placeholder="@">
+        <span class="input-group-btn">
+            <button type="button" class="btn btn-black">
+                Pridajte ma
+            </button>
+        </span>
+    </div>
+    <p class="text-sm mt-3 text-dark">Odoslaním súhlasím so spracovaním <a href="TODO">osobných údajov</a>.</p class="text-sm">
+</div>
