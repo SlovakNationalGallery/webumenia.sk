5673456d9b3f84c8760266f7d01954bfbed0bb72
Ernest Walzel <ernest.walzel@sng.sk>
2021-11-04 10:22:16 +0100

    Empty view (favourites)

--- resources/lang/sk/user-collection.php
+++ resources/lang/sk/user-collection.php
@@ -3,18 +3,16 @@
 return array(
     'title'            => 'Obľúbené diela',
     'empty'            => 'Momentálne nemáte uložené žiadne diela.',
-    'content-intro'    => 'Tu nájdete diela, ktoré ste si uložili medzi obľúbené.<br>
-Diela sú uložené iba dočasne vo vašom internetovom prehliadači.',
-    'content-usage'    => 'Dielo zo zoznamu odstránite kliknutím na hviezdičku.<br>
-Odstránené diela zmiznú po opätovnom načítaní stránky.',
-    'content-feedback' => 'Chyby, pripomienky a návrhy môžete posielať na <a href="mailto:lab@sng.sk">lab@sng.sk</a>',
+    'content-intro'    => 'Obľúbené diela sú uložené iba v tomto internetovom prehliadači.',
+    'content-usage'    => 'Dielo zo zoznamu odstrániš kliknutím na hviezdičku. Odstránené diela zmiznú po opätovnom načítaní stránky.',
+    'share-info'       => 'Z aktuálnych obľúbených diel môžeš vytvoriť tematický výber.',
     'clear'            => 'odstrániť všetky diela',
     'clear-confirm'    => 'Naozaj chcete vymazať všetky obľúbené diela zo zoznamu?',
-    'share'            => 'zdieľať výber',
+    'share'            => 'nový výber diel',
 
     'shared'           => [
         'og' => [
-            'fallback_description' => 'Moje obľúbené diela z Webu umenia'
+            'fallback_description' => 'Výber diel z Webu umenia'
         ],
     ],
 );
