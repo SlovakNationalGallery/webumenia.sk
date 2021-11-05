ef2f9585b7a6cbcf92fd2b2892708e3dd92d3835
Ernest Walzel <ernest.walzel@sng.sk>
2021-11-05 14:30:14 +0100

    Error handling

--- resources/views/livewire/newsletter-signup-form.blade.php
+++ resources/views/livewire/newsletter-signup-form.blade.php
@@ -4,6 +4,8 @@
             @if ($success)
                 <h3 class="text-5xl mt-4 pt-1 font-semibold animated fadeInDown visible-xs-block">Hotovo!</h3>
                 <h3 class="text-center mt-5 text-6xl font-semibold animated tada fast hidden-xs">Hotovo!</h3>
+            @elseif ($errors->any())
+                <h3 class="text-4xl font-semibold">Hups! Vyskytla sa neočakávaná chyba.</h3>
             @else
                 <h3 class="mt-2 text-3xl font-semibold visible-xs-block">Chceš umenie do mailovej schránky?</h3>
                 <h3 class="text-4xl font-semibold hidden-xs">Chceš umenie do mailovej schránky?</h3>
@@ -13,24 +15,24 @@
         <div class="col-md-6">
             <div class="row">
                 <div class="col-md-11 col-md-push-1">
-                    <p class="text-lg">Nové kolekcie, články, projekty a&nbsp;tipy na&nbsp;výtvarné diela <strong>2x mesačne</strong>.</p>
+                    <p class="text-lg pb-md-1">Nové kolekcie, články, projekty a&nbsp;tipy na&nbsp;výtvarné diela <strong>2x mesačne</strong>.</p>
 
                     <form wire:submit.prevent="subscribe" class="input-group mt-4">
                         <input
-                            {{-- type="email" --}}
+                            type="email"
                             class="form-control bg-light no-border placeholder-black {{ $success ? 'text-gray-500' : '' }}"
                             placeholder="@"
-                            {{-- required --}}
-                            {{-- @if ($success) readonly @endif --}}
+                            required
+                            @if ($success) readonly @endif
                             wire:model.defer="email"
                         >
                         <span class="input-group-btn">
-                            <button type="submit" class="btn btn-black font-light">
+                            <button type="submit" class="btn btn-black font-light" @if ($success) disabled @endif>
                                 <span wire:loading.remove>
                                     @if ($success)
-                                    <i class="fa fa-check text-success"></i> Je to tam
+                                        <i class="fa fa-check text-success"></i> Je to tam
                                     @else
-                                    Pridajte ma
+                                        Pridajte ma
                                     @endif
                                 </span>
                                 <span wire:loading>Moment...</span>
