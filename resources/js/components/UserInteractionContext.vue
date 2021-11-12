9e1ae4f19c519393203909f21aaaf3e69e037271
Ernest Walzel <ernest.walzel@sng.sk>
2021-11-12 13:19:00 +0100

    UserInteractionContext measures time on page

--- resources/js/components/UserInteractionContext.vue
+++ resources/js/components/UserInteractionContext.vue
@@ -1,31 +1,39 @@
 <template>
     <div @scroll="onScroll">
-        <slot :maxPercentScrolled="maxPercentScrolled"></slot>
+        <slot :maxPercentScrolled="maxPercentScrolled" :timeSpent="timeSpent"></slot>
     </div>
 </template>
 
 <script>
 import { debounce } from "debounce";
 
+const TimeSpentRefreshRate = 5; // seconds
+
 export default {
     data() {
         return {
-            maxPercentScrolled: 0
+            maxPercentScrolled: 0,
+            timeSpent: 0,
         }
     },
     created() {
         this.onScrollDebounced = debounce(this.onScroll, 500);
+        this.timeSpentInterval = setInterval(this.updateTimeSpent, TimeSpentRefreshRate * 1000);
     },
     mounted() {
         window.addEventListener('scroll', this.onScrollDebounced);
     },
     beforeDestroy() {
+        clearInterval(this.timeSpentInterval);
         window.removeEventListener('scroll', this.onScrollDebounced);
     },
     methods: {
         onScroll(event) {
             const percentScrolled = Math.floor(window.scrollY / (document.body.scrollHeight - document.body.clientHeight) * 100)
             this.maxPercentScrolled = Math.max(this.maxPercentScrolled, percentScrolled);
+        },
+        updateTimeSpent() {
+            this.timeSpent += TimeSpentRefreshRate;
         }
     },
 
