8141c15f111ee6d45c776c93cc9b2d6b1278efad
Ernest Walzel <ernest.walzel@sng.sk>
2021-11-12 12:55:16 +0100

    Fix padding

--- resources/views/frontend/articles/show.blade.php
+++ resources/views/frontend/articles/show.blade.php
@@ -114,7 +114,7 @@
             <div class="row">
                 <div class="col-md-8 col-md-push-2">
                     <div class="bg-blue p-4 p-md-5">
-                        <div class="mx-md-">
+                        <div class="mx-md-2">
                             <livewire:newsletter-signup-form />
                         </div>
                     </div>
