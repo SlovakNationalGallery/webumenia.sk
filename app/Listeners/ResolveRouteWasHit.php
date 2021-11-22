79a12b7d26bcb586e340d4a67ee5c04301353738
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-11-22 21:01:23 +0100

    add ShouldQueue to event

--- app/Listeners/ResolveRouteWasHit.php
+++ app/Listeners/ResolveRouteWasHit.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Spatie\MissingPageRedirector\Events\RouteWasHit;
 
-class ResolveRouteWasHit
+class ResolveRouteWasHit implements ShouldQueue
 {
     /**
      * Create the event listener.
