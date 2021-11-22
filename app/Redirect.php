f42f78d49a115cb1a4cef4b741389422550e290b
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-11-22 19:49:43 +0100

    add model Redirect

---
+++ app/Redirect.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Redirect extends Model
+{
+    use HasFactory;
+}
