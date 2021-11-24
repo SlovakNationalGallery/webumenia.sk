8b28a8ca2f00dbad75f3588713c5546d61143c3f
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2021-11-24 01:18:26 +0100

    add cors provider

--- config/app.php
+++ config/app.php
@@ -129,6 +129,7 @@ return array(
         'GrahamCampbell\Throttle\ThrottleServiceProvider',
         Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider::class,
         Dimsav\Translatable\TranslatableServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
     ),
 
     /*
