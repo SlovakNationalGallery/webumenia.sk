fe912502f98a0efa07a4faf7b206f38325b94760
Ernest Walzel <ernest.walzel@sng.sk>
2022-01-21 19:31:36 +0100

    Better Preflight implementation

--- webpack.mix.js
+++ webpack.mix.js
@@ -43,8 +43,8 @@ mix
     // CSS
     .less('resources/less/admin.less', 'public/css')
     .less('resources/less/style.less', 'public/css')
-    .postCss("resources/css/app-tailwind.css", "public/css", [
-        require("tailwindcss")
+    .postCss('resources/css/app-tailwind.css', 'public/css', [
+        require('tailwindcss')
     ])
 
     // E-mail themes are expected in resources/ but Mix does not save outside of public/
