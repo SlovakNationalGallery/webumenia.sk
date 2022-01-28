08f86fed70f366409612cc7fa413e71be40ecb51
Ernest Walzel <ernest.walzel@sng.sk>
2022-01-28 11:20:25 +0100

    Working tabs and basic carousel

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -25,10 +25,41 @@
 
 @section('content')
     <div class="tailwind-rules container">
-        <h3 class="tw-text-4xl tw-font-semibold">Nový obsah</h3>
-        <div class="tw-flex tw-underline-offset-4 tw-text-5xl tw-font-semibold tw-cursor-pointer tw-mt-8">
-            <h4 class="tw-transition hover:tw-text-gray-500 tw-underline">Kolekcie</h4>
-            <h4 class="tw-transition hover:tw-text-gray-500 tw-underline tw-text-gray-400 tw-ml-4">Články</h4>
-        </div>
+        <h2 class="tw-text-2xl tw-font-semibold">Nový obsah</h2>
+
+        <tabs-controller v-slot="tc">
+            <div class="tw-flex tw-text-4xl tw-font-semibold tw-mt-8 tw-space-x-8">
+                @foreach(['Kolekcie', 'Články'] as $tab)
+                <tab v-slot="{ active }">
+                    <h3 :class="[
+                        'tw-transition tw-cursor-pointer tw-underline tw-underline-offset-8 tw-decoration-4',
+                        !active && 'tw-text-gray-500'
+                    ]">{{ $tab }}</h3>
+                </tab>
+                @endforeach
+            </div>
+            <div class="tw-mt-8">
+                <tab-panel>
+                    <flickity :options="{ cellAlign: 'left', contain: true, pageDots: false }">
+                        @foreach([1,2,3,4,5] as $f)
+                            <div class="tw-w-96 tw-mr-4">
+                                <img src="https://webumenia.sk/images/clanky/148/06f6288416067a3704ca579fa22f986b.thumbnail.jpg" class="tw-object-cover tw-w-96 tw-h-48">
+                                <span class="tw-mt-4 tw-inline-block tw-bg-gray-200 tw-px-3 tw-py-1">Kolekcia</span>
+                                <h4 class="tw-mt-3 tw-text-xl tw-text-black tw-font-semibold">Brána</h4>
+                                <span class="tw-mt-3 tw-inline-block tw-text-gray-600">
+                                    26. 11. 2021 ∙ Oddelenie Galerijnej pedagogiky SNG
+                                </span>
+                            </div>
+                        @endforeach
+                    </flickity>
+                </tab-panel>
+                <tab-panel>
+                    TODO Clanky
+                </tab-panel>
+            </div>
+        </tabs-controller>
+
+        {{-- spacer --}}
+        <div class="tw-mb-96"></div>
     </div>
 @stop
