165e90eac47864189723ac1b0992a7d90bf52096
Ernest Walzel <ernest.walzel@sng.sk>
2022-02-04 10:32:14 +0100

    Allow Flickity component to be resized once

--- resources/js/components/Flickity.vue
+++ resources/js/components/Flickity.vue
@@ -9,10 +9,27 @@ import Flickity from "flickity"
 
 export default {
     props: {
-        options: Object
+        options: Object,
+
+        // Call resize() on Flickity instance when this prop changes to true for the first time.
+        // Useful when Flickity is initialized hidden
+        resizeOnce: Boolean,
+    },
+    data() {
+        return {
+            hasBeenResizedOnce: this.resizeOnce,
+        }
     },
     mounted() {
-        new Flickity(this.$el, this.options)
+        this.flickity = new Flickity(this.$el, this.options)
+    },
+    watch: {
+        resizeOnce(shouldResize) {
+            if (!shouldResize) return
+            if (this.hasBeenResizedOnce) return
+
+            this.flickity.resize()
+        },
     },
 }
 </script>
