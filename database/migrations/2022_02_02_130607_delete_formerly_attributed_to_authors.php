cb928965b59138149696b719bb7bb88b0bd38981
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2022-02-10 10:25:51 +0100

    lazily load db records

--- database/migrations/2022_02_02_130607_delete_formerly_attributed_to_authors.php
+++ database/migrations/2022_02_02_130607_delete_formerly_attributed_to_authors.php
@@ -13,7 +13,7 @@ class DeleteFormerlyAttributedToAuthors extends Migration
      */
     public function up()
     {
-        $items = DB::table('authority_item')
+        DB::table('authority_item')
             ->join('authorities', function ($join) {
                 $join->on('authorities.id', '=', 'authority_item.authority_id');
             })
@@ -22,35 +22,27 @@ class DeleteFormerlyAttributedToAuthors extends Migration
             })
             ->select('item_id', 'author', 'name')
             ->where('role', 'pôvodné určenie/formerly attributed to')
-            ->get();
+            ->orderBy('authority_id')
+            ->orderBy('item_id')
+            ->lazy()
+            ->each(function ($item) {
+                $authors = Str::of($item->author)
+                    ->explode(';')
+                    ->map(function ($author) {
+                        return trim($author);
+                    })
+                    ->reject(function ($author) use ($item) {
+                        return $author === $item->name;
+                    })
+                    ->join('; ');
 
-        foreach ($items as $item) {
-            $authors = Str::of($item->author)
-                ->explode(';')
-                ->map(function ($author) {
-                    return trim($author);
-                })
-                ->reject(function ($author) use ($item) {
-                    return $author === $item->name;
-                })
-                ->join('; ');
-
-            DB::table('items')
-                ->where('id', $item->item_id)
-                ->update(['author' => $authors]);
-        }
+                DB::table('items')
+                    ->where('id', $item->item_id)
+                    ->update(['author' => $authors]);
+            });
 
         DB::table('authority_item')
             ->where('role', 'pôvodné určenie/formerly attributed to')
             ->delete();
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
     }
 }
