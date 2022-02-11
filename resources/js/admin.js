de4035070292d861397e6cd5eb87f903ed593832
Ernest Walzel <ernest.walzel@sng.sk>
2022-02-11 13:17:53 +0100

    Autocomplete component Combining Bloodhound and VueSelect

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -3,5 +3,6 @@ window.Vue = require('vue').default;
 
 Vue.component('linked-combos', require('./components/vue/linked-combos').default);
 Vue.component('admin-links-input', require('./components/admin/LinksInput.vue').default);
+Vue.component('autocomplete', require('./components/Autocomplete.vue').default);
 
 new Vue({ el: '#wrapper' })
---
+++ resources/js/components/Autocomplete.vue
@@ -0,0 +1,64 @@
+<template>
+    <v-select
+        v-on:search="search"
+        :options="options"
+        :filterable="false"
+        :label="optionLabel"
+    >
+        <template v-slot:option="option">
+            <slot name="option" v-bind="option"> {{ option.id }}aaa </slot>
+        </template>
+    </v-select>
+</template>
+
+<script>
+import Bloodhound from "corejs-typeahead/dist/bloodhound"
+import VueSelect from "vue-select"
+
+const defaultRemote = {
+    wildcard: "%QUERY",
+}
+
+export default {
+    components: {
+        "v-select": VueSelect,
+    },
+    props: {
+        remote: Object,
+        optionLabel: String,
+    },
+    data() {
+        return {
+            options: [],
+        }
+    },
+    created() {
+        this.$bloodhound = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.whitespace,
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            remote: {
+                ...defaultRemote,
+                ...this.remote,
+            },
+        })
+    },
+    methods: {
+        search(query, loading) {
+            if (!query) return
+
+            loading(true)
+
+            this.$bloodhound.search(
+                query,
+                (syncResults) => {
+                    this.options = syncResults
+                },
+                (asyncResults) => {
+                    loading(false)
+                    this.options = asyncResults
+                }
+            )
+        },
+    },
+}
+</script>
