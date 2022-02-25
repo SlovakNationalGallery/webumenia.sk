b4d5f9421060a1760146c3f2c731030517c5efc1
Ernest Walzel <ernest.walzel@sng.sk>
2022-02-25 09:48:55 +0100

    Change rem to 16px (until we retire Bootstrap)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,29 @@
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  important: '.tailwind-rules',
-  theme: {
-    fontSize: {
-      'xs': '10px',
-      'sm': '12px',
-      'base': '14px',
-      'lg': '16px',
-      'xl': '18px',
-      '2xl': '24px',
-      '3xl': '28px',
-      '4xl': '36px',
-      '5xl': '42px',
-    },
-    extend: {
-      colors: {
-        gray: {
-          200: '#ededed',
-          300: '#cdcdcd',
-          500: '#969696',
-          600: '#777',
-          800: '#333',
+    content: ['./resources/**/*.blade.php', './resources/**/*.js', './resources/**/*.vue'],
+    important: '.tailwind-rules',
+    theme: {
+        extend: {
+            colors: {
+                gray: {
+                    200: '#ededed',
+                    300: '#cdcdcd',
+                    500: '#969696',
+                    600: '#777',
+                    800: '#333',
+                },
+                sky: {
+                    300: '#66ccf4',
+                    400: '#37bcf1',
+                },
+            },
+            textDecorationThickness: {
+                3: '3px',
+            },
         },
-        sky: {
-          300: '#66ccf4',
-          400: '#37bcf1'
-        }
-      },
-      spacing: {
-        '104': '26rem',
-      },
-      textDecorationThickness: {
-        3: '3px',
-      }
     },
-  },
-  plugins: [],
-  prefix: 'tw-',
-  corePlugins: {
-    preflight: false, // TODO Not needed. Re-enable after switching from Bootstrap
-  }
-}
+    plugins: [],
+    prefix: 'tw-',
+    corePlugins: {
+        preflight: false, // TODO Not needed. Re-enable after switching from Bootstrap
+    },
+};
