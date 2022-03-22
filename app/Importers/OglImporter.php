2e325011263a8fe70536283332408f5701574812
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2022-03-22 13:03:32 +0100

    [import] authority matching

--- app/Importers/OglImporter.php
+++ app/Importers/OglImporter.php
@@ -2,8 +2,6 @@
 
 namespace App\Importers;
 
-use App\Repositories\IFileRepository;
-use Illuminate\Translation\Translator;
 use Illuminate\Support\Str;
 
 class OglImporter extends AbstractImporter
@@ -38,15 +36,16 @@ class OglImporter extends AbstractImporter
 
     public static $name = 'ogl';
 
-    public function __construct(IFileRepository $repository, Translator $translator) {
-        parent::__construct($repository, $translator);
+    protected function init()
+    {
         $this->sanitizers[] = function ($value) {
             return empty_to_null($value);
         };
     }
 
-    protected function getItemId(array $record) {
-        $id = sprintf('CZE:OGL.%s_%s', $record['Rada_S'], (int)$record['PorC_S']);
+    protected function getItemId(array $record)
+    {
+        $id = sprintf('CZE:OGL.%s_%s', $record['Rada_S'], (int) $record['PorC_S']);
         if ($record['Lomeni_S'] != '_') {
             $id = sprintf('%s-%s', $id, $record['Lomeni_S']);
         }
@@ -54,8 +53,13 @@ class OglImporter extends AbstractImporter
         return $id;
     }
 
-    protected function getItemImageFilenameFormat(array $record) {
-        $filename = sprintf('%s%s', $record['Rada_S'], str_pad($record['PorC_S'], 5, '0', STR_PAD_LEFT));
+    protected function getItemImageFilenameFormat(array $record)
+    {
+        $filename = sprintf(
+            '%s%s',
+            $record['Rada_S'],
+            str_pad($record['PorC_S'], 5, '0', STR_PAD_LEFT)
+        );
         if ($record['Lomeni_S'] != '_') {
             $filename = sprintf('%s%s', $filename, $record['Lomeni_S']);
         }
@@ -63,28 +67,31 @@ class OglImporter extends AbstractImporter
         return $filename . '{_*,}';
     }
 
-    protected function hydrateWorkType(array $record, $locale) {
+    protected function hydrateWorkType(array $record, $locale)
+    {
         $workType = config('demus.work_type')[$record['Skupina']];
         if ($locale !== 'cs') {
-            $workType = $this->translateAttribute("work_type", $workType, $locale);
+            $workType = $this->translateAttribute('work_type', $workType, $locale);
         }
 
         return $workType;
     }
 
-    protected function hydrateTopic(array $record, $locale) {
+    protected function hydrateTopic(array $record, $locale)
+    {
         $topic = Str::lower($record['Námět']);
         if ($locale !== 'cs') {
-            $topic = $this->translateAttribute("topic", $topic, $locale);
+            $topic = $this->translateAttribute('topic', $topic, $locale);
         }
 
         return $topic;
     }
 
-    protected function hydrateTechnique(array $record, $locale) {
+    protected function hydrateTechnique(array $record, $locale)
+    {
         $technika = $record['Technika'];
         if ($locale !== 'cs') {
-            $technika = $this->translateAttribute("technique", $record['Technika'], $locale);
+            $technika = $this->translateAttribute('technique', $record['Technika'], $locale);
         }
 
         if (!$record['TechSpec']) {
@@ -93,7 +100,7 @@ class OglImporter extends AbstractImporter
 
         $techSpec = $record['TechSpec'];
         if ($locale !== 'cs') {
-            $techSpec = $this->translateAttribute("technique", $techSpec, $locale);
+            $techSpec = $this->translateAttribute('technique', $techSpec, $locale);
         }
 
         if (!$technika || !$techSpec) {
@@ -103,12 +110,17 @@ class OglImporter extends AbstractImporter
         return "$technika, $techSpec";
     }
 
-    protected function hydrateMeasurement(array $record, $locale) {
+    protected function hydrateMeasurement(array $record, $locale)
+    {
         if (empty($record['SlužF']) || $record['SlužF'] === '=') {
             return null;
         }
 
-        $replacements = $this->translator->get('importer.demus.measurement_replacements', [], $locale);
+        $replacements = $this->translator->get(
+            'importer.demus.measurement_replacements',
+            [],
+            $locale
+        );
         return strtr($record['SlužF'], $replacements);
     }
 
--- tests/Importers/PnpImporterTest.php
+++ tests/Importers/PnpImporterTest.php
@@ -4,6 +4,7 @@ namespace Tests\Importers;
 
 use App\Import;
 use App\Importers\PnpImporter;
+use App\Matchers\AuthorityMatcher;
 use App\Repositories\CsvRepository;
 use Illuminate\Contracts\Translation\Translator;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -13,96 +14,115 @@ class PnpImporterTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testId() {
+    public function testId()
+    {
         $data = $this->fakeData(['Inventární číslo:' => 'A B/C']);
         $items = $this->importSingle($data);
         $this->assertEquals('CZE:PNP-A_B-C', $items[0]->id);
     }
 
-    public function testTitle() {
+    public function testTitle()
+    {
         $data = $this->fakeData();
         $items = $this->importSingle($data);
         $this->assertEquals($data['Název:'], $items[0]->title);
     }
 
-    public function testAuthor() {
+    public function testAuthor()
+    {
         $data = $this->fakeData();
         $items = $this->importSingle($data);
         $this->assertEquals($data['Autor:'], $items[0]->author);
     }
 
-    public function testDating() {
+    public function testDating()
+    {
         $data = $this->fakeData();
         $items = $this->importSingle($data);
         $this->assertEquals($data['Datace:'], $items[0]->dating);
     }
 
-    public function testDateEarliest() {
+    public function testDateEarliest()
+    {
         $data = $this->fakeData();
         $items = $this->importSingle($data);
         $this->assertEquals($data['Rok - od'], $items[0]->date_earliest);
     }
 
-    public function testDateLatest() {
+    public function testDateLatest()
+    {
         $data = $this->fakeData();
         $items = $this->importSingle($data);
         $this->assertEquals($data['Rok - do'], $items[0]->date_latest);
     }
 
-    public function testMeasurement() {
+    public function testMeasurement()
+    {
         $data = $this->fakeData();
         $items = $this->importSingle($data);
         $this->assertEquals($data['Rozměry:'], $items[0]->measurement);
     }
 
-    public function testWorkType() {
+    public function testWorkType()
+    {
         $data = $this->fakeData();
         $items = $this->importSingle($data);
         $this->assertEquals($data['Výtvarný druh:'], $items[0]->work_type);
     }
 
-    public function testTopic() {
+    public function testTopic()
+    {
         $data = $this->fakeData();
         $items = $this->importSingle($data);
         $this->assertEquals($data['Námět:'], $items[0]->topic);
     }
 
-    public function testMedium() {
+    public function testMedium()
+    {
         $data = $this->fakeData();
         $items = $this->importSingle($data);
         $this->assertEquals($data['Materiál:'], $items[0]->medium);
     }
 
-    public function testTechnique() {
+    public function testTechnique()
+    {
         $data = $this->fakeData();
         $items = $this->importSingle($data);
         $this->assertEquals($data['Technika:'], $items[0]->technique);
     }
 
-    public function testIdentifier() {
+    public function testIdentifier()
+    {
         $data = $this->fakeData();
         $items = $this->importSingle($data);
         $this->assertEquals($data['Inventární číslo:'], $items[0]->identifier);
     }
 
-    public function testInscription() {
+    public function testInscription()
+    {
         $data = $this->fakeData();
         $items = $this->importSingle($data);
         $this->assertEquals($data['Značení:'], $items[0]->inscription);
     }
 
-    public function testDescription() {
+    public function testDescription()
+    {
         $data = $this->fakeData();
         $items = $this->importSingle($data);
         $this->assertEquals($data[' Popis:'], $items[0]->description);
     }
 
-    protected function importSingle(array $data) {
+    protected function importSingle(array $data)
+    {
         $records = new \ArrayIterator([$data]);
         $repositoryMock = $this->createMock(CsvRepository::class);
         $repositoryMock->method('getFiltered')->willReturn($records);
 
-        $importer = new PnpImporter($repositoryMock, $this->app->get(Translator::class));
+        $importer = new PnpImporter(
+            $this->app->get(AuthorityMatcher::class),
+            $repositoryMock,
+            $this->app->get(Translator::class)
+        );
         $import = Import::create();
         $file = ['basename' => '', 'path' => ''];
         $items = $importer->import($import, $file);
@@ -113,7 +133,8 @@ class PnpImporterTest extends TestCase
         return $items;
     }
 
-    protected function fakeData(array $data = []) {
+    protected function fakeData(array $data = [])
+    {
         return $data + [
             'Název:' => $this->faker->word,
             'Autor:' => $this->faker->name,
