49944bf416c543fe71831eb44209c58e3ab00563
Ernest Walzel <ernest.walzel@sng.sk>
2022-03-24 12:15:18 +0100

    Add strings to CS/EN, pending translation

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -1,12 +1,6 @@
 <?php
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Language Lines for home.blade.php template
-    |--------------------------------------------------------------------------
-    */
-
     'definition_start' => 'web umenia is an online catalog of',
     'definition_end' => 'artworks',
     'from_galleries_start' => 'from the collections of',
@@ -18,4 +12,45 @@ return [
     'are_free_start' => 'of which',
     'are_free_end' => 'in public domain',
     'show_more_articles' => 'more articles',
+
+    'featured_piece' => [
+        'title' => 'Odporúčame',
+        'button_collection' => 'Prejsť na kolekciu',
+        'button_article' => 'Prejsť na článok',
+    ],
+
+    'featured_artwork' => [
+        'title' => 'Dielo dňa',
+        'button' => 'Viac o diele',
+    ],
+
+    'latest_content' => [
+        'collections' => [
+            'tab' => 'Kolekcie',
+            'button' => 'Všetky kolekcie',
+            'item_count_suffix' => 'v kolekcii',
+            'default_type' => 'kolekcia',
+            'promo_slide' => [
+                'line_1' => 'Na Webe umenia je ďalších viac',
+                'line_2' => 'ako :count kolekcií',
+                'link' => 'Zobraziť ďalšie kolekcie',
+            ],
+        ],
+        'articles' => [
+            'tab' => 'Články',
+            'button' => 'Všetky články',
+            'default_type' => 'článok',
+            'promo_slide' => [
+                'line_1' => 'Na Webe umenia je ďalších viac',
+                'line_2' => 'ako :count článkov',
+                'link' => 'Zobraziť ďalšie články',
+            ],
+        ],
+    ],
+
+    'featured_author' => [
+        'title' => 'Autor týždňa',
+        'button_author' => 'Zobraziť <strong>:count diel</strong> od autora',
+        'button_more' => 'zobraziť ďalších autorov',
+    ],
 ];
