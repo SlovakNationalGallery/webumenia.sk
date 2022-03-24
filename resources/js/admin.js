5d8bd6ae031c078cb1891df8d53cd87f498e642f
Ernest Walzel <ernest.walzel@sng.sk>
2022-03-24 14:10:21 +0100

    Croppr component

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,9 +1,10 @@
-window.debounce = require('debounce');
-window.Vue = require('vue').default;
+window.debounce = require('debounce')
+window.Vue = require('vue').default
 
-Vue.component('admin-links-input', require('./components/admin/LinksInput.vue').default);
-Vue.component('autocomplete', require('./components/Autocomplete.vue').default);
-Vue.component('linked-combos', require('./components/vue/linked-combos').default);
-Vue.component('query-string', require('./components/QueryString.vue').default);
+Vue.component('admin-links-input', require('./components/admin/LinksInput.vue').default)
+Vue.component('autocomplete', require('./components/Autocomplete.vue').default)
+Vue.component('linked-combos', require('./components/vue/linked-combos').default)
+Vue.component('query-string', require('./components/QueryString.vue').default)
+Vue.component('croppr', require('./components/Croppr.vue').default)
 
 new Vue({ el: '#wrapper' })
