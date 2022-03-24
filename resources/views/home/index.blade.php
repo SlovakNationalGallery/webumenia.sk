606477ff0b58f35dd75ad0297183a5706140f07c
Ernest Walzel <ernest.walzel@sng.sk>
2022-03-24 11:32:38 +0100

    Extract translatable strings WEBUMENIA-1804

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -34,7 +34,7 @@
                 <div
                     class="tw-container tw-relative tw-mx-auto tw-max-w-screen-xl tw-py-8 tw-px-6 tw-text-white md:tw-py-20">
                     <h2 class="tw-mt-36 tw-font-semibold tw-drop-shadow md:tw-mt-48 md:tw-text-lg">
-                        Odporúčame</h2>
+                        {{ trans('home.featured_piece.title') }}</h2>
                     <h3 class="tw-mt-4 tw-text-3xl tw-font-semibold tw-drop-shadow md:tw-text-6xl">
                         {{ $featuredPiece->title }}
                     </h3>
@@ -79,7 +79,7 @@
                     </div>
                     <h2
                         class="tw-mt-6 tw-mb-4 tw-font-semibold lg:tw-col-span-2 lg:tw-mt-0 lg:tw-text-lg">
-                        Dielo dňa
+                        {{ trans('home.featured_artwork.title') }}
                     </h2>
                     <div>
                         <h3 class="tw-text-3xl lg:tw-text-4xl">{{ $featuredArtwork->title }}</h3>
@@ -105,7 +105,7 @@
                         </div>
                         <a href="{{ route('dielo', ['id' => $featuredArtwork->item->id]) }}"
                             class="tw-mt-6 tw-inline-block tw-border tw-border-gray-300 tw-px-4 tw-py-2 tw-text-sm tw-transition tw-duration-300 hover:tw-border-gray-400 hover:tw-bg-white hover:tw-text-gray-800">
-                            Viac o diele
+                            {{ trans('home.featured_artwork.button') }}
                             <i class="fa icon-arrow-right tw-ml-2"></i>
                         </a>
                     </div>
@@ -113,21 +113,19 @@
             </div>
         @endif
 
+        {{-- Latest content --}}
         <div class="tw-container tw-mx-auto tw-max-w-screen-xl tw-px-6 tw-py-8 md:tw-py-16">
             <h2 class="tw-font-semibold md:tw-text-lg">Nový obsah</h2>
 
             <tabs-controller v-cloak v-slot="{ activeIndex }" class="tw-mt-4">
                 <div class="tw-flex tw-items-end">
                     <div class="tw-flex tw-grow tw-space-x-4">
-                        @foreach (['Kolekcie', 'Články'] as $tab)
+                        @foreach ([trans('home.latest_content.collections.tab'), trans('home.latest_content.articles.tab')] as $tab)
                             <tab v-slot="{ active }">
-                                {{-- blade-formatter-disable --}}
                                 <button
-                                    :class="[
-                                        'tw-transition-colors tw-font-semibold tw-text-2xl md:tw-text-4xl',
+                                    :class="['tw-transition-colors tw-font-semibold tw-text-2xl md:tw-text-4xl',
                                         !active && 'tw-text-gray-300 hover:tw-underline tw-underline-offset-[5px] md:tw-underline-offset-8 tw-decoration-[3px]'
                                     ]">{{ $tab }}</button>
-                            {{-- blade-formatter-enable --}}
                             </tab>
                         @endforeach
                     </div>
@@ -139,13 +137,13 @@
                         <a key="0" v-if="activeIndex === 0"
                             href="{{ route('frontend.collection.index') }}"
                             class="tw-hidden tw-border tw-border-gray-300 tw-px-4 tw-py-2 tw-text-sm tw-transition tw-duration-300 hover:tw-border-gray-400 hover:tw-bg-gray-300 sm:tw-inline-block">
-                            Všetky kolekcie
+                            {{ trans('home.latest_content.collections.button') }}
                             <i class="fa icon-arrow-right tw-ml-1 tw-mr-2"></i>
                         </a>
                         <a key="1" v-if="activeIndex === 1"
                             href="{{ route('frontend.article.index') }}"
                             class="tw-hidden tw-border tw-border-gray-300 tw-px-4 tw-py-2 tw-text-sm tw-transition tw-duration-300 hover:tw-border-gray-400 hover:tw-bg-gray-300 sm:tw-inline-block">
-                            Všetky články
+                            {{ trans('home.latest_content.articles.button') }}
                             <i class="fa icon-arrow-right tw-ml-1 tw-mr-2"></i>
                         </a>
                     </transition>
@@ -164,11 +162,12 @@
 
                                         <div
                                             class="tw-pointer-events-none tw-absolute tw-right-6 tw-top-6 tw-rounded-sm tw-bg-black/60 tw-px-1.5 tw-text-right tw-text-sm tw-text-white">
-                                            {{ $c->items_count }} diel v kolekcii
+                                            {{ trans_choice('general.artworks_counted', $c->items_count) }}
+                                            {{ trans('home.latest_content.collections.item_count_suffix') }}
                                         </div>
                                     </div>
                                     <span class="tw-mt-4 tw-inline-block tw-text-sm tw-text-gray-600">
-                                        {{ Str::ucfirst($c->type ?? 'kolekcia') }}
+                                        {{ Str::ucfirst($c->type ?? trans('home.latest_content.collections.default_type')) }}
                                     </span>
                                     <h4
                                         class="tw-truncate tw-text-lg tw-font-semibold tw-leading-tight tw-text-black">
@@ -186,11 +185,13 @@
                             @endforeach
                             <div
                                 class="tw-flex tw-h-48 tw-w-72 tw-flex-col tw-justify-center tw-bg-gray-200 tw-text-center tw-text-xl tw-font-semibold tw-text-black md:tw-w-[25rem]">
-                                <p>Na Webe umenia je ďalších viac<br /> ako
-                                    {{ $collectionsRemainingCount }} kolekcií</p>
+                                <p>
+                                    {{ trans('home.latest_content.collections.promo_slide.line_1') }}
+                                    <br />
+                                    {{ trans('home.latest_content.collections.promo_slide.line_2', ['count' => $collectionsRemainingCount]) }}
+                                </p>
                                 <a class="tw-mt-5 tw-underline tw-decoration-gray-300 tw-decoration-[3px] tw-underline-offset-4 hover:tw-decoration-current hover:tw-transition-colors"
-                                    href="{{ route('frontend.collection.index') }}">Zobraziť ďalšie
-                                    kolekcie</a>
+                                    href="{{ route('frontend.collection.index') }}">{{ trans('home.latest_content.collections.promo_slide.link') }}</a>
                             </div>
                         </x-home.carousel>
                     </tab-panel>
@@ -206,7 +207,7 @@
                                             class="tw-h-48 tw-object-cover tw-transition-opacity tw-duration-300 hover:tw-opacity-80">
                                     </a>
                                     <span class="tw-mt-4 tw-inline-block tw-text-sm tw-text-gray-600">
-                                        {{ Str::ucfirst($c->type ?? 'kolekcia') }}
+                                        {{ Str::ucfirst($c->type ?? trans('home.latest_content.articles.default_type')) }}
                                     </span>
                                     <h4
                                         class="tw-truncate tw-text-lg tw-font-semibold tw-leading-tight tw-text-black">
@@ -224,11 +225,13 @@
                             @endforeach
                             <div
                                 class="tw-flex tw-h-48 tw-w-72 tw-flex-col tw-justify-center tw-bg-gray-200 tw-text-center tw-text-xl tw-font-semibold tw-text-black md:tw-w-[25rem]">
-                                <p>Na Webe umenia je ďalších viac<br /> ako
-                                    {{ $articlesRemainingCount }} článkov</p>
+                                <p>
+                                    {{ trans('home.latest_content.articles.promo_slide.line_1') }}
+                                    <br />
+                                    {{ trans('home.latest_content.articles.promo_slide.line_2', ['count' => $articlesRemainingCount]) }}
+                                </p>
                                 <a class="tw-mt-5 tw-underline tw-decoration-gray-300 tw-decoration-[3px] tw-underline-offset-4 hover:tw-decoration-current hover:tw-transition-colors"
-                                    href="{{ route('frontend.article.index') }}">Zobraziť ďalšie
-                                    články</a>
+                                    href="{{ route('frontend.article.index') }}">{{ trans('home.latest_content.articles.promo_slide.link') }}</a>
                             </div>
                         </x-home.carousel>
                     </tab-panel>
@@ -245,7 +248,8 @@
                             class="tw-mt-12 tw-mr-6 tw-hidden tw-h-52 tw-w-52 tw-rounded-full lg:tw-block"
                             alt="{{ $featuredAuthor->formated_name }}">
                         <div>
-                            <h2 class="tw-font-semibold lg:tw-text-lg">Autor týždňa</h2>
+                            <h2 class="tw-font-semibold lg:tw-text-lg">
+                                {{ trans('home.featured_author.title') }}</h2>
                             <h3 class="tw-mt-4 tw-text-3xl tw-font-semibold lg:tw-text-4xl">
                                 <a href="{{ route('frontend.author.detail', $featuredAuthor) }}"
                                     class="tw-underline tw-decoration-gray-300 tw-underline-offset-4 tw-transition-colors hover:tw-decoration-current">
@@ -269,14 +273,13 @@
                             </div>
                             <a href="{{ route('frontend.author.detail', $featuredAuthor) }}"
                                 class="tw-mt-6 tw-inline-block tw-border tw-border-gray-300 tw-px-4 tw-py-2 tw-text-sm tw-transition tw-duration-300 hover:tw-border-gray-400 hover:tw-bg-white hover:tw-text-gray-800">
-                                Zobraziť <strong>{{ $featuredAuthor->items_count }} diel</strong> od
-                                autora
+                                {!! trans('home.featured_author.button_author', ['count' => $featuredAuthor->items_count]) !!}
                                 <i class="fa icon-arrow-right tw-ml-2"></i>
                             </a>
                             <br />
                             <a href="{{ route('frontend.author.index') }}"
                                 class="tw-mt-6 tw-hidden tw-cursor-pointer tw-text-sm tw-underline tw-decoration-gray-300 tw-underline-offset-4 tw-transition-colors hover:tw-decoration-current lg:tw-inline-block">
-                                zobraziť ďalších autorov
+                                {{ trans('home.featured_author.button_more') }}
                             </a>
                         </div>
                     </div>
@@ -296,7 +299,7 @@
                     </x-home.carousel>
                     <a href="{{ route('frontend.author.index') }}"
                         class="tw-mt-6 tw-inline-block tw-cursor-pointer tw-underline tw-decoration-gray-300 tw-underline-offset-4 tw-transition-colors hover:tw-decoration-current lg:tw-hidden">
-                        zobraziť ďalších autorov
+                        {{ trans('home.featured_author.button_more') }}
                     </a>
                 </div>
             </div>
