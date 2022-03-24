5570fc831102d29a5aae4e69589a2f96ea20be8b
Ernest Walzel <ernest.walzel@sng.sk>
2022-03-24 10:38:08 +0100

    Re-name lang files intro -> home

---
+++ resources/lang/sk/home.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Language Lines for home.blade.php template
+    |--------------------------------------------------------------------------
+    */
+
+    'definition_start' => 'web umenia je on-line katalóg',
+    'definition_end' => 'výtvarných diel',
+    'from_galleries_start' => 'zo zbierok',
+    'from_galleries_end' => 'slovenských a českých galérií',
+    'from_authors_start' => 'od',
+    'from_authors_end' => 'autorov',
+    'in_high_res_start' => 'z toho',
+    'in_high_res_end' => 'vo vysokom rozlíšení',
+    'are_free_start' => 'z toho',
+    'are_free_end' => 'autorsky voľných',
+    'show_more_articles' => 'viac článkov',
+];
