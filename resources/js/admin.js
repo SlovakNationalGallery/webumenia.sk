c9bd3e4d88352a91c52b10e72e031d5470218a32
Ernest Walzel <ernest.walzel@sng.sk>
2022-03-31 14:03:55 +0200

    Admin tabs component

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -6,5 +6,8 @@ Vue.component('autocomplete', require('./components/Autocomplete.vue').default)
 Vue.component('linked-combos', require('./components/vue/linked-combos').default)
 Vue.component('query-string', require('./components/QueryString.vue').default)
 Vue.component('croppr', require('./components/Croppr.vue').default)
+Vue.component('tabs-controller', require('./components/TabsController.vue').default)
+Vue.component('tab', require('./components/Tab.vue').default)
+Vue.component('tab-panel', require('./components/TabPanel.vue').default)
 
 new Vue({ el: '#wrapper' })
