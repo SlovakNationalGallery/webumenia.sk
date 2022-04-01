9d559579459abc43e888f1791e77facd76c2d94e
Ernest Walzel <ernest.walzel@sng.sk>
2022-04-01 12:22:13 +0200

    shuffle item filter forms

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,5 +1,8 @@
+import { Lang } from 'laravel-vue-lang'
+
 window.debounce = require('debounce')
 window.Vue = require('vue').default
+window.Vue.use(Lang, { fallback: 'sk' })
 
 Vue.component('admin-links-input', require('./components/admin/LinksInput.vue').default)
 Vue.component('autocomplete', require('./components/Autocomplete.vue').default)
@@ -9,5 +12,10 @@ Vue.component('croppr', require('./components/Croppr.vue').default)
 Vue.component('tabs-controller', require('./components/TabsController.vue').default)
 Vue.component('tab', require('./components/Tab.vue').default)
 Vue.component('tab-panel', require('./components/TabPanel.vue').default)
+Vue.component(
+    'admin.shuffle-item-filter',
+    require('./components/admin/ShuffleItemFilter.vue').default
+)
+Vue.component('list-manager', require('./components/utils/ListManager.vue').default)
 
 new Vue({ el: '#wrapper' })
---
+++ resources/js/components/utils/ListManager.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    props: {
+        // NOTE default items must have an `id` parameter
+        defaultItems: {
+            type: Array,
+        },
+    },
+    data() {
+        return {
+            id: 0,
+            items: this.defaultItems || [],
+        }
+    },
+    render() {
+        return this.$scopedSlots.default({
+            items: this.items,
+            push: (item) => this.items.push({ id: this.id++, ...item }),
+            delete: (index) => {
+                this.items.splice(index, 1)
+            },
+        })
+    },
+}
+</script>
