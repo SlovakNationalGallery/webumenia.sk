c96e52b29c20fb2a3a00cef9508b0ab57a8bedae
Ernest Walzel <ernest.walzel@sng.sk>
2022-04-07 13:19:54 +0200

    Remove ShuffleItemFilter & ListManager

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 import 'livewire-vue'
-import { Lang } from 'laravel-vue-lang'
 
 window.Vue = Vue
-Vue.use(Lang, { fallback: 'sk' })
-
 window.debounce = require('debounce')
 
 Vue.component('admin-links-input', require('./components/admin/LinksInput.vue').default)
@@ -15,10 +12,5 @@ Vue.component('croppr', require('./components/Croppr.vue').default)
 Vue.component('tabs-controller', require('./components/TabsController.vue').default)
 Vue.component('tab', require('./components/Tab.vue').default)
 Vue.component('tab-panel', require('./components/TabPanel.vue').default)
-Vue.component(
-    'admin.shuffle-item-filter',
-    require('./components/admin/ShuffleItemFilter.vue').default
-)
-Vue.component('list-manager', require('./components/utils/ListManager.vue').default)
 
 new Vue({ el: '#wrapper' })
