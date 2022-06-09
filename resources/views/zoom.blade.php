6dcd5265dd23cafa7e1bcdb198d16cd4e877123e
Ernest Walzel <ernest.walzel@sng.sk>
2022-06-09 16:07:19 +0200

    Page changes working

--- resources/views/zoom.blade.php
+++ resources/views/zoom.blade.php
@@ -40,11 +40,11 @@
             $fullIIPImgURLs = [...$fullIIPImgURLs, ...$fullIIPImgURLs, ...$fullIIPImgURLs];
         @endphp
 
-        <zoom-viewer v-cloak class="tw-h-screen tw-overflow-hidden tw-bg-green-300"
+        <zoom-viewer v-cloak class="tw-h-screen tw-overflow-hidden"
             :tile-sources={{ Js::from($fullIIPImgURLs) }}
             v-slot="{ thumbnailUrls, page, methods }">
 
-            <div class="tw-absolute tw-inset-0 tw-flex">
+            <div class="tw-pointer-events-none tw-absolute tw-inset-0 tw-flex">
 
                 {{-- Controls --}}
                 <div class="tw-flex tw-w-full tw-flex-col tw-justify-between tw-p-6">
@@ -53,7 +53,7 @@
                     <div class="tw-flex tw-justify-between">
 
                         {{-- Back button --}}
-                        <a class="tw-group tw-flex tw-items-center tw-justify-center tw-border tw-border-gray-300 tw-bg-white tw-px-4 tw-text-sm tw-uppercase tw-tracking-widest tw-opacity-70 tw-transition hover:tw-border-gray-400 hover:tw-bg-white hover:tw-text-gray-800 hover:tw-opacity-90"
+                        <a class="tw-group tw-pointer-events-auto tw-flex tw-items-center tw-justify-center tw-border tw-border-gray-300 tw-bg-white tw-px-4 tw-text-sm tw-uppercase tw-tracking-widest tw-opacity-70 tw-transition hover:tw-border-gray-400 hover:tw-bg-white hover:tw-text-gray-800 hover:tw-opacity-90"
                             href={{ $item->getUrl() }}>
                             <i
                                 class="fa fa-arrow-left tw-mr-2 -tw-ml-1.5 tw-align-[-0.05rem] tw-transition-transform group-hover:tw--translate-x-0.5"></i>
@@ -63,20 +63,20 @@
                         {{-- Control buttons --}}
                         <div class="tw-space-x-1.5 tw-text-lg">
                             <button v-on:click="methods.zoomIn"
-                                class="tw-h-10 tw-w-10 tw-bg-white tw-opacity-70 tw-transition hover:tw-bg-white hover:tw-text-gray-800 hover:tw-opacity-90 disabled:tw-opacity-30">
+                                class="tw-pointer-events-auto tw-h-10 tw-w-10 tw-bg-white tw-opacity-70 tw-transition hover:tw-bg-white/90 hover:tw-text-gray-800 hover:tw-opacity-90 active:tw-bg-white disabled:tw-opacity-30">
                                 <i class="fa fa-plus"></i>
                             </button>
                             <button v-on:click="methods.zoomOut"
-                                class="tw-h-10 tw-w-10 tw-bg-white tw-opacity-70 tw-transition hover:tw-bg-white hover:tw-text-gray-800 hover:tw-opacity-90 disabled:tw-opacity-30">
+                                class="tw-pointer-events-auto tw-h-10 tw-w-10 tw-bg-white tw-opacity-70 tw-transition hover:tw-bg-white/90 hover:tw-text-gray-800 hover:tw-opacity-90 active:tw-bg-white disabled:tw-opacity-30">
                                 <i class="fa fa-minus"></i>
                             </button>
                             <button v-on:click="methods.previousPage" :disabled="page === 0"
-                                class="tw-h-10 tw-w-10 tw-bg-white tw-opacity-70 tw-transition hover:tw-bg-white hover:tw-text-gray-800 hover:tw-opacity-90 disabled:tw-opacity-30">
+                                class="tw-pointer-events-auto tw-h-10 tw-w-10 tw-bg-white tw-opacity-70 tw-transition hover:tw-bg-white/90 hover:tw-text-gray-800 hover:tw-opacity-90 active:tw-bg-white disabled:tw-opacity-30">
                                 <i class="fa fa-arrow-up"></i>
                             </button>
                             <button v-on:click="methods.nextPage"
                                 :disabled="page === thumbnailUrls.length - 1"
-                                class="tw-h-10 tw-w-10 tw-bg-white tw-opacity-70 tw-transition hover:tw-bg-white hover:tw-text-gray-800 hover:tw-opacity-90 disabled:tw-opacity-30">
+                                class="tw-pointer-events-auto tw-h-10 tw-w-10 tw-bg-white tw-opacity-70 tw-transition hover:tw-bg-white/90 hover:tw-text-gray-800 hover:tw-opacity-90 active:tw-bg-white disabled:tw-opacity-30">
                                 <i class="fa fa-arrow-down"></i>
                             </button>
                         </div>
@@ -86,7 +86,8 @@
                     <div class="tw-relative">
 
                         {{-- Page indicator --}}
-                        <div class="tw-ml-32 tw-hidden tw-justify-center md:tw-flex">
+                        <div
+                            class="tw-pointer-events-auto tw-ml-32 tw-hidden tw-justify-center md:tw-flex">
                             <div
                                 class="tw-bg-white tw-px-4 tw-py-2 tw-opacity-70 tw-transition-opacity hover:tw-opacity-90">
                                 @{{ page + 1 }} / @{{ thumbnailUrls.length }}
@@ -95,7 +96,7 @@
 
                         {{-- Copyright indicator --}}
                         <div
-                            class="tw-absolute tw-inset-y-0 tw-right-0 tw-flex tw-items-center tw-bg-white tw-px-4 tw-py-2 tw-text-sm tw-opacity-70 tw-transition-opacity hover:tw-opacity-90">
+                            class="tw-pointer-events-auto tw-absolute tw-inset-y-0 tw-right-0 tw-flex tw-items-center tw-bg-white tw-px-4 tw-py-2 tw-text-sm tw-opacity-70 tw-transition-opacity hover:tw-opacity-90">
                             @if ($item->isFree())
                                 <img alt="Creative Commons License"
                                     class="tw-mr-1.5 tw-h-4 tw-opacity-70"
@@ -111,7 +112,7 @@
 
                 {{-- Reference strip --}}
                 <div v-dragscroll
-                    class="tw-flex tw-h-24 tw-flex-shrink-0 tw-overflow-auto tw-bg-white tw-bg-opacity-70 md:tw-h-full md:tw-w-32 md:tw-flex-col">
+                    class="tw-pointer-events-auto tw-flex tw-h-24 tw-flex-shrink-0 tw-overflow-auto tw-bg-white tw-bg-opacity-70 md:tw-h-full md:tw-w-32 md:tw-flex-col">
                     <img v-for="src, index in thumbnailUrls" :key="index" :src="src"
                         v-on:click="methods.setPage(index)"
                         :class="['tw-h-full md:tw-h-auto tw-p-2 md:tw-px-4 tw-border tw-cursor-pointer tw-transition-colors tw-border-sky-300', page === index ? 'tw-border-opacity-100' : 'tw-border-transparent hover:tw-border-sky-300/30']" />
