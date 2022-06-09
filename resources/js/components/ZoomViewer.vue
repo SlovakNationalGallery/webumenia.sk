7d60cd94749e3bbed72b6ba92210d22671b80f00
Ernest Walzel <ernest.walzel@sng.sk>
2022-06-09 15:34:34 +0200

    New Zoom controls

---
+++ resources/js/components/ZoomViewer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="tw-relative">
+        <div id="viewer" class="tw-absolute tw-inset-0"></div>
+        <slot :thumbnailUrls="thumbnailUrls" :page="page" :methods="methods" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['tileSources'],
+    data() {
+        return {
+            page: 0,
+        }
+    },
+    computed: {
+        thumbnailUrls() {
+            return this.tileSources.map((ts) => ts.replace(/\.dzi$/, '_files/0/0_0.jpg'))
+        },
+        methods() {
+            return {
+                setPage: (page) => (this.page = page),
+                nextPage: () => this.page++,
+                previousPage: () => this.page--,
+                zoomIn: () => {},
+                zoomOut: () => {},
+            }
+        },
+    },
+}
+</script>
