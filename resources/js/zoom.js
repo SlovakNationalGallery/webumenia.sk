27909508b2642a83eda87956440ec70e95a34005
Ernest Walzel <ernest.walzel@sng.sk>
2022-06-10 09:07:02 +0200

    Zoom buttons working

--- resources/js/zoom.js
+++ resources/js/zoom.js
@@ -3,9 +3,6 @@ import VueDragscroll from 'vue-dragscroll'
 
 Vue.use(VueDragscroll)
 
-window.$ = window.jQuery = require('jquery')
-window.OpenSeadragon = require('openseadragon')
-
 Vue.component('zoom-viewer', require('./components/ZoomViewer.vue').default)
 
 new Vue({ el: '#app' })
