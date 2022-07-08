c210f5b534053013bcc79fb074de82d759728965
Ernest Walzel <ernest.walzel@sng.sk>
2022-07-08 13:49:59 +0200

    Bring DatabaseSeeder in-line with v9 skeleton

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,25 +2,23 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder {
-
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		// Eloquent::unguard();
-
-		$this->call('UsersTableSeeder');
-		$this->call('HarvestsTableSeeder');
-		// $this->call('CollectionsTableSeeder');
-		$this->call('CategoriesTableSeeder');
-		// $this->call('ArticlesTableSeeder');
-		// $this->call('SketchbooksTableSeeder');
-	}
-
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call('UsersTableSeeder');
+        $this->call('HarvestsTableSeeder');
+        // $this->call('CollectionsTableSeeder');
+        $this->call('CategoriesTableSeeder');
+        // $this->call('ArticlesTableSeeder');
+        // $this->call('SketchbooksTableSeeder');
+    }
 }
