22231fc4fb8e3045efa2556aa8263db138a25511
Ernest Walzel <ernest.walzel@sng.sk>
2022-07-08 09:53:03 +0200

    Bring EventServiceProvider in-line with v9 skeleton

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -1,36 +1,31 @@
-<?php namespace App\Providers;
+<?php
+
+namespace App\Providers;
 
 use App\Authority;
 use App\Item;
-use App\Events\ItemPrimaryImageChanged;
-use App\Listeners\ItemPrimaryImageChangedListener;
-use App\Listeners\IncreaseRedirectCounter;
-use App\Observers\AuthorityObserver;
-use App\Observers\ItemObserver;
-use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Spatie\MissingPageRedirector\Events\RouteWasHit;
 
 class EventServiceProvider extends ServiceProvider
 {
-
     /**
-     * The event handler mappings for the application.
+     * The event to listener mappings for the application.
      *
-     * @var array
+     * @var array<class-string, array<int, class-string>>
      */
     protected $listen = [
-        'event.name' => [
-            'EventListener',
-        ],
-        ItemPrimaryImageChanged::class => [
-            ItemPrimaryImageChangedListener::class,
-        ],
-        RouteWasHit::class => [
-            IncreaseRedirectCounter::class,
-        ],
+        Registered::class => [SendEmailVerificationNotification::class],
+        ItemPrimaryImageChanged::class => [ItemPrimaryImageChangedListener::class],
+        RouteWasHit::class => [IncreaseRedirectCounter::class],
     ];
 
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
     public function boot()
     {
         Authority::observe(AuthorityObserver::class);
@@ -38,5 +33,15 @@ class EventServiceProvider extends ServiceProvider
 
         // disable scout observers
         Item::disableSearchSyncing();
+    }
+
+    /**
+     * Determine if events and listeners should be automatically discovered.
+     *
+     * @return bool
+     */
+    public function shouldDiscoverEvents()
+    {
+        return false;
     }
 }
