ae20556014012ae930f3fcc8084059445d905559
Ernest Walzel <ernest.walzel@sng.sk>
2022-07-21 08:42:44 +0200

    Fix authority link labels to be optional

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 All notable changes to this project will be documented in this file[^1].
 
 ## [Unreleased]
+### Fixed
+- Authority link labels to be optional
 
 ## [2.59.0] - 2022-07-15
 ### Changed
---
+++ tests/Feature/AuthorityTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Authority;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AuthorityTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testLinkLabelsAreGeneratedFromUrlAsFallback()
+    {
+        $authority = factory(Authority::class)->create();
+        $user = factory(User::class)->create(['role' => 'admin']);
+
+        $this->actingAs($user)->put(
+            route('authority.update', $authority),
+            array_merge($authority->getAttributes(), [
+                'externalLinks' => [
+                    [
+                        'id' => '',
+                        'url' => 'https://www.register-architektury.sk/architekti/titl-lubomir',
+                        'label' => '',
+                    ],
+                ],
+            ])
+        );
+
+        $this->assertEquals(
+            'www.register-architektury.sk',
+            $authority->externalLinks()->first()['label']
+        );
+    }
+}
