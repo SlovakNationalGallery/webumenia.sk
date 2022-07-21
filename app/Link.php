2dddba8b882d605c64a64fd87ea584a94ba00ee5
Ernest Walzel <ernest.walzel@sng.sk>
2022-07-21 08:42:44 +0200

    Fix authority link labels to be optional

--- app/Link.php
+++ app/Link.php
@@ -1,33 +1,15 @@
 <?php
 
-
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Link extends Model
 {
-
-    protected $fillable = array(
-        'url',
-        'label',
-        'type',
-    );
-
-    public static $rules = array(
-        'url' => 'nullable|url',
-        'label' => 'string|required',
-        'type' => 'in:external,source|required',
-    );
+    protected $fillable = ['url', 'label', 'type'];
 
     public function linkable()
     {
         return $this->morphTo();
-    }
-
-    public static function parse($url)
-    {
-        $url_parts = parse_url($url);
-        return $url_parts['host'];
     }
 }
