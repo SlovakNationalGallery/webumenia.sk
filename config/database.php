825c30ee3a8899fff5da70932f69504b6886d27e
Ernest Walzel <ernest.walzel@sng.sk>
2022-08-19 10:57:47 +0200

    Disable strict mode temporarily

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
             'collation' => 'utf8_slovak_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql')
                 ? array_filter([
