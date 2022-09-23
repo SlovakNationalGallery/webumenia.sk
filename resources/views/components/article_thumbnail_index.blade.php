01f4eff14d9c706c06e459dd15ac7f0e4d7e4e47
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2022-09-23 15:06:24 +0200

    style: according to design

--- resources/views/components/article_thumbnail_index.blade.php
+++ resources/views/components/article_thumbnail_index.blade.php
@@ -6,8 +6,8 @@ $showEduTags = $showEduTags ?? false;
 @endphp
 <div class="tailwind-rules">
     <div
-        class="tw-container tw-mx-auto tw-flex tw-h-[475px] tw-max-w-screen-xl tw-flex-wrap tw-px-6 tw-py-4 md:tw-h-[400px] md:tw-flex-nowrap md:tw-py-8">
-        <div class="tw-h-1/3 tw-w-full md:tw-mr-4 md:tw-h-full md:tw-w-1/2">
+        class="tw-container tw-mx-auto tw-flex tw-h-[30rem] tw-max-w-screen-xl tw-flex-wrap tw-px-6 tw-py-4 md:tw-h-[25.75rem] md:tw-flex-nowrap">
+        <div class="tw-h-1/3 tw-w-full md:tw-h-full md:tw-w-1/2">
             @if ($article->main_image)
                 <a href="{{ $url }}" class="tw-h-full">
                     <img src="{!! $article->getThumbnailImage() !!}"
@@ -15,13 +15,13 @@ $showEduTags = $showEduTags ?? false;
                 </a>
             @endif
         </div>
-        <div class="tw-flex tw-h-2/3 tw-w-full tw-flex-col tw-pt-4 md:tw-pt-0 md:tw-ml-4 md:tw-max-h-full md:tw-w-1/2">
+        <div class="tw-flex tw-h-2/3 tw-w-full md:tw-h-full md:tw-box-border tw-flex-col tw-pt-4 md:tw-px-12 md:tw-py-10 md:tw-max-h-full md:tw-w-1/2">
             @if ($article->category?->name)
                 <div class="tw-pb-3 tw-text-lg tw-font-medium tw-text-gray-600">
                     {{ $article->category->name }}
                 </div>
             @endif
-            <div class="tw-pb-2 tw-text-2xl tw-font-semibold">
+            <div class="tw-pb-2 tw-text-4xl tw-font-semibold">
                 <a href="{{ $url }}">
                     {{ $article->title }}
                 </a>
@@ -31,7 +31,7 @@ $showEduTags = $showEduTags ?? false;
                     {{ $article->author }}</span> &nbsp;&middot;&nbsp;
                 {!! $article->created_at->format('d. m. Y') !!}
             </div>
-            <div class="tw-mb-3 tw-min-h-0 tw-overflow-hidden tw-font-serif">
+            <div class="tw-mb-9 tw-min-h-0 tw-overflow-hidden tw-text-xl tw-font-serif">
                 {{ strip_tags(html_entity_decode($article->summary)) }}
             </div>
             <div class="tw-text-sm tw-text-gray-600">
