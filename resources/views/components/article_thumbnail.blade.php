32148bbbe1e35625c7ce3374789e1aa149864dbe
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2022-09-25 19:33:25 +0200

    chore: article thumbnail component format with prettier

--- resources/views/components/article_thumbnail.blade.php
+++ resources/views/components/article_thumbnail.blade.php
@@ -1,8 +1,8 @@
 @props([
-    'article' => NULL,
+    'article' => null,
     'url' => $article->getUrl(),
     'showEduTags' => false,
-    ])
+])
 
 <div class="tailwind-rules">
     <div
@@ -15,7 +15,8 @@
                 </a>
             @endif
         </div>
-        <div class="tw-flex tw-h-2/3 tw-w-full md:tw-h-full md:tw-box-border tw-flex-col tw-pt-4 md:tw-px-12 md:tw-py-10 md:tw-max-h-full md:tw-w-1/2">
+        <div
+            class="tw-flex tw-h-2/3 tw-w-full tw-flex-col tw-pt-4 md:tw-box-border md:tw-h-full md:tw-max-h-full md:tw-w-1/2 md:tw-px-12 md:tw-py-10">
             @if ($article->category?->name)
                 <div class="tw-pb-3 tw-text-lg tw-font-medium tw-text-gray-600">
                     {{ $article->category->name }}
@@ -29,9 +30,10 @@
             <div class="tw-pb-4 tw-text-base tw-text-gray-600">
                 <span class="hover:tw-underline">
                     {{ $article->author }}</span> &nbsp;&middot;&nbsp;
-                    @dateShort($article->created_at)
+                @dateShort($article->created_at)
             </div>
-            <div class="tw-mb-9 tw-flex-1 tw-min-h-0 tw-overflow-hidden tw-text-xl tw-font-serif tw-text-transparent tw-bg-clip-text tw-bg-gradient-to-b tw-from-gray-800 tw-via-gray-800 tw-to-white">
+            <div
+                class="tw-mb-9 tw-min-h-0 tw-flex-1 tw-overflow-hidden tw-bg-gradient-to-b tw-from-gray-800 tw-via-gray-800 tw-to-white tw-bg-clip-text tw-font-serif tw-text-xl tw-text-transparent">
                 {{ strip_tags(html_entity_decode($article->summary)) }}
             </div>
             <div class="tw-text-sm tw-text-gray-600">
