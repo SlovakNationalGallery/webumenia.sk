86cb2b1cbae6dabaad4eeaf804884e8ad8e72e00
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2022-10-20 12:07:56 +0200

    fix: remove url from props

--- resources/views/components/article_teaser.blade.php
+++ resources/views/components/article_teaser.blade.php
@@ -1,19 +1,19 @@
-@props(['article', 'url'])
+@props(['article'])
 
 <div>
     @if ($article->main_image)
-        <a href="{{ $url }}" class="image-container">
+        <a href="{{ $article->getUrl() }}" class="image-container">
             <img data-src="{!! $article->getThumbnailImage() !!}" class="img-responsive lazyload"
                 alt="{{ $article->title }}">
         </a>
     @endif
-    <a href="{{ $url }}">
+    <a href="{{ $article->getUrl() }}">
         <h4 class="title">
             {!! $article->title_with_category !!}
         </h4>
     </a>
     <div class="attributes">{!! $article->getShortTextAttribute($article->summary, 250) !!}
-        (<a href="{{ $url }}">{{ trans('general.more') }}</a>)
+        (<a href="{{ $article->getUrl() }}">{{ trans('general.more') }}</a>)
     </div>
     <p class="meta mt-3">{!! $article->published_date !!} / {!! $article->author !!}</p>
 </div>
