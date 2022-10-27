af7d113d58d3447abf78b4355dc2fc82e6b87b29
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2022-10-27 14:49:46 +0200

    feat: add list of buttons for mobile (#780)
    * feat: add list of buttons for mobile
    
    * feat: add multi-select (#782)

--- resources/js/app.js
+++ resources/js/app.js
@@ -26,9 +26,9 @@ import 'livewire-vue'
 import { Lang } from 'laravel-vue-lang';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faCaretDown, faCaretUp, faRotateLeft } from '@fortawesome/free-solid-svg-icons'
+import { faCaretDown, faCaretUp, faRotateLeft, faChevronLeft, faXmark, faCaretRight, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faCaretDown, faCaretUp, faRotateLeft)
+library.add(faCaretDown, faCaretUp, faRotateLeft, faChevronLeft, faXmark, faCaretRight, faMagnifyingGlass)
 
 
 window.Vue = Vue
