4e5dd5dd8ca8752c904dc8c290753d4e29067043
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2022-11-07 22:31:03 +0100

    [import] constant class_name in factory

--- database/factories/ImportFactory.php
+++ database/factories/ImportFactory.php
@@ -2,12 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Importers\GmuRnlImporter;
-use App\Importers\MgImporter;
-use App\Importers\OglImporter;
-use App\Importers\PnpImporter;
-use App\Importers\PnpKarasekImporter;
-use App\Importers\PnpTrienaleImporter;
 use App\Importers\WebumeniaMgImporter;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -25,15 +19,7 @@ class ImportFactory extends Factory
     {
         return [
             'name' => fake()->text(),
-            'class_name' => fake()->randomElement([
-                GmuRnlImporter::class,
-                MgImporter::class,
-                OglImporter::class,
-                PnpImporter::class,
-                PnpKarasekImporter::class,
-                PnpTrienaleImporter::class,
-                WebumeniaMgImporter::class,
-            ]),
+            'class_name' => WebumeniaMgImporter::class,
         ];
     }
 }
