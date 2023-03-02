b5bad50b796bfecf191a4776237287757ff707f5
Ernest Walzel <ernest.walzel@sng.sk>
2023-03-02 13:52:31 +0100

    Re-factor test

--- tests/Feature/Api/V1/ItemsTest.php
+++ tests/Feature/Api/V1/ItemsTest.php
@@ -13,20 +13,11 @@ class ItemsTest extends TestCase
     use RefreshDatabase;
     use RecreateSearchIndex;
 
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        Item::factory()->create(['topic' => 'spring', 'has_image' => true]);
-        Item::factory()->create(['topic' => 'summer']);
-        Item::factory()->create(['topic' => 'autumn']);
-
-        // Ensure the index is up-to-date
-        app(ItemRepository::class)->refreshIndex();
-    }
-
     public function test_no_filters_applied()
     {
+        Item::factory()->count(3)->create();
+        app(ItemRepository::class)->refreshIndex();
+
         $url = route('api.v1.items.index');
 
         $this->getJson($url)->assertJson([
@@ -36,6 +27,10 @@ class ItemsTest extends TestCase
 
     public function test_single_term_filtering()
     {
+        Item::factory()->create(['topic' => 'spring']);
+        Item::factory()->create(['topic' => 'summer']);
+        app(ItemRepository::class)->refreshIndex();
+
         $url = route('api.v1.items.index', [
             'filter[topic]' => 'spring',
         ]);
@@ -47,6 +42,11 @@ class ItemsTest extends TestCase
 
     public function test_multi_terms_filtering()
     {
+        Item::factory()->create(['topic' => 'spring']);
+        Item::factory()->create(['topic' => 'summer']);
+        Item::factory()->create(['topic' => 'autumn']);
+        app(ItemRepository::class)->refreshIndex();
+
         $url = route('api.v1.items.index', [
             'filter[topic]' => ['spring', 'summer'],
         ]);
@@ -58,9 +58,18 @@ class ItemsTest extends TestCase
 
     public function test_filtering_by_boolean_values()
     {
+        Item::factory()->create(['has_image' => true]);
+        Item::factory()->create(['has_image' => false]);
+        app(ItemRepository::class)->refreshIndex();
+
         $url = route('api.v1.items.index', [
             'filter[has_image]' => 'true',
         ]);
+
+        $this->getJson($url)->assertJson([
+            'total' => 1,
+        ]);
+    }
 
         $this->getJson($url)->assertJson([
             'total' => 1,
