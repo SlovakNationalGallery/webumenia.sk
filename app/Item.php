cad942db3921dafe329b474ecc598bafa16a0f79
Ernest Walzel <ernest.walzel@sng.sk>
2023-03-02 12:52:36 +0100

    Remove unused code

--- app/Item.php
+++ app/Item.php
@@ -808,23 +808,4 @@ class Item extends Model implements IndexableModel, TranslatableContract
     {
         return sprintf('%s%s', config('app.url'), $this->getImagePath());
     }
-
-    public static function filterQuery(array $filter, BoolQueryBuilder $builder = null)
-    {
-        $builder = $builder ?: new BoolQueryBuilder();
-        foreach ($filter as $field => $value) {
-            if (is_string($value) && in_array($field, self::$filterables, true)) {
-                $builder->filter('term', [$field => $value]);
-            } else if (is_array($value) && in_array($field, self::$rangeables, true)) {
-                $range = collect($value)
-                    ->only(['lt', 'lte', 'gt', 'gte'])
-                    ->transform(function ($value) {
-                        return (string)$value;
-                    })
-                    ->all();
-                $builder->filter('range', [$field => $range]);
-            }
-        }
-        return $builder;
-    }
 }
