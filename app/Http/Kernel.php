f306805e5ba9e3c08c5bb427b4f61d806acf82f6
Ernest Walzel <ernest.walzel@sng.sk>
2023-03-16 15:38:56 +0100

    $routeMiddleware -> $middlewareAliases

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -48,13 +48,13 @@ class Kernel extends HttpKernel
     ];
 
     /**
-     * The application's route middleware.
+     * The application's middleware aliases.
      *
-     * These middleware may be assigned to groups or used individually.
+     * Aliases may be used to conveniently assign middleware to routes and groups.
      *
      * @var array<string, class-string|string>
      */
-    protected $routeMiddleware = [
+    protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
