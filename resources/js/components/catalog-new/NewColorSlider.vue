3983d92511772f672341018546895c82c4390f80
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2023-03-21 22:45:40 +0100

    feat: rework color slider

--- resources/js/components/catalog-new/NewColorSlider.vue
+++ resources/js/components/catalog-new/NewColorSlider.vue
@@ -1,35 +1,82 @@
 <template>
-    <div>
-        <div class="hue">
-            <slider
-                :pointerColorFn="hueColorFn"
-                :value="getHue()"
-                :min="0"
-                :max="360"
-                :step="1"
-                @change="hueChange"
-            />
-        </div>
-        <div v-if="color">
-            <slider
-                :bgColorFn="lightnessBgColorFn"
-                :pointerColorFn="lightnessColorFn"
-                @change="lightnessChange"
-                :value="getLightness()"
-                :min="0"
-                :step="0.01"
-                :max="1"
-            />
-        </div>
+    <div class="tw-mt-6">
+        <slider
+            tooltip="none"
+            :value="getHue()"
+            :min="0"
+            :max="360"
+            :duration="0"
+            :dotSize="24"
+            :height="12"
+            :process="false"
+            :railStyle="hueBgColorFn()"
+            lazy
+            class="tw-cursor-pointer"
+            @dragging="hueChange"
+            @change="$emit('change', color)"
+        >
+            <template #dot>
+                <svg
+                    width="24"
+                    height="25"
+                    viewBox="0 0 24 25"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <circle cx="12" cy="12.6421" r="12" :fill="hueColorFn(getHue())" />
+                    <path opacity="0.4" d="M9 6.64209V18.6421" stroke="black" />
+                    <path opacity="0.4" d="M15 6.64209V18.6421" stroke="black" />
+                </svg>
+            </template>
+        </slider>
+        <slider
+            v-if="color"
+            tooltip="none"
+            :value="getLightness()"
+            :min="0"
+            :max="1"
+            :interval="0.01"
+            :duration="0"
+            :dotSize="24"
+            :height="12"
+            :process="false"
+            :railStyle="lightnessBgColorFn()"
+            lazy
+            class="tw-cursor-pointer tw-mt-3"
+            @dragging="lightnessChange"
+            @change="$emit('change', color)"
+        >
+            <template #dot>
+                <svg
+                    width="24"
+                    height="25"
+                    viewBox="0 0 24 25"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <circle cx="12" cy="12.6421" r="12" :fill="`#${color}`" />
+                    <path opacity="0.4" d="M9 6.64209V18.6421" stroke="black" />
+                    <path opacity="0.4" d="M15 6.64209V18.6421" stroke="black" />
+                </svg>
+            </template>
+        </slider>
     </div>
 </template>
-
 <script>
 import tinycolor from 'tinycolor2'
+import VueSlider from 'vue-slider-component'
 
 export default {
     props: {
-        color: String,
+        defaultColor: String,
+    },
+    data() {
+        return {
+            color: this.defaultColor,
+        }
+    },
+    components: {
+        slider: VueSlider,
     },
     methods: {
         getHue() {
@@ -44,16 +91,19 @@ export default {
             return 'hsl(' + hue + ', 80%, 50%)'
         },
         lightnessColorFn(lightness) {
-            return 'hsl(' + this.getHue() + ',80% ,' + lightness * 100 + '%)'
+            return 'hsl(' + this.getHue() + ',80% ,' + lightness + '%)'
         },
         hueChange(hue) {
-            const color = tinycolor(`hsl(${hue}, 0.8, ${this.getLightness() || 0.5})`).toHex()
-            this.$emit('change', color)
+            this.color = tinycolor(`hsl(${hue}, 0.8, ${this.getLightness() || 0.5})`).toHex()
         },
-
         lightnessChange(lightness) {
-            const color = tinycolor(`hsl(${this.getHue()}, 80%, ${lightness * 100}%)`).toHex()
-            this.$emit('change', color)
+            this.color = tinycolor(`hsl(${this.getHue()}, 80%, ${lightness * 100}%)`).toHex()
+        },
+        hueBgColorFn() {
+            return {
+                background:
+                    'linear-gradient(to right, #d82626 0%, #d8d826 17%, #26d826 33%, #26d8d8 50%, #2626d8 67%, #d826d8 83%, #d82626 100%)',
+            }
         },
         lightnessBgColorFn() {
             return {
@@ -70,18 +120,3 @@ export default {
     },
 }
 </script>
-
-<style>
-.hue {
-    background: linear-gradient(
-        to right,
-        #d82626 0%,
-        #d8d826 17%,
-        #26d826 33%,
-        #26d8d8 50%,
-        #2626d8 67%,
-        #d826d8 83%,
-        #d82626 100%
-    );
-}
-</style>
