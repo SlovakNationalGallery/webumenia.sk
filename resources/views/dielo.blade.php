008ca07f84b5292e3c95032792c78a08896dc5ba
Ernest Walzel <ernest.walzel@sng.sk>
2023-03-23 10:14:24 +0100

    Fix artwork map styling

--- resources/views/dielo.blade.php
+++ resources/views/dielo.blade.php
@@ -645,7 +645,116 @@
 
         map.addStyle({
             styledMapName:"Styled Map",
-            styles: light_style,
+            styles: [
+                {
+                    "elementType": "all",
+                    "featureType": "water",
+                    "stylers": [
+                        {
+                            "hue": "#e9ebed"
+                        },
+                        {
+                            "saturation": -78
+                        },
+                        {
+                            "lightness": 67
+                        },
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "elementType": "all",
+                    "featureType": "landscape",
+                    "stylers": [
+                        {
+                            "hue": "#ffffff"
+                        },
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "lightness": 100
+                        },
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "elementType": "geometry",
+                    "featureType": "road",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "elementType": "all",
+                    "featureType": "poi",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "elementType": "geometry",
+                    "featureType": "road.local",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "elementType": "all",
+                    "featureType": "transit",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "elementType": "all",
+                    "featureType": "administrative.locality",
+                    "stylers": [
+                        {
+                            "hue": "#ffffff"
+                        },
+                        {
+                            "saturation": 7
+                        },
+                        {
+                            "lightness": 0
+                        },
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                },
+                {
+                    "elementType": "labels",
+                    "featureType": "road",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "elementType": "labels",
+                    "featureType": "road.arterial",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                }
+            ],
             mapTypeId: "map_style"
         });
 
