b497ec6dbc428873b3a725f4e4b3673a934b64e6
Adam Plesnik <adam.plesnik@expersoft.com>
2023-04-20 14:37:14 +0200

    Move resource CSS to resources, update admin.less and style.less imports
    * move all CSS resources to resources/
    * keep only generated CSS in public/css/
    * put all imports into admin.less and style.less respectively, remove it from PHP
    * remove more unused less files

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -18,15 +18,6 @@
         <script src="https://use.fontawesome.com/73587c90bb.js"></script>
         <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="{{ mix('/css/app-tailwind.css') }}" />
-        {!! Html::style('css/admin/sb-admin.css') !!}
-        {!! Html::style('css/admin/ladda-themeless.min.css') !!}
-        {!! Html::style('css/admin/bootstrap-wysihtml5.css') !!}
-        {!! Html::style('css/admin/bootstrap-colorpicker.min.css') !!}
-        {!! Html::style('css/admin/selectize.css') !!}
-        {!! Html::style('css/admin/selectize.bootstrap3.css') !!}
-        {!! Html::style('css/admin/bootstrap-switch.css') !!}
-        {!! Html::style('css/admin/jquery-ui.min.css') !!}
-        {!! Html::style('css/admin/vue-select.css') !!}
         {!! Html::style(mix('/css/admin.css')) !!}
 </head>
 
