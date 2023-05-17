086c1eeb6f842050a39ca8d00d5f691b94aef78a
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2023-05-17 14:41:29 +0200

    fix: make view_header label clickable

--- resources/views/components/filter/view_header_button.blade.php
+++ resources/views/components/filter/view_header_button.blade.php
@@ -1,4 +1,4 @@
-<button {{ $attributes->merge(['class' => 'tw-pr-2 tw-flex tw-items-center tw-py-0.5']) }}>
-    <x-icons.caret-left class="tw-h-6 tw-w-6 tw-fill-current"></x-icons.caret-left>
+<button {{ $attributes->merge(['class' => 'tw-flex tw-items-center tw-py-0.5']) }}>
+    <x-icons.caret-left class="tw-h-6 tw-w-6 tw-fill-current tw-pr-2"></x-icons.caret-left>
     {{ $slot }}
 </button>
