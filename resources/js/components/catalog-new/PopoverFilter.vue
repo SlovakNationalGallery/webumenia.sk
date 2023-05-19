a437bb5270e921dd8f849e0878f892a57212844d
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2023-05-19 09:34:22 +0200

    style: make filter popover button smaller (#853)
    * style: make filter popover button smaller
    style: leave space for selectionCount
    style: stop border from jumping up and down
    
    * feat: requested changes
    
    * feat: requested changes 2

--- resources/js/components/catalog-new/PopoverFilter.vue
+++ resources/js/components/catalog-new/PopoverFilter.vue
@@ -1,41 +1,48 @@
 <template>
-    <div class="tw-pb-2 tw-z-10">
-        <button
+    <div class="tw-pb-2">
+        <div
             ref="button"
-            class="tw-border tw-border-gray-300 tw-bg-white tw-py-3.5 tw-px-4 tw-text-lg tw-font-bold hover:tw-border-gray-800"
+            class="tw-border tw-border-transparent"
             :class="{
-                'tw-border-gray-800': active,
-                'tw-border-gray-800 tw-border-2': isOpen,
+                'tw-border-gray-800': isOpen,
             }"
-            @click="togglePopover(name)"
         >
-            <div class="tw-flex">
-                <slot name="popover-label"></slot>
-                <div class="tw-flex tw-items-center tw-pl-4">
-                    <svg
-                        v-if="isOpen"
-                        class="tw-fill-sky-400 tw-w-4 tw-h-4"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 256 256"
-                    >
-                        <path
-                            d="M216.49,168.49a12,12,0,0,1-17,0L128,97,56.49,168.49a12,12,0,0,1-17-17l80-80a12,12,0,0,1,17,0l80,80A12,12,0,0,1,216.49,168.49Z"
-                        ></path>
-                    </svg>
-                    <svg
-                        v-else
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="tw-w-4 tw-h-4 tw-fill-current"
-                        viewBox="0 0 256 256"
-                    >
-                        <path
-                            d="M216.49,104.49l-80,80a12,12,0,0,1-17,0l-80-80a12,12,0,0,1,17-17L128,159l71.51-71.52a12,12,0,0,1,17,17Z"
-                        ></path>
-                    </svg>
+            <button
+                ref="button"
+                class="tw-border tw-border-gray-300 tw-bg-white tw-py-2.5 tw-px-4 tw-text-lg tw-font-bold hover:tw-border-gray-800"
+                :class="{
+                    'tw-border-gray-800': active || isOpen,
+                }"
+                @click="togglePopover(name)"
+            >
+                <div class="tw-flex">
+                    <slot name="popover-label"></slot>
+                    <div class="tw-flex tw-items-center tw-pl-4">
+                        <svg
+                            v-if="isOpen"
+                            class="tw-fill-sky-400 tw-w-4 tw-h-4"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 256 256"
+                        >
+                            <path
+                                d="M216.49,168.49a12,12,0,0,1-17,0L128,97,56.49,168.49a12,12,0,0,1-17-17l80-80a12,12,0,0,1,17,0l80,80A12,12,0,0,1,216.49,168.49Z"
+                            ></path>
+                        </svg>
+                        <svg
+                            v-else
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="tw-w-4 tw-h-4 tw-fill-current"
+                            viewBox="0 0 256 256"
+                        >
+                            <path
+                                d="M216.49,104.49l-80,80a12,12,0,0,1-17,0l-80-80a12,12,0,0,1,17-17L128,159l71.51-71.52a12,12,0,0,1,17,17Z"
+                            ></path>
+                        </svg>
+                    </div>
                 </div>
-            </div>
-        </button>
-        <div ref="body">
+            </button>
+        </div>
+        <div ref="body" class="tw-z-10">
             <div @click.stop v-if="isOpen" v-on-clickaway="closeOpenedPopover" id="popover">
                 <slot name="body"></slot>
             </div>
