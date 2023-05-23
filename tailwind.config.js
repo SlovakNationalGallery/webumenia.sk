53390e6c652d1277d2b598cf7522d0cac1da1163
Adam Plesnik <adam.plesnik@expersoft.com>
2023-05-23 07:50:07 +0200

    Remove @tailwindcss/line-clamp, it is included in TW3.3 by default.

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,7 +35,6 @@ module.exports = {
     },
     plugins: [
         require('@tailwindcss/typography'),
-        require('@tailwindcss/line-clamp'),
         require('@tailwindcss/forms'),
         require('tailwind-scrollbar')({ nocompatible: true }),
     ],
