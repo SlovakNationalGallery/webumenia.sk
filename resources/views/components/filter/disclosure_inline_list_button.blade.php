a8b6b573bdb12e5222fbcb776a5316b870c320aa
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2023-06-07 10:53:57 +0200

    fix: opening of filter fix: hover on mobile inline list

--- resources/views/components/filter/disclosure_inline_list_button.blade.php
+++ resources/views/components/filter/disclosure_inline_list_button.blade.php
@@ -1,13 +1,13 @@
-<toggle-controller v-slot="{isOpen, toggle}">
-    <div class="tw-border-b tw-border-gray-300 hover:tw-border-gray-800">
+<toggle-controller v-slot="{isOn, toggle}">
+    <div class="tw-border-b tw-border-gray-300">
         <button
-            class="tw-flex tw-w-full tw-items-center tw-justify-between tw-bg-white tw-py-3.5 tw-px-4 hover:tw-border-gray-800"
+            class="tw-flex tw-w-full tw-items-center tw-justify-between tw-bg-white tw-py-3.5 tw-px-4"
             v-on:click="toggle">
             {{ $header }}
-            <x-icons.caret-down class="tw-h-4 tw-w-4 tw-fill-current" v-if="isOpen" />
+            <x-icons.caret-down class="tw-h-4 tw-w-4 tw-fill-current" v-if="isOn" />
             <x-icons.caret-up class="tw-h-4 tw-w-4 tw-fill-current" v-else />
         </button>
-        <div v-if="isOpen" class="tw-px-4 tw-pb-6">
+        <div v-if="isOn" class="tw-px-4 tw-pb-6">
             {{ $body }}
         </div>
     </div>
