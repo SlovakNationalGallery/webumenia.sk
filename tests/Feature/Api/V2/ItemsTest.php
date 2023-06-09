6b65a7bfc6adea36ec4e4aabdef5a4784c6be1c5
Ernest Walzel <ernest.walzel@sng.sk>
2023-06-09 13:42:35 +0200

    Add test for /api/v2/items/{id}

---
+++ tests/Feature/Api/V2/ItemsTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature\Api\V2;
+
+use App\Authority;
+use App\Item;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ItemsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_detail()
+    {
+        $authority = Authority::factory()->create(['name' => 'Wouwerman, Philips']);
+
+        $item = Item::factory()->create([
+            'id' => 'test_id',
+            'title' => 'test_title',
+            'author' => 'Věšín, Jaroslav',
+            'dating' => 2000,
+            'date_earliest' => 2000,
+            'date_latest' => 2010,
+            'description' => 'test_description',
+        ]);
+        $item->authorities()->attach($authority);
+
+        $this->getJson('/api/v2/items/test_id')->assertExactJson([
+            'data' => [
+                'id' => 'test_id',
+                'title' => 'test_title',
+                'authorities' => [
+                    [
+                        ...$authority->only([
+                            'id',
+                            'name',
+                            'biography',
+                            'has_image',
+                            'birth_date',
+                            'death_date',
+                            'birth_place',
+                            'death_place',
+                            'image_path',
+                        ]),
+                        'image_path' => $authority->getImagePath(),
+                    ],
+                ],
+                'authors' => ['Wouwerman, Philips', 'Věšín, Jaroslav'],
+                'dating' => '2000',
+                'date_earliest' => 2000,
+                'date_latest' => 2010,
+                'description' => 'test_description',
+            ],
+        ]);
+    }
+}
