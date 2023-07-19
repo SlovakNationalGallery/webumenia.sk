4ad2699cc2ea5ab13ef4df832605bd17f7561402
Igor Rjabinin <igor.rjabinin@gmail.com>
2023-07-19 14:32:01 +0200

    add command GenerateImageRatios

---
+++ app/Console/Commands/GenerateImageRatios.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Item;
+
+class GenerateImageRatios extends Command
+{
+    protected $signature = 'images:generate-ratios';
+    protected $description = 'Generate image ratios for each Item';
+
+    public function handle()
+    {
+        $itemsCount = Item::count();
+        $this->output->progressStart($itemsCount);
+
+        $chunkSize = 100;
+        $count = 0;
+
+        Item::chunk($chunkSize, function ($items) use (&$count) {
+            foreach ($items as $item) {
+                if (!$item->hasImageForId($item->id)) {
+                    continue;
+                }
+
+                $path = $item->getImagePath(true);
+                $image = \Image::make($path);
+                $item->image_ratio = $image->getWidth() / $image->getHeight();
+                $item->save();
+                $count++;
+                $this->output->progressAdvance();
+            }
+        });
+
+        $this->output->progressFinish();
+
+        $this->info('Generated image ratios for ' . $count . ' items.');
+    }
+}
