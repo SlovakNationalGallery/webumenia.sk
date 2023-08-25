e29d04b59366744e746756f8756cf7e8fde65d33
Ernest Walzel <ernest.walzel@sng.sk>
2023-08-25 07:51:37 +0200

    SetExperiments middleware for setting experiments anywhere

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Spatie\MissingPageRedirector\RedirectsMissingPages::class,
+        \App\Http\Middleware\SetExperiments::class,
     ];
 
     /**
