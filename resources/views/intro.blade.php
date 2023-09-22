3370198809368b64bae6a8cc55db984f79f04335
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2023-09-22 09:57:11 +0200

    fix homepage zoomable condition

--- resources/views/intro.blade.php
+++ resources/views/intro.blade.php
@@ -59,7 +59,7 @@
                                 <img src="{!! $item->getImagePath() !!}" class="img-responsive" alt="{!! $item->getTitleWithAuthors() !!} ">
                             </a>
                             <div class="item-title">
-                                @if (!empty($item->iipimg_url))
+                                @if ($item->has_iip)
                                     <div class="pull-right"><a href="{!! URL::to('dielo/' . $item->id . '/zoom') !!}" data-toggle="tooltip" data-placement="left" title="Zoom obrázku"><i class="fa fa-search-plus"></i></a></div>
                                 @endif
                                 <a href="{!! $item->getUrl() !!}">
