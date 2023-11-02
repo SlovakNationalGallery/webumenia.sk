c16407a685516de9c9dac8634b5c0705ee6d3949
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2023-11-02 10:03:28 +0100

    [harvest] use static string in migration

--- database/migrations/2023_10_30_141138_delete_gmuhk_spice_harvester_records.php
+++ database/migrations/2023_10_30_141138_delete_gmuhk_spice_harvester_records.php
@@ -10,7 +10,7 @@ return new class extends Migration {
         DB::query()
             ->from('spice_harvester_records')
             ->join('spice_harvester_harvests', 'spice_harvester_harvests.id', '=', 'spice_harvester_records.harvest_id')
-            ->where('spice_harvester_harvests.type', GmuhkItemHarvester::class)
+            ->where('spice_harvester_harvests.type', 'App\\Harvest\\Harvesters\\GmuhkItemHarvester')
             ->delete();
     }
 
