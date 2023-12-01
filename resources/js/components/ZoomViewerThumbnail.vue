fd2765a777580b20efff26a762427490125d87c8
Ernest Walzel <ernest.walzel@sng.sk>
2023-12-01 12:20:32 +0100

    ZoomViewerThumbnail with lazy loading and scrolling into view

---
+++ resources/js/components/ZoomViewerThumbnail.vue
@@ -0,0 +1,17 @@
+<template>
+    <img ref="img" @load="onLoad" @click="$emit('click')" />
+</template>
+
+<script>
+export default {
+    props: {
+        scrollIntoView: Boolean,
+    },
+    methods: {
+        onLoad() {
+            if (!this.scrollIntoView) return
+            this.$refs.img.scrollIntoView()
+        },
+    },
+}
+</script>
--- resources/js/zoom.js
+++ resources/js/zoom.js
@@ -4,5 +4,6 @@ import VueDragscroll from 'vue-dragscroll'
 Vue.use(VueDragscroll)
 
 Vue.component('zoom-viewer', require('./components/ZoomViewer.vue').default)
+Vue.component('zoom-viewer.thumbnail', require('./components/ZoomViewerThumbnail.vue').default)
 
 new Vue({ el: '#app' })
