ea7712a656a3257ddd0b8231fcf11564d6827127
Ernest Walzel <ernest.walzel@sng.sk>
2024-02-16 12:22:19 +0100

    New catalog title is based on filters (in Blade)

--- app/Http/Controllers/NewCatalogController.php
+++ app/Http/Controllers/NewCatalogController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Authority;
-use App\Item;
-use Illuminate\Support\Facades\Cache;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class NewCatalogController extends Controller
 {
@@ -13,8 +12,62 @@ class NewCatalogController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('frontend.catalog-new.index-new');
+        return view('frontend.catalog-new.index-new', ['title' => self::generateTitle($request)]);
+    }
+
+    private static function generateTitle(Request $request)
+    {
+        $filters = $request->input('filter');
+
+        $attributes = collect([
+            'search',
+            'author',
+            'work_type',
+            'tag',
+            'gallery',
+            'credit',
+            'topic',
+            'medium',
+            'technique',
+            'has_image',
+            'has_iip',
+            'is_free',
+            'related_work',
+            'date_earliest',
+        ]);
+
+        return $attributes
+            ->filter(fn($attribute) => Arr::has($filters, $attribute))
+            ->map(function ($attribute) use ($filters) {
+                if ($attribute === 'date_earliest') {
+                    $from = Arr::get($filters, 'date_latest.gte');
+                    $to = Arr::get($filters, 'date_earliest.lte');
+
+                    if (!$from || !$to) {
+                        return;
+                    }
+
+                    return trans('item.filter.title_generator.years', [
+                        'from' => $from,
+                        'to' => $to,
+                    ]);
+                }
+
+                $values = collect(
+                    is_array($filters[$attribute]) ? $filters[$attribute] : [$filters[$attribute]]
+                );
+
+                if ($attribute === 'author') {
+                    $values = $values->map(fn($v) => formatName($v));
+                }
+
+                return trans('item.filter.title_generator.' . $attribute, [
+                    'value' => $values->implode(', '),
+                ]);
+            })
+            ->filter() // filter out empty values
+            ->implode(' • ');
     }
 }
---
+++ tests/NewCatalogTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Views\Frontend;
+
+use App\Facades\Experiment;
+use Tests\TestCase;
+
+class NewCatalogTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Experiment::set('new-catalog');
+    }
+
+    public function test_title_is_based_on_filters()
+    {
+        $this->get(
+            route('frontend.catalog.index', [
+                'filter[author]' => ['Mednyánszky, Ladislav', 'Giambologna'],
+                'filter[date_latest][gte]' => '1651',
+                'filter[date_earliest][lte]' => '1932',
+            ])
+        )->assertSeeText('autor: Ladislav Mednyánszky, Giambologna • v rokoch 1651 — 1932 |');
+    }
+
+    public function test_title_works_with_authority_ids()
+    {
+        $this->markTestIncomplete('Waiting for WEBUMENIA-2005');
+    }
+}
