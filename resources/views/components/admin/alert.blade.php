68ea86bfee14b55668af22e9d16f168cd6f2c597
Ernest Walzel <ernest.walzel@sng.sk>
2024-03-15 09:30:52 +0100

    Make admin.alert component not dismissable by default. Add warning style

--- resources/views/components/admin/alert.blade.php
+++ resources/views/components/admin/alert.blade.php
@@ -1,9 +1,14 @@
 @props([
     'danger' => false,
+    'warning' => false,
     'info' => false,
+    'dismissable' => false,
 ])
 
-<div {{ $attributes->class(['alert', 'alert-danger' => $danger, 'alert-info' => $info]) }}>
-    <a href="#" class="close" data-dismiss="alert">&times;</a>
+<div
+    {{ $attributes->class(['alert', 'alert-danger' => $danger, 'alert-info' => $info, 'alert-warning' => $warning]) }}>
+    @if ($dismissable)
+        <a href="#" class="close" data-dismiss="alert">&times;</a>
+    @endif
     {{ $slot }}
 </div>
