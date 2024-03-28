39441771ce9b7758936c14abbd3e761e50e131b9
Ernest Walzel <ernest.walzel@sng.sk>
2024-03-28 13:19:17 +0100

    Re-work app initializers into Vue 3 mounting style

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,24 +1,22 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import { Lang } from 'laravel-vue-lang'
 
 import 'vue-select/dist/vue-select.css'
 
-Vue.use(Lang, { fallback: 'sk' })
+createApp()
+    .use(Lang, { fallback: 'sk' })
 
-window.Vue = Vue
-window.debounce = require('debounce')
+    .component('admin-links-input', require('./components/admin/LinksInput.vue').default)
+    .component('autocomplete', require('./components/Autocomplete.vue').default)
+    .component('linked-combos', require('./components/vue/linked-combos').default)
+    .component('query-string', require('./components/QueryString.vue').default)
+    .component('croppr', require('./components/Croppr.vue').default)
+    .component('tabs-controller', require('./components/TabsController.vue').default)
+    .component('tab', require('./components/Tab.vue').default)
+    .component('tab-panel', require('./components/TabPanel.vue').default)
+    .component(
+        'admin.shuffle-item-filters',
+        require('./components/admin/ShuffleItemFilters.vue').default
+    )
 
-Vue.component('admin-links-input', require('./components/admin/LinksInput.vue').default)
-Vue.component('autocomplete', require('./components/Autocomplete.vue').default)
-Vue.component('linked-combos', require('./components/vue/linked-combos').default)
-Vue.component('query-string', require('./components/QueryString.vue').default)
-Vue.component('croppr', require('./components/Croppr.vue').default)
-Vue.component('tabs-controller', require('./components/TabsController.vue').default)
-Vue.component('tab', require('./components/Tab.vue').default)
-Vue.component('tab-panel', require('./components/TabPanel.vue').default)
-Vue.component(
-    'admin.shuffle-item-filters',
-    require('./components/admin/ShuffleItemFilters.vue').default
-)
-
-new Vue({ el: '#wrapper' })
+    .mount('#wrapper')
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 require('./bootstrap')
-require( 'slick-carousel');
+require('slick-carousel')
 require('selectize')
 
 const jQueryBridget = require('jquery-bridget')
@@ -14,72 +14,139 @@ require('lazysizes')
 require('lazysizes/plugins/unveilhooks/ls.unveilhooks')
 require('lazysizes/plugins/respimg/ls.respimg')
 require('jquery.easing')
+require('@lottiefiles/lottie-player')
 
 // Components
 require('./components/searchbar')
 require('./components/clipboard-button')
 
-// Vue components
-import Vue from 'vue'
-import { ZiggyVue } from 'ziggy';
-import * as LottiePlayer from "@lottiefiles/lottie-player";
+// Vue
+import { createApp } from 'vue'
 import { VueMasonryPlugin } from 'vue-masonry'
-import { Lang } from 'laravel-vue-lang';
-import VueClickAway from "vue3-click-away";
+import VueClickAway from 'vue3-click-away'
+import { Lang } from 'laravel-vue-lang'
+import { ZiggyVue } from 'ziggy'
 
-window.Vue = Vue
-Vue.use(VueMasonryPlugin)
-Vue.use(VueClickAway)
-Vue.use(Lang, { fallback: 'sk' })
-Vue.component('featured-piece-click-tracker', require('./components/FeaturedPieceClickTracker.vue').default);
-Vue.component('filter-sort-by', require('./components/filter/SortBy.vue').default);
-Vue.component('filter-checkbox', require('./components/filter/Checkbox.vue').default);
-Vue.component('filter-custom-select', require('./components/filter/CustomSelect.vue').default);
-Vue.component('filter-new-items-controller', require('./components/catalog-new/ItemsFilterController.vue').default);
-Vue.component('filter-new-popover.group-controller', require('./components/catalog-new/PopoverGroupController.vue').default);
-Vue.component('filter-new-custom-select-popover-label', require('./components/catalog-new/NewCustomSelectPopoverLabel.vue').default);
-Vue.component('filter-new-color-slider', require('./components/catalog-new/NewColorSlider.vue').default);
-Vue.component('filter-new-year-slider', require('./components/catalog-new/NewYearSlider.vue').default);
-Vue.component('filter-new-custom-checkbox', require('./components/catalog-new/NewCustomCheckbox.vue').default);
-Vue.component('filter-disclosure-controller', require('./components/catalog-new/DisclosureModalController.vue').default);
-Vue.component('toggle-controller', require('./components/ToggleController.vue').default);
-Vue.component('reload-controller', require('./components/ReloadController.vue').default);
-Vue.component('filter-search-options-controller', require('./components/catalog-new/SearchOptionsController.vue').default);
-Vue.component('filter-popover-controller', require('./components/catalog-new/PopoverController.vue').default);
-Vue.component('catalog.infinite-scroll', require('./components/catalog-new/InfiniteScroll.vue').default);
-Vue.component('catalog.artwork-image-controller', require('./components/catalog-new/ArtworkImageController.vue').default);
-Vue.component('catalog.number-formatter', require('./components/catalog-new/NumberFormatter.vue').default);
-Vue.component('catalog.author-formatter', require('./components/catalog-new/AuthorFormatter.vue').default);
-Vue.component('flickity', require('./components/Flickity.vue').default);
-Vue.component('home.shuffle-orchestrator', require('./components/home/ShuffleOrchestrator.vue').default);
-Vue.component('home.transition-in-place', require('./components/home/TransitionInPlace.vue').default);
-Vue.component('tabs-controller', require('./components/TabsController.vue').default);
-Vue.component('tab', require('./components/Tab.vue').default);
-Vue.component('tab-panel', require('./components/TabPanel.vue').default);
-Vue.component('user-collections-link', require('./components/user-collections/Link.vue').default);
-Vue.component('user-collections-nav-link', require('./components/user-collections/NavLink.vue').default);
-Vue.component('user-collections-favourite-button', require('./components/user-collections/FavouriteButton.vue').default);
-Vue.component('user-collections-clear-button', require('./components/user-collections/ClearButton.vue').default);
-Vue.component('user-collections-share-form', require('./components/user-collections/ShareForm.vue').default);
-Vue.component('user-collections-store', require('./components/user-collections/Store.vue').default);
-Vue.component('user-collections-shared-collection', require('./components/user-collections/SharedCollection.vue').default);
-Vue.component('color-widget', require('./components/ColorWidget.vue').default);
-Vue.component('year-slider', require('./components/YearSlider.vue').default);
-Vue.component('inline-input', require('./components/InlineInput.vue').default);
-Vue.component('copy-to-clipboard-group', require('./components/CopyToClipboardGroup.vue').default);
-Vue.component('copy-to-clipboard-link', require('./components/CopyToClipboardLink.vue').default);
-Vue.component('bottom-modal', require('./components/BottomModal.vue').default);
-Vue.component('user-interaction-context', require('./components/UserInteractionContext.vue').default);
-Vue.component('newsletter-signup.form-controller', require('./components/newsletter-signup/FormController.vue').default);
-Vue.component('newsletter-signup.bottom-modal-controller', require('./components/newsletter-signup/BottomModalController.vue').default);
-Vue.component('slider', require('./components/vue/slider').default);
-Vue.component('color-slider', require('./components/vue/color-slider').default);
+createApp()
+    .use(VueMasonryPlugin)
+    .use(VueClickAway)
+    .use(Lang, { fallback: 'sk' })
+    .use(ZiggyVue)
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+    .component(
+        'featured-piece-click-tracker',
+        require('./components/FeaturedPieceClickTracker.vue').default
+    )
+    .component('filter-sort-by', require('./components/filter/SortBy.vue').default)
+    .component('filter-checkbox', require('./components/filter/Checkbox.vue').default)
+    .component('filter-custom-select', require('./components/filter/CustomSelect.vue').default)
+    .component(
+        'filter-new-items-controller',
+        require('./components/catalog-new/ItemsFilterController.vue').default
+    )
+    .component(
+        'filter-new-popover.group-controller',
+        require('./components/catalog-new/PopoverGroupController.vue').default
+    )
+    .component(
+        'filter-new-custom-select-popover-label',
+        require('./components/catalog-new/NewCustomSelectPopoverLabel.vue').default
+    )
+    .component(
+        'filter-new-color-slider',
+        require('./components/catalog-new/NewColorSlider.vue').default
+    )
+    .component(
+        'filter-new-year-slider',
+        require('./components/catalog-new/NewYearSlider.vue').default
+    )
+    .component(
+        'filter-new-custom-checkbox',
+        require('./components/catalog-new/NewCustomCheckbox.vue').default
+    )
+    .component(
+        'filter-disclosure-controller',
+        require('./components/catalog-new/DisclosureModalController.vue').default
+    )
+    .component('toggle-controller', require('./components/ToggleController.vue').default)
+    .component('reload-controller', require('./components/ReloadController.vue').default)
+    .component(
+        'filter-search-options-controller',
+        require('./components/catalog-new/SearchOptionsController.vue').default
+    )
+    .component(
+        'filter-popover-controller',
+        require('./components/catalog-new/PopoverController.vue').default
+    )
+    .component(
+        'catalog.infinite-scroll',
+        require('./components/catalog-new/InfiniteScroll.vue').default
+    )
+    .component(
+        'catalog.artwork-image-controller',
+        require('./components/catalog-new/ArtworkImageController.vue').default
+    )
+    .component(
+        'catalog.number-formatter',
+        require('./components/catalog-new/NumberFormatter.vue').default
+    )
+    .component(
+        'catalog.author-formatter',
+        require('./components/catalog-new/AuthorFormatter.vue').default
+    )
+    .component('flickity', require('./components/Flickity.vue').default)
+    .component(
+        'home.shuffle-orchestrator',
+        require('./components/home/ShuffleOrchestrator.vue').default
+    )
+    .component(
+        'home.transition-in-place',
+        require('./components/home/TransitionInPlace.vue').default
+    )
+    .component('tabs-controller', require('./components/TabsController.vue').default)
+    .component('tab', require('./components/Tab.vue').default)
+    .component('tab-panel', require('./components/TabPanel.vue').default)
+    .component('user-collections-link', require('./components/user-collections/Link.vue').default)
+    .component(
+        'user-collections-nav-link',
+        require('./components/user-collections/NavLink.vue').default
+    )
+    .component(
+        'user-collections-favourite-button',
+        require('./components/user-collections/FavouriteButton.vue').default
+    )
+    .component(
+        'user-collections-clear-button',
+        require('./components/user-collections/ClearButton.vue').default
+    )
+    .component(
+        'user-collections-share-form',
+        require('./components/user-collections/ShareForm.vue').default
+    )
+    .component('user-collections-store', require('./components/user-collections/Store.vue').default)
+    .component(
+        'user-collections-shared-collection',
+        require('./components/user-collections/SharedCollection.vue').default
+    )
+    .component('color-widget', require('./components/ColorWidget.vue').default)
+    .component('year-slider', require('./components/YearSlider.vue').default)
+    .component('inline-input', require('./components/InlineInput.vue').default)
+    .component('copy-to-clipboard-group', require('./components/CopyToClipboardGroup.vue').default)
+    .component('copy-to-clipboard-link', require('./components/CopyToClipboardLink.vue').default)
+    .component('bottom-modal', require('./components/BottomModal.vue').default)
+    .component(
+        'user-interaction-context',
+        require('./components/UserInteractionContext.vue').default
+    )
+    .component(
+        'newsletter-signup.form-controller',
+        require('./components/newsletter-signup/FormController.vue').default
+    )
+    .component(
+        'newsletter-signup.bottom-modal-controller',
+        require('./components/newsletter-signup/BottomModalController.vue').default
+    )
+    .component('slider', require('./components/vue/slider').default)
+    .component('color-slider', require('./components/vue/color-slider').default)
 
-Vue.prototype.$route = route;
-new Vue({ el: '#app' });
+    .mount('#app')
