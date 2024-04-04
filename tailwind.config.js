3e0bca77575776e32a2e59178b7eb81f8121dfaf
Ernest Walzel <ernest.walzel@sng.sk>
2024-04-04 10:53:30 +0200

    Tailwind CSS working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
     content: ['./resources/**/*.blade.php', './resources/**/*.js', './resources/**/*.vue'],
     important: '.tailwind-rules',
     theme: {
