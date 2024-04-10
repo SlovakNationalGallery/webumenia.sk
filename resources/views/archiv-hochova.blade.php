15e819417d127b83c87f95a1f4309228403b9e88
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2024-04-10 12:04:24 +0200

    fix template

--- resources/views/archiv-hochova.blade.php
+++ resources/views/archiv-hochova.blade.php
@@ -7,54 +7,52 @@
 
 @section('content')
 
-    @section('content')
-
-        <section class="info content-section">
-            <div class="info-body">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-4 pl-50 lead attributes">
-                            <p>
-                                V roce 2013 získala Moravská galerie pozůstalost významné české fotografky Dagmar Hochové (1926–2012). Díky daru, obsahujícímu tisíce pozitivů a rozsáhlý archiv negativů, se Moravská galerie stala správcem největší části dochovaného autorčina fotografického díla.
-                            </p>
-                            <p>
-                                Dagmar Hochová byla českou dokumentární fotografkou, která vyrostla z kořenů humanitní fotografie padesátých let. Její dílo je dnes ceněno nejen jako osobitý projev umělecké fotografie druhé poloviny minulého století, ale je stále více vnímáno i jako svědek této etapy naší národní historie.
-                            </p>
-                            <p>
-                                V roce 1989 dokumentovala Dagmar Hochová sametovou revoluci a od roku 1990 dva roky působila jako poslankyně Občanského fóra v České národní radě. Z mnoha tisíců políček negativů, které v přelomové době nasnímala, za svého života zveřejnila jen velmi malou část.
-                            </p>
-                        </div>
-                        <div class="col-md-6 attributes">
-                            <h3>Archiv negativů Dagmar Hochové</h3>
-                            <p>
-                                V letech 2020–2024 Moravská galerie v Brně ve spolupráci s Ústavem pro soudobé dějiny Akademie věd ČR a Kanceláří Poslanecké sněmovny Parlamentu ČR realizovala úplnou digitalizaci a odborné zpracování souboru negativů Dagmar Hochové.
-                                Projekt „Demokratická revoluce objektivem Dagmar Hochové: Zpřístupnění unikátní fotodokumentace z let 1989–1992“ byl spolufinancován se státní podporou Technologické agentury ČR v rámci Programu ÉTA.
-                            </p>
-                            <p>
-                                Část obsahově identifikovaných snímků dokumentujících utváření demokratické politiky po listopadu 1989 je zveřejněn jako <a href="https://sbirky.moravska-galerie.cz/katalog?related_work=Archiv+negativ%C5%AF+Dagmar+Hochov%C3%A9">Archiv negativů Dagmar Hochové</a>. Soubor je unikátní rozsahem i významem pro poznání veřejného života a politického vývoje. Obrázky jsou snímané z poslanecké lavice a dalších míst vyhrazených ne fotografům, ale politickým aktérům. „Myslím, že ocenění toho souboru teprve časem přijde,“ předpovídala Hochová ve vzpomínkové knize Narodila jsem se za bouřky (2008), v níž vyslovila přání, aby snímky jednou posloužily jako historický pramen ke studiu demokratické revoluce. Spolu s ní věříme, že najdou mnohostranné využití při vědeckých a osvětových projektech i jako pomůcka ve výuce.
-                            </p>
-                            <p>
-                                Naprostá většina snímků je zveřejněna poprvé a soubor se bude nadále rozšiřovat. Odborné zpracovávání fondu pokračuje a údaje jsou průběžně aktualizovány. Vaše připomínky a doplňující informace k fotografiím můžete zasílat na adresu <a href="mailto:jiri.patek@moravska-galerie.cz">jiri.patek@moravska-galerie.cz</a>.
-                            </p>
-                            <p>
-                                Poděkování za pomoc a podporu:<br />
-                                Zdeněk Bárta, David Binar, Ivan Binar, Terezie Burdová OSF, Pavel Dibelka, Jan Dobeš, Petr Dvořáček, Petr Coufal, Karel Cudlín, Josef Faktor, Jiří Fiedor, Štěpán Filípek, Adéla Gjuričová, Richard Hájek, Lukáš Hoch, Martin Hoch, Adam Hudek, Heřman Chromý, Katarína Chudá, Gabriela Imreczeová, Daniela Iwashita, Markéta Janíčková, Gabriela Kaliská, Eva Kantůrková, Markéta Kočí, Roman Krasnický, Dagmar Křížková, Vladimír Laštůvka, Vladimír Líbal, Ivo Mathé, Martina Morávková, Michal Musil, František Nárovec, Vít Němec, Rudolf Němeček, František Nerad, Štěpán Pecháček, Petr Roubal, Jana Ryšlinková, Karel Sieber, Martina Středová, Olga Sommerová, Jiří Suk, Táňa Šedová, Olga Šmejkalová, Karel Štindl, Karel Šuster, Ondřej Tikovský, Petr Valenta.
-                            </p>
-                            <p>
-                                Jiří Pátek, Moravská galerie v Brně<br />
-                                Tomáš Zahradníček, Ústav pro soudobé dějiny AV ČR
-                            </p>
-                            <p>
-                                <i>Tato databáze byla vytvořena se státní podporou <a href="https://www.tacr.cz/" target="_blank">Technologické agentury ČR</a> v rámci Programu ÉTA.</i>
-                            </p>
-                            <div class="tiraz">
-                                <img src="/images/mg/tacr.png" height="100" />
-                                <img src="/images/mg/usd.jpg" height="100" />
-                            </div>
+    <section class="info content-section">
+        <div class="info-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 pl-50 lead attributes">
+                        <p>
+                            V roce 2013 získala Moravská galerie pozůstalost významné české fotografky Dagmar Hochové (1926–2012). Díky daru, obsahujícímu tisíce pozitivů a rozsáhlý archiv negativů, se Moravská galerie stala správcem největší části dochovaného autorčina fotografického díla.
+                        </p>
+                        <p>
+                            Dagmar Hochová byla českou dokumentární fotografkou, která vyrostla z kořenů humanitní fotografie padesátých let. Její dílo je dnes ceněno nejen jako osobitý projev umělecké fotografie druhé poloviny minulého století, ale je stále více vnímáno i jako svědek této etapy naší národní historie.
+                        </p>
+                        <p>
+                            V roce 1989 dokumentovala Dagmar Hochová sametovou revoluci a od roku 1990 dva roky působila jako poslankyně Občanského fóra v České národní radě. Z mnoha tisíců políček negativů, které v přelomové době nasnímala, za svého života zveřejnila jen velmi malou část.
+                        </p>
+                    </div>
+                    <div class="col-md-6 attributes">
+                        <h3>Archiv negativů Dagmar Hochové</h3>
+                        <p>
+                            V letech 2020–2024 Moravská galerie v Brně ve spolupráci s Ústavem pro soudobé dějiny Akademie věd ČR a Kanceláří Poslanecké sněmovny Parlamentu ČR realizovala úplnou digitalizaci a odborné zpracování souboru negativů Dagmar Hochové.
+                            Projekt „Demokratická revoluce objektivem Dagmar Hochové: Zpřístupnění unikátní fotodokumentace z let 1989–1992“ byl spolufinancován se státní podporou Technologické agentury ČR v rámci Programu ÉTA.
+                        </p>
+                        <p>
+                            Část obsahově identifikovaných snímků dokumentujících utváření demokratické politiky po listopadu 1989 je zveřejněn jako <a href="https://sbirky.moravska-galerie.cz/katalog?related_work=Archiv+negativ%C5%AF+Dagmar+Hochov%C3%A9">Archiv negativů Dagmar Hochové</a>. Soubor je unikátní rozsahem i významem pro poznání veřejného života a politického vývoje. Obrázky jsou snímané z poslanecké lavice a dalších míst vyhrazených ne fotografům, ale politickým aktérům. „Myslím, že ocenění toho souboru teprve časem přijde,“ předpovídala Hochová ve vzpomínkové knize Narodila jsem se za bouřky (2008), v níž vyslovila přání, aby snímky jednou posloužily jako historický pramen ke studiu demokratické revoluce. Spolu s ní věříme, že najdou mnohostranné využití při vědeckých a osvětových projektech i jako pomůcka ve výuce.
+                        </p>
+                        <p>
+                            Naprostá většina snímků je zveřejněna poprvé a soubor se bude nadále rozšiřovat. Odborné zpracovávání fondu pokračuje a údaje jsou průběžně aktualizovány. Vaše připomínky a doplňující informace k fotografiím můžete zasílat na adresu <a href="mailto:jiri.patek@moravska-galerie.cz">jiri.patek@moravska-galerie.cz</a>.
+                        </p>
+                        <p>
+                            Poděkování za pomoc a podporu:<br />
+                            Zdeněk Bárta, David Binar, Ivan Binar, Terezie Burdová OSF, Pavel Dibelka, Jan Dobeš, Petr Dvořáček, Petr Coufal, Karel Cudlín, Josef Faktor, Jiří Fiedor, Štěpán Filípek, Adéla Gjuričová, Richard Hájek, Lukáš Hoch, Martin Hoch, Adam Hudek, Heřman Chromý, Katarína Chudá, Gabriela Imreczeová, Daniela Iwashita, Markéta Janíčková, Gabriela Kaliská, Eva Kantůrková, Markéta Kočí, Roman Krasnický, Dagmar Křížková, Vladimír Laštůvka, Vladimír Líbal, Ivo Mathé, Martina Morávková, Michal Musil, František Nárovec, Vít Němec, Rudolf Němeček, František Nerad, Štěpán Pecháček, Petr Roubal, Jana Ryšlinková, Karel Sieber, Martina Středová, Olga Sommerová, Jiří Suk, Táňa Šedová, Olga Šmejkalová, Karel Štindl, Karel Šuster, Ondřej Tikovský, Petr Valenta.
+                        </p>
+                        <p>
+                            Jiří Pátek, Moravská galerie v Brně<br />
+                            Tomáš Zahradníček, Ústav pro soudobé dějiny AV ČR
+                        </p>
+                        <p>
+                            <i>Tato databáze byla vytvořena se státní podporou <a href="https://www.tacr.cz/" target="_blank">Technologické agentury ČR</a> v rámci Programu ÉTA.</i>
+                        </p>
+                        <div class="tiraz">
+                            <img src="/images/mg/tacr.png" height="100" />
+                            <img src="/images/mg/usd.jpg" height="100" />
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+    </section>
 
-    @stop
+@stop
