70d37256a7eb95a3c7ef61cee85081ad4838df5b
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2024-08-20 10:55:45 +0200

    Use gate instead of user attribute

--- app/Http/Middleware/ConfigureFrontendFromUser.php
+++ app/Http/Middleware/ConfigureFrontendFromUser.php
@@ -6,13 +6,14 @@ use App\Enums\FrontendEnum;
 use App\Facades\Frontend;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\Response;
 
 class ConfigureFrontendFromUser
 {
     public function handle(Request $request, Closure $next): Response
     {
-        $frontend = !$request->user()->can_administer
+        $frontend = Gate::denies('administer')
             ? FrontendEnum::from($request->user()->frontend)
             : null;
 
